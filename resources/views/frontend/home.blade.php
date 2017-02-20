5d6718bd35655fe286d6ee463023a359d4c268c6
hugovom <hugo.om@gmail.com>
2017-02-20 17:25:21 -0600

    add frontend view

---
+++ resources/views/frontend/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.frontend.master')
+@section('title', 'Gobierno Abierto desde lo local para el desarrollo sostenible')
+@section('description', 'Plataforma en línea de vinculación, aprendizaje continuo, intercambio de experiencias y acción coordinada de los agentes de cambio formados en el fellowship')
+@section('body_class', 'home')
+@section('canonical', '' )
+
+@section('content')
+<div class="row">
+	<div class="col-sm-10 col-sm-offser-1">
+		<h1>SÉ UN AGENTE DE CAMBIO</h1>
+	</div>
+</div>
+@endsection
