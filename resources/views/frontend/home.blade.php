d262573b6f3bbc8be25ca01fdd0ed1339e33b6f9
hugovom <hugo.om@gmail.com>
2017-02-20 21:57:41 -0600

    update home

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -6,8 +6,39 @@
 
 @section('content')
 <div class="row">
-	<div class="col-sm-10 col-sm-offser-1">
-		<h1>SÉ UN AGENTE DE CAMBIO</h1>
+	<div class="col-sm-10 col-sm-offset-1">
+		<div class="bgdimg">
+			<figure>
+				<img src="{{url('img/home_bgd.jpg')}}" alt="Gobierno Abierto desde lo local para el desarrollo sostenible">
+			</figure>
+			<div class="cta">
+				<div class="col-sm-8 col-sm-offset-2">
+				<h1>SÉ UN <strong>AGENTE DE CAMBIO</strong></h1>
+				<h2>Programa de Fortalecimiento de Capacidades para <strong>Agentes de Cambio</strong> de <strong>Gobierno Abierto</strong></h2>
+				<h3>Convocatoria abierta del <span><strong>3 al 31 de marzo de 2017</strong></span></h3>
+				<a href="#" class="btn">Participa</a>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="row">
+	<div class="col-sm-10 col-sm-offset-1">
+		<h2>PROGRAMA DE FORTALECIMIENTO DE CAPACIDADES PARA AGENTES DE CAMBIO DE GOBIERNO ABIERTO</h2>
+		<p>Lorem ipsum dolor sit amet, te semper detracto imperdiet has. Atqui utinam diceret mea ad, causae qualisque vis ne. In sea essent ponderum efficiendi, eu nisl phaedrum vis. Sumo wisi iisque ei has, tale facete deterruisset his an. Cu vix nisl choro menandri, euripidis necessitatibus cu mea, vix virtute albucius maluisset et. Duo quaeque veritus dissentias te, eu lorem ubique his.</p>
+		<div class="row">
+			<div class="col-sm-4">
+				<h3>PROGRAMA</h3>
+			</div>
+			<div class="col-sm-4">
+				<h3>CONVOCATORIA</h3>
+			</div>
+			<div class="col-sm-4">
+				<h3>ALCANCE</h3>
+			</div>
+		</div>
 	</div>
 </div>
 @endsection
