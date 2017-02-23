3d4fa1dba8d8c664ec6a6062ec8e95711c83fa7f
hugovom <hugo.om@gmail.com>
2017-02-22 22:54:03 -0600

    update home

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -26,20 +26,26 @@
 
 <div class="row">
 	<div class="col-sm-8 col-sm-offset-2">
-		<h2 class="title">PROGRAMA DE FORTALECIMIENTO DE CAPACIDADES PARA <strong>AGENTES DE CAMBIO</strong> DE <strong>GOBIERNO ABIERTO</strong></h2>
+		<h2 class="title">¿QUÉ ES EL PROGRAMA DE FORTALECIMIENTO DE CAPACIDADES PARA <strong>AGENTES DE CAMBIO</strong> DE <strong>GOBIERNO ABIERTO?</strong></h2>
 	</div>
 	<div class="col-sm-10 col-sm-offset-1">
-		<p>El objetivo del Programa es contribuir al fortalecimiento de una buena gobernanza en México a partir de prácticas de <strong>Gobierno Abierto</strong>, participación ciudadana, transparencia y anticorrupción; con esquemas innovadores de desarrollo de capacidades y de vinculación social que permitan el empoderamiento de agentes de cambio, así como el impulso de espacios de diálogo y co creación a nivel subnacional.</p>
-		<p>Esta es una iniciativa del <a href="http://www.mx.undp.org/">Programa de las Naciones Unidas para el Desarrollo (PNUD)</a> y estará apoyado permanentemente por actores tanto del gobierno como de la sociedad civil: el <a href="http://inicio.ifai.org.mx">Instituto Nacional de Transparencia, Acceso a la Información y Protección de Datos Personales (INAI)</a>, <a href="http://www.gesoc.org.mx/site/">Gestión Social y Cooperación (GESOC)</a>, <a href="https://gobiernofacil.com">Gobierno Fácil</a> y <a href="http://www.prosociedad.org/">ProSociedad Hacer el Bien</a>. Conoce más sobre el proyecto:</p>
+		<p>El Programa contribuye al fortalecimiento de una buena gobernanza en México a partir de prácticas de <strong>Gobierno Abierto</strong>, participación ciudadana, transparencia y anticorrupción, con esquemas innovadores de desarrollo de capacidades y de vinculación social que permitan el empoderamiento de agentes de cambio, así como el impulso de espacios de diálogo y co creación a nivel subnacional.</p>
+		<p>Es una iniciativa del <a href="http://www.mx.undp.org/">Programa de las Naciones Unidas para el Desarrollo (PNUD)</a>, posible gracias al apoyo de la <a href="https://www.usaid.gov/mexico">Agencia de los Estados Unidos para el Desarrollo Internacional</a> (USAID por sus siglas en inglés), desarrollada y acompañada por el trabajo conjunto de actores tanto del gobierno como de la sociedad civil: el <a href="http://inicio.ifai.org.mx">Instituto Nacional de Transparencia, Acceso a la Información y Protección de Datos Personales (INAI)</a>, <a href="http://www.gesoc.org.mx/site/">Gestión Social y Cooperación (GESOC)</a>, <a href="https://gobiernofacil.com">Gobierno Fácil</a> y <a href="http://www.prosociedad.org/">ProSociedad Hacer el Bien</a>. Conoce más sobre el proyecto:</p>
 		<div class="row">
 			<div class="col-sm-4">
-				<h3>PROGRAMA</h3>
+				<a href="" class="icon i_programa">
+					<span>PROGRAMA</span>
+				</a>
 			</div>
 			<div class="col-sm-4">
-				<h3>CONVOCATORIA</h3>
+				<a href="" class="icon i_convocatoria">
+					<span>CONVOCATORIA</span>
+				</a>
 			</div>
 			<div class="col-sm-4">
-				<h3>ALCANCE</h3>
+				<a href="" class="icon i_alcance">
+					<span>ALCANCE</span>
+				</a>
 			</div>
 		</div>
 	</div>
