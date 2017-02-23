d4873a35d6e2dd5a68de8a9e11bb81dd405db88b
hugovom <hugo.om@gmail.com>
2017-02-22 22:54:11 -0600

    update footer

---
+++ resources/views/layouts/frontend/footer.blade.php
@@ -0,0 +1,33 @@
+<div class="allies">
+	<div class="container">
+	<div class="row">
+		<div class="col-sm-12">
+			<h2>Aliados</h2>
+			<div class="row">
+				<a href="#" class="usaid">USAID</a>				
+				<a href="#" class="pnud">PNUD</a>				
+				<a href="#" class="inai">INAI</a>				
+				<a href="#" class="gesoc">GESOC</a>				
+				<a href="#" class="gf">Gobierno Fácil</a>
+				<a href="#" class="prosociedad">Prosociedad</a>
+			</div>
+		</div>
+	</div>
+	</div>
+</div>
+<footer>
+	<div class="container">
+		<div class="container">
+			<div class="row">
+				<div class="col-sm-6">
+					<p>® 2017-2020 Gobierno Abierto desde lo local para el Desarrollo Sostenible.<br>
+					<a href="{{url('politica')}}">Política de Privacidad</a></p>
+				</div>
+				<div class="col-sm-6 right">
+					<p>Forjado Artesanalmente por:<br>
+					<a href="https://www.gobiernofacil.com" class="gobiernofacil">Gobierno Fácil</a></p>
+				</div>
+			</div>
+		</div>
+	</div>
+</footer>
