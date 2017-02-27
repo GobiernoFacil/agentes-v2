122599ec24d69a65e5510e32b4953afee6fd54f5
hugovom <hugo.om@gmail.com>
2017-02-27 10:46:27 -0600

    add links

--- resources/views/frontend/programas/scope-list.blade.php
+++ resources/views/frontend/programas/scope-list.blade.php
@@ -9,11 +9,11 @@
 		<h1><strong>Alcance</strong> del Programa de Formación de Agentes Locales de Cambio en Gobierno Abierto y Desarrollo Sostenible</h1>
 		<p>En este componente del proyecto se desarrollará un piloto para el impulso de los espacios institucionalizados de diálogo y co-creación de compromisos alineados a los Objetivos de Desarrollo Sostenible, con las siguientes entidades federativas: Chihuahua, Coahuila, Morelos, Nuevo León y Oaxaca. La selección de las entidades federativas se realizó otorgando prioridad a aquellos estados que estén participando desde 2015 en los ejercicios de gobierno abierto local promovidos por el <strong>INAI</strong>, y procurando garantizar representación geográfica. Asimismo, se consideraron estados en diferentes momentos de avance en cuanto a la instalación de sus Secretariados Tripartitas Locales y en relación al diseño e implementación de sus planes de acción.</p>
 		<ul>
-			<li>Chihuahua</li>
-			<li>Coahuila</li>
-			<li>Morelos</li>
-			<li>Nuevo León</li>
-			<li>Oaxaca</li>
+			<li><a href="{{ url('programa-gobierno-abierto/alcance/chihuahua')}}">Chihuahua</a></li>
+			<li><a href="{{ url('programa-gobierno-abierto/alcance/coahuila')}}">Coahuila</a></li>
+			<li><a href="{{ url('programa-gobierno-abierto/alcance/morelos')}}">Morelos</a></li>
+			<li><a href="{{ url('programa-gobierno-abierto/alcance/nuevo-leon')}}">Nuevo León</a></li>
+			<li><a href="{{ url('programa-gobierno-abierto/alcance/oaxaca')}}">Oaxaca</a></li>
 		</ul>
 	</div>
 </div>
