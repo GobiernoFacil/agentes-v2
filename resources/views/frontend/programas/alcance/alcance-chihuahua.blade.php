718f0d16d2a7b602e7b900b6a94c7b392c1e2f5b
hugovom <hugo.om@gmail.com>
2017-02-27 10:46:39 -0600

    add chihuahua

---
+++ resources/views/frontend/programas/alcance/alcance-chihuahua.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.frontend.master')
+@section('title', 'Chihuahua, ejercicio local de Gobierno Abierto')
+@section('description', 'El 20 de junio de 2016, el estado de Chihuahua se integró formalmente a los Ejercicios Locales de Gobierno Abierto a través de la firma de la Declaración Conjunta para la Implementación de las Acciones para un Gobierno Abierto.')
+@section('body_class', 'convocatoria')
+@section('canonical', url('programa-gobierno-abierto/alcance/chihuahua') )
+@section('content')
+<div class="row">
+	<div class="col-sm-10 col-sm-offset-1">
+		<h1><strong>Chihuahua</strong>, ejercicio local de gobierno abierto</h1>
+		<h2>¿Quiénes conforman el ejercicio?</h2>
+		<p>El Grupo Promotor del ejercicio de gobierno abierto está conformado por:</p>
+		<ul>
+			<li>
+				<h3>1 representantes de la <strong>Sociedad Civil</strong></h3>				 
+				<ol>
+					<li>Promotora Social, Personas con Discapacidad, A.C.</li>
+				</ol>
+			</li>
+			<li><h3>1 representantes del Gobierno								 </h3>
+				<ol>
+					<li>Tribunal Superior de Justicia de Chihuahua</li>
+				</ol>
+			</li>
+			<li><h3>1 representante del Órgano Garante de Acceso a la Información</h3>
+				<ol>
+					<li>Instituto Chihuahuense para la Transparencia y Acceso a la Información Pública</li>
+				</ol>
+			</li>
+		</ul>
+		
+		<!---contexto-->
+		<h2>Contexto</h2>
+					<p>El 20 de junio de 2016, el estado de Chihuahua se integró formalmente a los Ejercicios Locales de Gobierno Abierto a través de la firma de la <a href="http://inicio.inai.org.mx/SiteCollectionDocuments/Transparencia/Chihuahua/Chihuahua.pdf">Declaración Conjunta para la Implementación de las Acciones para un Gobierno Abierto</a>.</p>
+			<p>En esta Declaración firmada por un representante del Gobierno del estado, un representante de la sociedad civil y el Organismo Garante de Acceso a la Información, Chihuahua se comprometió a implementar políticas de apertura por medio de la institucionalización de órganos consultivos permanentes, metodologías para la identificación de problemáticas específicas y adoptar esfuerzos ciudadanos innovadores.</p>
+			<p>El 29 de septiembre se sostuvo una reunión con dos magistrados del Tribunal Superior de Justicia de Chihuahua, en la cual se les orientó acerca de los pasos iniciales para la realización del ejercicio en el estado. En dicha oportunidad, se comentó un tema identificado en el que existe interés para que posteriormente se pueda establecer como un compromiso de Gobierno Abierto: igualdad en el acceso a la justicia para personas con alguna discapacidad.</p>
+			<p>Los días 15 y 16 de noviembre se realizaron dos <a href="http://www.ichitaip.org/infoweb/noticias/muestra_noticia.php?id=405">Foros de Sensibilización en Chihuahua y Ciudad Juárez</a> respectivamente, dirigidos a altos funcionarios del estado y a sociedad civil en general. </p>
+			<p>Posteriormente, el estado definirá su Plan de Trabajo para avanzar en los siguientes pasos del proceso que contempla el Ejercicio Local de Gobierno Abierto. </p>
+		
+		<!-- estatus-->
+		<h2>Estatus</h2>
+		<p>En proceso de sensibilización de la iniciativa y de convocatoria a un mayor número de actores gubernamentales y sociales.</p>
+
+	</div>
+</div>
+
+@endsection
