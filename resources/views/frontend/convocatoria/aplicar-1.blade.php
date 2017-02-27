d53c6db82df71a9e50382e45e5cbc1c5d403b98c
hugovom <hugo.om@gmail.com>
2017-02-27 00:27:51 -0600

    update aplicar

--- resources/views/frontend/convocatoria/aplicar-1.blade.php
+++ resources/views/frontend/convocatoria/aplicar-1.blade.php
@@ -1,9 +1,22 @@
 @extends('layouts.frontend.master')
+@section('title', 'Aplica a la Convocatoria Programa de Formación de Agentes Locales de Cambio en Gobierno Abierto y Desarrollo Sostenible')
+@section('description', 'Aplica a la Convocatoria Programa de Formación de Agentes Locales de Cambio en Gobierno Abierto y Desarrollo Sostenible')
+@section('body_class', 'convocatoria')
+@section('canonical', url('convocatoria/aplicar') )
+
 @section('content')
-@if(Session::has('success'))
-  <div class="col-sm-12 message success">
-      {{ Session::get('success') }}
-  </div>
-@endif
-@include('frontend.convocatoria.forms.register')
+<div class="row">
+	<div class="col-sm-10 col-sm-offset-1">
+		<h1><strong>Aplica</strong> a la Convocatoria Programa de Formación de Agentes Locales de Cambio en Gobierno Abierto y Desarrollo Sostenible</h1>
+		@if(Session::has('success'))
+		<div class="message success">
+	      {{ Session::get('success') }}
+	  	</div>
+	  	@endif
+	</div>
+	<div class="col-sm-8 col-sm-offset-2">
+		<p>Todos los campos son obligatorios.</p>
+		@include('frontend.convocatoria.forms.register')
+	</div>
+</div>
 @endsection
--- resources/views/frontend/convocatoria/forms/register.blade.php
+++ resources/views/frontend/convocatoria/forms/register.blade.php
@@ -1,65 +1,82 @@
 {!! Form::open(['url' => 'convocatoria/aplicar', "class" => "form-horizontal"]) !!}
-
-<p>
-  <label>Nombre</label>
-  {{Form::text('name', null, ["class" => "form-control"])}}
-  @if($errors->has('name'))
-    <strong>{{$errors->first('name')}}</strong>
-  @endif
-</p>
-<p>
-  <label>Apellido Paterno</label>
-  {{Form::text('surname', null, ["class" => "form-control"])}}
-  @if($errors->has('surname'))
-    <strong>{{$errors->first('surname')}}</strong>
-  @endif
-</p>
-<p>
-  <label>Apellido Materno</label>
-  {{Form::text('lastname', null, ["class" => "form-control"])}}
-  @if($errors->has('lastname'))
-    <strong>{{$errors->first('lastname')}}</strong>
-  @endif
-</p>
-<p>
-  <label>Grado de estudios completados</label>
-  {{Form::text('degree', null, ["class" => "form-control"])}}
-  @if($errors->has('degree'))
-    <strong>{{$errors->first('degree')}}</strong>
-  @endif
-</p>
-<p>
-  <label>Correo</label>
-  {{Form::text('email', null, ["class" => "form-control"])}}
-  @if($errors->has('email'))
-    <strong>{{$errors->first('email')}}</strong>
-  @endif
-</p>
-
-<p>
-  <label>Confirmar correo</label>
-  {{Form::text('email-confirm', null, ['class' => 'form-control'])}}
-  @if($errors->has('email-confirm'))
-    <strong>{{$errors->first('email-confirm')}}</strong>
-  @endif
-</p>
-
-<p>
-  <label>Ciudad</label>
-  {{Form::text('city',null, ['class' => 'form-control'])}}
-  @if($errors->has('city'))
-    <strong>{{$errors->first('city')}}</strong>
-  @endif
-</p>
-
-<p>
-  <label>Estado</label>
-  {{Form::text('state',null, ['class' => 'form-control'])}}
-  @if($errors->has('state'))
-    <strong>{{$errors->first('state')}}</strong>
-  @endif
-</p>
-
-<p>{{Form::submit('Enviar', ['class' => 'btn'])}}</p>
-
+<div class="row">
+	<div class="col-sm-12">
+		<p>
+		  <label>Nombre(s)</label>
+		  {{Form::text('name', null, ["class" => "form-control"])}}
+		  @if($errors->has('name'))
+		    <strong>{{$errors->first('name')}}</strong>
+		  @endif
+		</p>
+	</div>
+	<div class="col-sm-6">
+		<p>
+		  <label>Apellido Paterno</label>
+		  {{Form::text('surname', null, ["class" => "form-control"])}}
+		  @if($errors->has('surname'))
+		    <strong>{{$errors->first('surname')}}</strong>
+		  @endif
+		</p>
+	</div>
+	<div class="col-sm-6">
+		<p>
+		  <label>Apellido Materno</label>
+		  {{Form::text('lastname', null, ["class" => "form-control"])}}
+		  @if($errors->has('lastname'))
+		    <strong>{{$errors->first('lastname')}}</strong>
+		  @endif
+		</p>
+	</div>
+	<div class="col-sm-12">
+		<p>
+		  <label>Grado de estudios completados</label>
+		  {{Form::text('degree', null, ["class" => "form-control"])}}
+		  @if($errors->has('degree'))
+		    <strong>{{$errors->first('degree')}}</strong>
+		  @endif
+		</p>
+	</div>
+	<div class="col-sm-6">
+		<p>
+		  <label>Correo</label>
+		  {{Form::text('email', null, ["class" => "form-control"])}}
+		  @if($errors->has('email'))
+		    <strong>{{$errors->first('email')}}</strong>
+		  @endif
+		</p>
+	</div>
+	<!--confirmar correo-->
+	<div class="col-sm-6">	
+		<p>
+		  <label>Confirmar correo</label>
+		  {{Form::text('email-confirm', null, ['class' => 'form-control'])}}
+		  @if($errors->has('email-confirm'))
+		    <strong>{{$errors->first('email-confirm')}}</strong>
+		  @endif
+		</p>
+	</div>
+	<!--estado-->
+	<div class="col-sm-6">
+		<p>
+		  <label>Estado</label>
+		  {{Form::text('state',null, ['class' => 'form-control'])}}
+		  @if($errors->has('state'))
+		    <strong>{{$errors->first('state')}}</strong>
+		  @endif
+		</p>
+	</div>
+	<!--ciudad-->
+	<div class="col-sm-6">
+		<p>
+		  <label>Ciudad</label>
+		  {{Form::text('city',null, ['class' => 'form-control'])}}
+		  @if($errors->has('city'))
+		    <strong>{{$errors->first('city')}}</strong>
+		  @endif
+		</p>
+	</div>
+	<div class="col-sm-6 col-sm-offset-3">
+		<p>{{Form::submit('Aplicar', ['class' => 'btn gde'])}}</p>
+	</div>
+</div>
 {!! Form::close() !!}
