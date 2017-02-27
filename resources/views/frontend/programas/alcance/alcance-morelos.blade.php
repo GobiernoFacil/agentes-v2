385b201e759ed43df6c7384d9d2da45b223447d0
hugovom <hugo.om@gmail.com>
2017-02-27 11:22:08 -0600

    add morelos

---
+++ resources/views/frontend/programas/alcance/alcance-morelos.blade.php
@@ -0,0 +1,64 @@
+@extends('layouts.frontend.master')
+@section('title', 'Morelos, ejercicio local de Gobierno Abierto')
+@section('description', 'El 9 de marzo de 2015, el estado de Morelos se integró formalmente a los Ejercicios Locales de Gobierno Abierto a través de la firma de la Declaración Conjunta para la Implementación de Acciones para un Gobierno Abierto.')
+@section('body_class', 'programa alcance')
+@section('canonical', url('programa-gobierno-abierto/alcance/morelos') )
+@section('content')
+<div class="row">
+	<div class="col-sm-10 col-sm-offset-1">
+		<h1><strong>Morelos</strong>, ejercicio local de gobierno abierto</h1>
+		<h2>¿Quiénes conforman el ejercicio?</h2>
+		<p>El Secretariado Técnico local (STL), está conformado por:</p>
+		<ul>
+			<li>
+				<h3>1 representantes de la <strong>Sociedad Civil</strong></h3>				 
+				<ol>
+					<li>COPARMEX Morelos</li>
+				</ol>
+			</li>
+			<li><h3>2 representantes del Gobierno								 </h3>
+				<ol>
+					<li>Magistrado Presidente del Tribunal Electoral del Estado de Morelos</li>
+					<li>Secretario de Hacienda del Estado de Morelos</li>
+				</ol>
+			</li>
+			<li><h3>1 representante del Órgano Garante de Acceso a la Información</h3>
+				<ol>
+					<li>Instituto Morelense de Información Pública y Estadística</li>
+				</ol>
+			</li>
+		</ul>
+		
+		<h3>Facilitador:</h3>
+		<p>Luz Stella Vallejo, Universidad Autónoma del Estado de Morelos</p>
+		
+		<!---contexto-->
+		<h2>Contexto</h2>
+		<p>El 9 de marzo de 2015, el estado de Morelos se integró formalmente a los Ejercicios Locales de Gobierno Abierto a través de la firma de la <a href="http://inicio.ifai.org.mx/Ms_Transparencia/PracticasExitosas/Nacionales/morelos/FirmaDeclaratoriaMorelos.pdf">Declaración Conjunta para la Implementación de Acciones para un Gobierno Abierto</a>.</p>
+		<p>En esta Declaración, firmada por dos representantes del Gobierno del Estado, un representante de la sociedad civil y el Organismo Garante de Acceso a la Información, Morelos se comprometió a establecer actividades y trabajos encaminados a consolidar y expandir la apertura institucional y la participación ciudadana.</p>
+		<p>Durante los meses de abril y junio de 2015 se realizaron jornadas de sensibilización con organizaciones de la sociedad civil y público en general, donde se informó y socializó el ejercicio Gobierno Abierto “Co-creación desde lo Local” y se capacitó a servidores públicos que mostraron interés hacia el proyecto.</p>
+		<p>Los días 16 y 18 de febrero de 2015 se mantuvieron reuniones de introducción al ejercicio de Gobierno Abierto entre el Órgano Garante de Transparencia en Morelos y aquellas instituciones gubernamentales interesadas en formar parte del STL. <a href="http://inicio.ifai.org.mx/Ms_Transparencia/PracticasExitosas/Nacionales/morelos/Acta%20de%20instalaci%C3%B3n%20STL.pdf">La instalación del STL de Morelos</a> y la selección de su facilitador se formalizó el 16 de abril de 2015. </p>
+		<p>Los días 16 de mayo, 19 y 21 de agosto de 2015, se realizaron las <a href="http://imipe.org.mx/index.php?opc=eventos&sec=Mesa_de_Trabajo_Gobierno_Abierto">Mesas de Trabajo de Demandas de la Sociedad Civil</a>, en las cuales participaron organizaciones de la sociedad civil, colegios de profesionistas, medios de comunicación y sociedad en general.</p>
+		<p>El 24 de agosto de 2015 se desarrolló la <a href="http://imipe.org.mx/index.php?opc=eventos&sec=Reunion_del_STL_Gobierno_Abierto_en_IMIPE">Mesa de Trabajo de Oferta de las Instituciones Gubernamentales</a>, donde se dio respuesta preliminar a las demandas de las Organizaciones de la Sociedad Civil.</p>
+		<p>Las Mesas de Trabajo trataron temas como Desarrollo Sustentable; Justicia Electoral; Seguridad Pública; Hacienda y Recaudación, Transparencia y Gobierno Abierto y Educación Pública.</p>
+		<p>De forma paralela, se llevó a cabo una Mesa de Tecnologías en la que el STL acordó las características mínimas que debería contener la <a href="http://gobiernoabiertomorelos.org/">página web sobre Gobierno Abierto en Morelos</a>, misma que aglutina todas las acciones, actividades y compromisos ejecutados en el marco de la iniciativa Cocreación Local.</p>
+		<p>Este proceso dio como resultado que el 29 de septiembre de 2015 se publicara el <a href="http://inicio.ifai.org.mx/Ms_Transparencia/PracticasExitosas/Nacionales/morelos/PLANACCIONMorelos.pdf">Plan de Acción Local</a>. El documento está integrado por 6 compromisos puntuales, realizables, relevantes y medibles, con duración de un año, además de 2 compromisos proactivos:</p>
+		<ul>
+			<li><strong>Compromiso 1:</strong> Generación de esquemas participativos entre sociedad y gobierno para el diseño de estrategias de manejo integral de residuos sólidos para la sustentabilidad ambiental del estado.</li>
+			<li><strong>Compromiso 2:</strong> Detonar, mediante la autoridad de justicia electoral, la simplificación de la comunicación gobierno-sociedad, promoviendo la rendición de cuentas, la homologación criterios y un mayor acceso a la justicia en el estado.</li>
+			<li><strong>Compromiso 3:</strong> Promover la mejora de los niveles de confianza en los policías estatales y municipales.</li>
+			<li><strong>Compromiso 4:</strong> Modificar el esquema de comunicación con la ciudadanía, elevando el nivel de transparencia del destino de la recaudación y fomentando una actitud positiva de cumplimiento en el pago de contribuciones.</li>
+			<li><strong>Compromiso 5:</strong> Potenciar el conocimiento del derecho de acceso a la información y del Gobierno Abierto como detonadores del ejercicio de otros derechos y factor de incidencia social para la detección de necesidades ciudadanas y soluciones colectivas.</li>
+			<li><strong>Compromiso 6:</strong> Seguir el dinero asignado al programa local “Beca Salario” e incrementar el conocimiento del destino de los recursos públicos en distintos ámbitos.</li>
+			<li><strong>Compromiso proactivo:</strong> Vigilancia del cumplimiento de los compromisos de la Agenda Mínima Ciudadana firmada por los candidatos a ocupar un puesto de elección popular en la cámara local de diputados.</li>
+			<li><strong>Compromiso proactivo:</strong> Reconstrucción y rehabilitación de la vía ferroviaria (kilómetro 43 al 123) y construcción de una estación multimodal para el traslado de distintos productos.</li>
+		</ul>
+		
+		<!-- estatus-->
+		<h2>Estatus</h2>
+		<p>En proceso de cierre del primer plan de acción y de inicio de trabajos para la elaboración del segundo plan.</p>
+
+	</div>
+</div>
+
+@endsection
