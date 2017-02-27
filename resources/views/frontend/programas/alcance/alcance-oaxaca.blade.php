7182ddc4518e7992257a2001709df0e5053ffb41
hugovom <hugo.om@gmail.com>
2017-02-27 08:04:17 -0600

    add oaxaca view

---
+++ resources/views/frontend/programas/alcance/alcance-oaxaca.blade.php
@@ -0,0 +1,57 @@
+@extends('layouts.frontend.master')
+@section('title', 'Oaxaca, ejercicio local de Gobierno Abierto')
+@section('description', 'El 9 de marzo de 2015, el estado de Oaxaca se integró formalmente a los Ejercicios Locales de Gobierno Abierto a través de la firma de la Declaración Conjunta para la Implementación de las Acciones para un Gobierno Abierto.')
+@section('body_class', 'convocatoria')
+@section('canonical', url('programa-gobierno-abierto/alcance/oaxaca') )
+@section('content')
+<div class="row">
+	<div class="col-sm-10 col-sm-offset-1">
+		<h1>Oaxaca</h1>
+		<h2>¿Quiénes conforman el ejercicio?</h2>
+		<p>El Secretariado Técnico Local (STL), está conformado por:</p>
+		<ul>
+			<li><h3>2 representantes de la <strong>Sociedad Civil</strong> 		 </h3>				 
+				<ol>
+					<li>Solidaridad Internacional Kanda, A.C.				  </li>
+					<li>Consejo de Colaboración Municipal de Oaxaca de Juárez </li>
+				</ol>
+			</li>
+			<li><h3>2 representantes del Gobierno								 </h3>
+				<ol>
+					<li>Subsecretario de Transparencia y Participación Social de la Secretaría de la Contraloría y Transparencia Gubernamental del Gobierno del Estado</li>
+					<li>Coordinación de Transparencia y Acceso a la Información Pública del Municipio de Oaxaca de Juárez </li>
+				</ol>
+			</li>
+			<li><h3>1 representante del Órgano Garante de Acceso a la Información</h3>
+				<ol>
+					<li>Instituto de Acceso a la Información Pública y Protección de Datos Personales del Estado de Oaxaca</li>
+				</ol>
+			</li>
+		</ul>
+		<h3>Facilitador:</h3> 
+		<p>Arantxa Sánchez López, Consultora </p>
+		<h2>Contexto</h2>
+		<p>El 9 de marzo de 2015, el estado de Oaxaca se integró formalmente a los Ejercicios Locales de Gobierno Abierto a través de la firma de la Declaración Conjunta para la Implementación de las Acciones para un Gobierno Abierto.</p>
+		<p>En esta Declaración, firmada por un representante del Gobierno del Estado, un representante de la sociedad civil y el Organismo Garante de Acceso a la Información, Oaxaca se comprometió a impulsar un nuevo modelo de gobernanza basado en la apertura institucional, la transparencia, rendición de cuentas y la participación ciudadana en el diseño e implementación de políticas públicas.</p>
+		<p>La instalación del STL de Oaxaca y la selección de su facilitador se formalizó el 7 de abril de 2015.
+		El 4 de julio, el STL convocó al foro "Gobierno Abierto y Sociedad Civil, Co creación de soluciones", realizado en las instalaciones del lmpact HUB Oaxaca, con la finalidad de dar a conocer al STL e invitar a la sociedad civil a sumarse a los trabajos para la conformación de un Plan de Acción Local (PAL). </p>
+		<p>En el mismo mes de julio de 2015, el STL realizó una consulta ciudadana con la finalidad de contar con mayores elementos para definir y sustentar los objetivos del PAL.</p>
+		<p>Con base en los objetivos propuestos por los miembros del STL y con la información obtenida por la consulta ciudadana, el 18 de agosto de 2015 se desarrollaron en el Centro de Evaluación e Innovación Educativa (CEVIE) de la Universidad Autónoma Benito Juárez de Oaxaca, Cuatro Mesas de Trabajo con las temáticas de Salud, Desarrollo Social, Educación y Servicios Públicos. </p>
+		<p>El 25 de agosto de 2015, se realizó una 5° Mesa de Trabajo para la revisión del Programa Prospera, como componente del PAL. 
+		Este proceso dio como resultado que el 19 de mayo de 2016 se publicara el Plan de Acción Local. El documento está integrado por 5 compromisos puntuales, realizables, relevantes y medibles, con duración de un año, además de 1 compromiso proactivo:</p>
+		<ul>
+			<li>Compromiso 1: Seguimiento de los recursos del Programa Bienestar de Dotación Gratuita de Uniformes y Útiles Escolares para alumnos de Educación Básica en Escuelas Públicas de la Ciudad de Oaxaca.</li>
+			<li>Compromiso 2: Fortalecimiento de la accesibilidad del espacio público para personas con discapacidad. </li>
+			<li>Compromiso 3: Fortalecimiento de la participación ciudadana en el Programa Cocina Comedor Nutricional Comunitaria del DIF Oaxaca. </li>
+			<li>Compromiso 4: Creación del Banco de Información Georreferenciada de Oaxaca de Juárez (BIG-Oax). </li>
+			<li>Compromiso 5: Solución de problemas logísticos y administrativos en la gestión de información de los Comités de Contraloría Social.</li>
+			<li>Compromiso proactivo: Promoción de una cultura local para el Gobierno Abierto.</li>
+		</ul>
+
+		<h2>Estatus</h2>
+		<p>En proceso de implementación del Plan de Acción Local e inicio de la Evaluación Preliminar</p>
+
+	</div>
+</div>
+
+@endsection
