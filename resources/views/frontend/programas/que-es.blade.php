fc2b52b0d00e5b76b861fd7392e3812c75e15b2c
hugovom <hugo.om@gmail.com>
2017-02-26 23:26:15 -0600

    add programas

---
+++ resources/views/frontend/programas/que-es.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.frontend.master')
+@section('title', 'Gobierno Abierto desde lo local para el desarrollo sostenible')
+@section('description', 'Plataforma en línea de vinculación, aprendizaje continuo, intercambio de experiencias y acción coordinada de los agentes de cambio formados en el fellowship')
+@section('body_class', 'home')
+@section('canonical', '' )
+
+@section('content')
+<div class="row">
+	<div class="col-sm-10 col-sm-offset-1">
+	<h1>PROGRAMA DE FORTALECIMIENTO DE CAPACIDADES PARA <strong>AGENTES DE CAMBIO</strong> DE <strong>GOBIERNO ABIERTO</strong>
+	</h1>
+	</div>
+</div>
+@endsection
