61eac7e162acd1b169f7f649d3d92cee59c357b7
hugovom <hugo.om@gmail.com>
2017-02-27 11:31:33 -0600

    add nuevo leon

---
+++ resources/views/frontend/programas/alcance/alcance-nuevo-leon.blade.php
@@ -0,0 +1,58 @@
+@extends('layouts.frontend.master')
+@section('title', 'Nuevo Léon, ejercicio local de Gobierno Abierto')
+@section('description', 'El 20 de junio de 2016, el estado de Nuevo León se integró formalmente a los Ejercicios Locales de Gobierno Abierto a través de la firma de la Declaración Conjunta para la Implementación de las Acciones para un Gobierno Abierto.')
+@section('body_class', 'programa alcance')
+@section('canonical', url('programa-gobierno-abierto/alcance/morelos') )
+@section('content')
+<div class="row">
+	<div class="col-sm-10 col-sm-offset-1">
+		<h1><strong>Nuevo Léon</strong>, ejercicio local de gobierno abierto</h1>
+		<h2>¿Quiénes conforman el ejercicio?</h2>
+		<p>El Secretariado Técnico Local (STL), está conformado por:</p>
+		<ul>
+			<li>
+				<h3>3 representantes de la <strong>Sociedad Civil</strong></h3>				 
+				<ol>
+					<li>Centro de Integración Ciudadana (CIC)</li>
+					<li>Cómo Vamos Nuevo León</li>
+					<li>Redes Quinto Poder IDEA</li>
+				</ol>
+			</li>
+			<li>
+				<h3>3 representantes del Gobierno</h3>
+				<ol>
+					<li>Coordinación Ejecutiva de la Administración Pública del estado de Nuevo León</li>
+					<li>Tribunal Superior de Justicia del estado de Nuevo León 						</li>
+					<li>Congreso del estado de Nuevo León 											</li>
+
+				</ol>
+			</li>
+			<li><h3>1 representante del Órgano Garante de Acceso a la Información</h3>
+				<ol>
+					<li>Instituto Morelense de Información Pública y Estadística</li>
+				</ol>
+			</li>
+		</ul>
+		
+		
+		
+		<!---contexto-->
+		<h2>Contexto</h2>
+		<p>El 20 de junio de 2016, el estado de Nuevo León se integró formalmente a los Ejercicios Locales de Gobierno Abierto a través de la firma de la <a href="http://inicio.inai.org.mx/SiteCollectionDocuments/Transparencia/Nuevo%20Le%C3%B3n/Nuevo%20Le%C3%B3n.pdf">Declaración Conjunta para la Implementación de las Acciones para un Gobierno Abierto</a>.</p>
+		<p>En esta Declaración, firmada por tres representantes del Gobierno del Estado, dos representantes de la sociedad civil y el Organismo Garante de Acceso a la Información, Nuevo León se comprometió a impulsar esquemas de apertura gubernamental e instituir Secretariados Técnicos Tripartitas como mecanismos responsables de coordinar, incorporar y orientar los intereses de los actores que participen en los ejercicios de Gobierno Abierto Local.</p>
+		<p>Los días 12 y 13 de septiembre de 2016 se llevó a cabo el <a href="http://revistacontraportada.com/archivos/805">Seminario de Gobierno y Parlamento Abierto</a> que sirvió como primera herramienta para sensibilizar sobre el tema a autoridades, miembros de la sociedad civil, académicos, público en general y otros sectores interesados.</p>
+		<p>Aunado a ello, se han realizado una serie de talleres de sensibilización dirigidos a sociedad civil, autoridades y periodistas; el 14 de septiembre tuvo lugar el <a href="http://revista-equidad.com/?p=42306">Taller de Sensibilización para Conocer el Ejercicio Local de Gobierno Abierto en Nuevo León</a> en los municipios de la zona sur del estado; el 14 de octubre el taller se dirigió a periodistas de la entidad, mientras que el 20 de octubre el taller se enfocó en los municipios de la zona norte.</p>
+		<p>El 4 de noviembre de 2016 tuvo lugar una Reunión de Trabajo entre los integrantes del grupo promotor del Ejercicio Local de Gobierno Abierto y personal de la Dirección de Gobierno Abierto del Instituto Nacional de Transparencia, Acceso a la Información y Protección de Datos Personales (INAI) en la que se presentaron los avances de la entidad en materia de sensibilización del ejercicio y se precisaron algunas características de la iniciativa y del proceso a seguir.</p>
+		<p>El 18 de noviembre de 2016 se formalizará la <a href="http://www.gobiernoabiertonl.org.mx/convocatoria.pdf">instalación del Secretariado Técnico Local</a> en un evento al que acudirán miembros de la sociedad civil, autoridades, academia y otros sectores interesados.</p>
+		<p>Posteriormente, el estado definirá su Plan de Trabajo para avanzar en los siguientes pasos del proceso que contempla el Ejercicio Local de Gobierno Abierto. </p>
+
+
+		
+		<!-- estatus-->
+		<h2>Estatus</h2>
+		<p>En proceso de instalación del Secretariado Técnico Local.</p>
+
+	</div>
+</div>
+
+@endsection
