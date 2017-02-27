989518d585f11b66cf6a251a2cbf105cad74e104
hugovom <hugo.om@gmail.com>
2017-02-27 13:31:33 -0600

    update view

--- resources/views/frontend/programas/que-es.blade.php
+++ resources/views/frontend/programas/que-es.blade.php
@@ -1,14 +1,124 @@
 @extends('layouts.frontend.master')
-@section('title', 'Gobierno Abierto desde lo local para el desarrollo sostenible')
-@section('description', 'Plataforma en línea de vinculación, aprendizaje continuo, intercambio de experiencias y acción coordinada de los agentes de cambio formados en el fellowship')
-@section('body_class', 'home')
-@section('canonical', '' )
+@section('title', 'Programa de fortalecimiento de capacidades para agentes de cambio de Gobierno Abierto')
+@section('description', 'Programa de fortalecimiento de capacidades para agentes de cambio de Gobierno Abierto')
+@section('body_class', 'programa')
+@section('canonical', url('programa-gobierno-abierto'))
 
 @section('content')
 <div class="row">
 	<div class="col-sm-10 col-sm-offset-1">
-	<h1>PROGRAMA DE FORTALECIMIENTO DE CAPACIDADES PARA <strong>AGENTES DE CAMBIO</strong> DE <strong>GOBIERNO ABIERTO</strong>
-	</h1>
+		<h1>PROGRAMA DE FORTALECIMIENTO DE CAPACIDADES PARA <strong>AGENTES DE CAMBIO</strong> DE <strong>GOBIERNO ABIERTO</strong></h1>
+		<p>El objetivo del proyecto es contribuir al fortalecimiento de una buena gobernanza en México a partir de prácticas de gobierno abierto, participación ciudadana, transparencia y anticorrupción. Durante los tres años de implementación de las estrategias, se buscará contribuir al cumplimiento de la agenda de prevención de la corrupción del Estado mexicano, así como de los Objetivos de Desarrollo Sostenible (ODS). </p>
+		
+		<p>Para alcanzar el objetivo, el componente se implementará en apoyo a las estrategias de gobierno abierto del INAI, con la colaboración de tres organizaciones de la sociedad civil (Gesoc, Gobierno Fácil, y ProSociedad Hacer Bien el Bien, A.C), que cuentan con experiencia en la implementación de prácticas de gobierno abierto y rendición de cuentas.</p>
+		<div class="row">
+			<div class="col-sm-9">
+			<h2>Objetivo específico </h2>
+			<p>El objetivo específico del componente es contribuir al ejercicio pleno de los derechos de las personas y abatir los factores de riesgo que propician la corrupción a nivel local, por medio de la puesta en marcha de esquemas innovadores de fortalecimiento de capacidades y de vinculación social que permitan el empoderamiento de agentes de cambio de gobierno abierto, así como del fortalecimiento de espacios de diálogo y co creación a nivel subnacional. Este objetivo se alcanzará a partir de la implementación de principios y prácticas de gobierno abierto que fortalezcan la participación de organizaciones de la sociedad civil (OSC), así como la creación de plataformas sociales que promuevan el compromiso cívico. De acuerdo con varias investigaciones, las prácticas de gobierno abierto proporcionan información para el diseño de políticas públicas, mejoran la provisión de los servicios, actúan como un enlace entre el gobierno y la ciudadanía mejorando la rendición de cuentas y fortalecen la democracia. </p>
+
+			<p>La igualdad de género se considera un aspecto transversal del programa de fortalecimiento de capacidades de las y los agentes de cambio en gobierno abierto y desarrollo sostenible, así como en el diseño de los planes de acción. Asimismo, y como parte del proceso de institucionalización de los espacios locales de diálogo y co creación, se buscará integrar Secretariados Técnicos plurales que privilegien una membresía equitativa entre hombres y mujeres, y se procurará la paridad en el marco del fellowship que se describirá más adelante.</p>
+			
+			<p>Por último, dada la orientación última del proyecto al cumplimiento de los Objetivos de Desarrollo Sostenible, se espera que el diseño y puesta en marcha de Planes de Acción de gobierno abierto robustos y con metas ambiciosas abonen a la atención efectiva de grupos tradicionalmente excluidos en el ámbito local como mujeres, jóvenes, indígenas, personas con discapacidad, entre otros. </p>
+			
+			
+			<h2>Producto #1. Programa de fortalecimiento de capacidades para agentes de cambio del gobierno abierto (fellowship)</h2>
+			<ul>
+				<li>
+					<h3>Actividad 1.1 Diseño de contenidos, temas y actividades del programa de fortalecimiento de capacidades</h3>
+					<p>Tomando como referencia otros programas de fellowship en temas de gobierno abierto y emprendimiento social, promovidos por organizaciones internacionales como la OEA y Ashoka, se pretende que cada generación de fellows reciba durante 8 meses formación presencial y a distancia, así como acompañamiento por parte de un coach o mentor; Dichos servicios estarán  orientados a incrementar sus conocimientos y competencias individuales, para impulsar su capacidad de agencia para promover uno o varios proyecto de incidencia en los espacios de interacción entre autoridades y ciudadanos creados por la iniciativa de Gobierno Co-creación desde lo Local.</p>
+					
+					<p>Por un lado, los fellows participarán en dos seminarios intensivos presenciales que tendrán una duración estimada de una semana en la que expertos y líderes sociales facilitarán a las y los participantes conocimientos y habilidades estratégicas.</p>
+					
+					<p>El primer Seminario estará orientado a asegurar que los participantes adquieran capacidades relacionadas con gobierno abierto:</p>
+					<ul>
+						<li>Objetivos de Desarrollo Sostenible,  </li>
+						<li>resolución de conflictos multiactor, </li>
+						<li>enfoque territorial de intervención, </li>
+						<li>igualdad de género, 				 </li>
+						<li>legalidad, 							 </li>
+						<li>integridad, 						 </li>
+						<li>ética pública, 						 </li>
+						<li>innovación, 						 </li>
+						<li>entre otros. 						 </li>
+					</ul>
+					
+					<p>El segundo seminario se enfocará en desarrollar las capacidades de los participantes en planeación, gestión y evaluación de proyectos; así como en habilidades y herramientas para el lanzamiento de los proyectos, entre ellas habilidades de liderazgo, comunicación y gestión de grupos de interés. Cabe mencionar que los espacios de formación presencial, además de servir para la transferencia de conocimientos y competencias, serán críticos para la creación de redes de apoyo y colaboración que, aunado al sentido de pertenencia, serán de utilidad en la implementación de sus propuestas al generar alianzas para la concreción, réplica y escalamiento nacional de sus iniciativas locales.</p>
+					
+					<p>Como parte del programa y posterior a los 8 meses de preparación en capacidades, los fellows desarrollarán acciones de intervención benéficas para el fortalecimiento de los espacios de co-creación locales que existen en cada uno de los estados donde se encuentren e implementarán un proyecto específico (que da inicio de forma teórica durante los ocho primeros meses) que les permita aplicar sus capacidades adquiridas mediante un trabajo colaborativo con uno o más de sus compañeros fellows que genere utilidad o beneficio social a una escala nacional.
+					</p>
+					<p>Para lograr lo anterior, los fellows contarán con una herramienta clave; una plataforma de formación en línea, en donde tendrán acceso a contenidos teóricos (sesiones pregrabadas, lecturas, herramientas) y a una comunidad de aprendizaje virtual en la que -acompañados por un tutor y otros fellows- podrán ir aterrizando sus ideas en proyectos concretos. Además de monitorear avances y retos de los fellows y acompañarles para la superación de los mismos, el tutor también será clave para facilitar a los participantes de vínculos y relaciones que puedan ser de apoyo para la concreción de sus propuestas. Se espera que al final del programa los fellows presenten un proyecto específico a implementar en sus estados en el marco de la estrategia de co creación del INAI.</p>
+				</li>
+				<li>
+					<h3>Actividad 1.2 Identificación de agentes de cambio</h3>
+					<p>Se prevé la identificación (exhaustiva y por medio de una convocatoria abierta) y la participación de líderes sociales (organizaciones, académico/as, periodistas, entre otros) y de funcionarios/as gubernamentales clave que serán formados y que potencialmente pueden fungir como promotores/as y replicadores/as de los principios y prácticas de gobierno abierto, desarrollo sostenible e intervención territorial transmitidas durante el programa de formación. Un público clave al que se orienta este fellowship – aunque no el único – son las organizaciones y funcionarios/as que forman parte de los Secretariados Técnicos Locales (STL) de los ejercicios de gobierno abierto locales promovidos por el Instituto Nacional de Transparencia, Acceso a la Información y Protección de Datos Personales (INAI). </p>
+
+					<p>La selección de los y las participantes se realizará con base en criterios de género y representatividad. La participación en el fellowship no implica el establecimiento de una relación contractual con el PNUD o con el INAI, sino que se limita a la participación de los agentes en un programa financiado de vinculación y fortalecimiento de capacidades que permita potenciar el alcance de los ejercicios de gobierno abierto realizados a nivel subnacional. Uno de los requisitos para la admisión en el fellowship será el compromiso explícito de vinculación con los ejercicios de gobierno abierto en las entidades federativas. El proyecto contempla apoyar a los agentes para su inserción formalizada en los Secretariados Técnicos Tripartitas, y se dará seguimiento a su participación en estos espacios para asegurar la aplicación de las competencias adquiridas en el campo.</p>
+
+					<p>El reclutamiento de becarios se llevará a cabo a través de un proceso competitivo, en el que se invitará a aplicar a los y las líderes locales. El proceso de selección será realizado a través de un comité conformado por representantes de: INAI, PNUD Mexico, Gesoc, Gobierno Fácil y ProSociedad Hacer el Bien, A.C; y estará basado en los siguientes criterios:</p>
+					<ol>
+						<li>Se dará preferencia a los candidatos/as de los estados que ya sean participantes en la iniciativa del INAI (Gobierno abierto: Co-creación desde lo local). En 2017 se recibirán aplicaciones de Chihuahua, Morelos, Oaxaca, Coahuila y Nuevo León; mientras que en 2018 se recibirán aplicaciones de otros 10 estados. Por cada estado se seleccionarán 4 becarios/as, en total en 2017 el programa contará con 20 becarios/as mientras que en 2018 se ampliará a 40 becarios/as. </li>
+						<li>Edad mínima de 20 años. </li>
+						<li>Se dará preferencia a candidatos/as que cuenten con publicaciones académicas o experiencia en implementación de ejercicios de gobierno abierto promovidos por el INAI.</li>
+						<li>Se tomarán en cuenta criterios de género y diversidad, para asegurar una representación equitativa.	</li>
+					</ol>
+					<p>Es importante resaltar que los participantes del fellowship no recibirán un salario, el proyecto absorberá los costos de viaje cuando sea necesario. Por medio de este programa de formación voluntario y honorario, el proyecto busca reclutar líderes sociales ya comprometidos con los principios de gobierno abierto que apoyen la institucionalización de la iniciativa del INAI. Algunos de estos actores podrán ser parte.</p>
+				</li>
+				
+				<li>
+					<h3>Actividad 1.3 Puesta en marcha progresiva del programa de fortalecimiento de capacidades </h3>
+					<p>Durante los tres años de duración de la intervención se prevé la puesta en marcha progresiva del programa de fortalecimiento de capacidades. En el primer año de implementación se proyecta el piloteo de programa hasta en cinco entidades federativas, y en los dos años subsecuentes se prevé su ampliación y difusión a 15 estados del país. La formación se impartirá de forma presencial, aunque con apoyos tecnológicos y materiales en línea que permitan la réplica y sustentabilidad del proyecto. La estructura del programa será la misma para todos los grupos: seminarios presenciales, sesiones y tutoría online, y presentación de proyectos de gobierno abierto con una perspectiva de desarrollo sostenible.</p>
+				</li>
+				
+				<h2>Producto #2. Plataforma en línea de vinculación, aprendizaje continuo, intercambio de experiencias y acción coordinada de los agentes de cambio formados en el fellowship.</h2>
+				<ul>
+					<li>
+						<h3>Actividad 2.1 Diseño y programación de la plataforma electrónica de vinculación </h3>
+						<p>De la mano con el desarrollo del programa de fortalecimiento de capacidades se prevé el diseño y programación de un sitio en línea que – combinando características de una red social, de una plataforma de aprendizaje y de un tablero público y abierto de seguimiento de actividades– permita vincular a las y los agentes de cambio capacitados para el intercambio de experiencias locales, así como para el trabajo conjunto en proyectos puntuales de intervención territorial en el país.</p>
+					</li>
+					<li>
+						<h3>Actividad 2.2. Puesta en marcha de la plataforma de agentes de cambio en gobierno abierto y desarrollo sostenible</h3>
+						<p>Al concluirse con las actividades de fortalecimiento de capacidades de la primera cohorte de agentes de cambio local se prevé el lanzamiento de esta plataforma en línea que favorezca la vinculación dinámica y colaborativa, así como el aprendizaje continuo de los actores sociales y gubernamentales participantes en el fellowship. Las características de interfaz de aprendizaje estarán disponibles para los participantes al momento del comienzo de la primera generación del programa de fortalecimiento de capacidades. El resto de las características estarán funcionando al final de la primera generación (8 meses).</p>
+					</li>
+					<li>
+						<h3>Actividad 2.3. Identificación participativa e innovadora de retos nacionales por medio de la plataforma. </h3> 
+						<p>Además de vincular y permitir el intercambio de experiencias, se espera que a través de la plataforma las y los agentes de cambio local participen en ejercicios de identificación de retos y/o problemáticas de alcance nacional o regional que puedan ser atendidos bajo los esquemas de co creación y territorialidad impulsados desde el programa de fortalecimiento de capacidades. De tal suerte, los márgenes de actuación de las y los agentes de cambio trascenderían la escala de sus propias entidades federativas, para promover iniciativas innovadoras de mayor alcance. Desde el inicio hasta el fin del proyecto, se considera el acompañamiento permanente de PNUD, INAI, GESOC, Gobierno Fácil, Pro Sociedad así como del equipo que será contratado para el mismo para la identificación, selección y puesta en marcha de acciones que den respuesta a los retos nacionales.</p>
+					</li>					
+				</ul>
+				
+				
+				<h2>Producto #3. Fortalecimiento e institucionalización de espacios de diálogo y co-creación subnacional (Secretariados Técnicos)</h2>
+				<ul>
+					<li>
+						<h3>Actividad 3.1. Integración y fortalecimiento de los espacios de diálogo y co-creación local </h3>
+						<p>Derivado de la experiencia del proyecto de gobierno abierto local promovido por el INAI, se prevé el fortalecimiento de los Secretariados Técnicos Locales por medio de la incorporación en estos espacios de las y los agentes de cambio local formados a través del programa de fortalecimiento de capacidades descrito previamente. En aquellas entidades federativas en las que aún no existe un ejercicio de gobierno abierto se promovería la conformación de un Secretariado Técnico Local en el que participen, aunque no exclusivamente, las y los agentes de cambio formados durante el fellowship, observando que la integración sea sensible a criterios de procedencia, género y vulnerabilidad social.</p>
+
+						<p>Al respecto, cabe señalar que como parte de los requisitos mínimos que el INAI establece a los estados que participan de la iniciativa, se encuentra la obligatoriedad de incorporar a nuevos actores en el proceso de deliberación y toma de decisiones al interior de los Secretariados Técnicos, por lo que la inserción de aquellos fellows que no participen (hasta ese momento) de las acciones desarrolladas por los Secretariados Técnicos Locales será promovida por el propio INAI.</p>
+	
+						<p>De los estados seleccionados para el primer año Coahuila, Nuevo León, Oaxaca, Morelos y Sonora ya cuentan con un Secretariado Técnico Local conformado por varios representantes de gobierno, sociedad civil y del órgano garante de acceso a la información. Mientras que Chihuahua cuenta con un grupo promotor del ejercicio de gobierno abierto, conformado por un representante de cada grupo. </p>
+					</li>
+				</ul>
+				
+				<h2>Producto #4. Planes de Acción de gobierno abierto locales robustos, diseñados a través de espacios institucionalizados de diálogo y co creación; y que se orientan a la resolución integral de problemas de desarrollo y corrupción</h2>
+				<p>Como parte del proyecto se pretende realizar un trabajo de acompañamiento y seguimiento continuo de las actividades realizadas por los Secretariados Técnicos Locales durante el diseño e implementación de sus Planes de Acción de gobierno abierto. Esta labor tiene como intención asegurar la materialización de Planes de Acción que contengan compromisos relevantes, medibles y orientados a la atención integral de problemas de desarrollo y corrupción. En esta línea, se aplicarán metodologías (como las de diálogo multiactor, glosa ciudadana, entre otras) que permitan concretar los principios de transparencia y participación que fomenten la confianza entre el gobierno y las ciudadanías locales. Se impulsará además que los planes de acción incluyan compromisos y metas de impacto diferenciado para hombres y mujeres.</p>
+				<ul>
+					<li>
+						<h3>Actividad 4.2. Desarrollo de indicadores y de esquemas de evaluación local en materia de gobierno abierto y desarrollo sostenible </h3>
+						<p>En paralelo al trabajo realizado por los Secretariados Técnicos Locales de gobierno abierto fortalecidos, una actividad clave para esta dimensión del proyecto es el diseño de sistemas de indicadores de alcance local que – retomando la experiencia internacional y nacional – se orienten a la medición, monitoreo y evaluación constante de los resultados alcanzados en materia de desarrollo sostenible y combate a la corrupción, así como en la construcción de una nueva cultura de diálogo gobierno-sociedad en cada una de las entidades federativas incorporadas al proyecto. GESOC, se encargará del desarrollo del esquema de seguimiento y evaluación para el proyecto en apego  a  estándares  de  buenas prácticas internacionales en la materia.</p>
+					</li>
+				</ul>
+				
+			</div>
+			<div class="col-sm-3">
+				<p><a href="{{url('programa-gobierno-abierto/alcance')}}" class="btn gde block">Alcance del programa</a> 
+				<a href="#" class="btn gde download block">Conoce al equipo</a>
+				<a href="{{url('convocatoria')}}" class="btn gde process block">Ver la Convocatoria</a>
+				</p>
+
+			</div>
+
+		</div>
+		
+		
 	</div>
 </div>
 @endsection
