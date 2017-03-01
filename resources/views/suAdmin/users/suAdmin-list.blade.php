3c263df0d09c6f6cf7b3300fa64ea76fb9cc4385
CarlosOsorio <carlos.aosorio89@gmail.com>
2017-03-01 14:09:39 -0600

    adds table super admin

--- resources/views/suAdmin/users/suAdmin-list.blade.php
+++ resources/views/suAdmin/users/suAdmin-list.blade.php
@@ -1,4 +1,20 @@
-
-@foreach($suAdmins as $user)
-<p>{{$user->name}}</p>
-@endforeach
+<table class="table">
+  <thead>
+    <tr>
+      <th>Nombre</th>
+      <th>Email</th>
+      <th>Acciones</th>
+    </tr>
+  </thead>
+  <tbody>
+    @foreach ($suAdmins as $user)
+      <tr>
+        <td>{{$user->name}}</td>
+        <td>{{$user->email}}</td>
+        <td>
+          <a href="{{ url('sa/dashboard/super-administradores/ver/' . $user->id) }}" class="btn xs view">Ver</a>
+          <a href ="{{ url('sa/dashboard/super-administradores/eliminar' . $user->id) }}"  id ="{{$user->id}}" class="btn xs danger" onclick="return confirm('¿Estás seguro?');">Eliminar</a></td>
+    </tr>
+    @endforeach
+  </tbody>
+</table>
