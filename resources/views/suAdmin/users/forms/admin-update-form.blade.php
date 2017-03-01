27f3d2cc5f9b9f66f955cf242e1dc3f95ac2fef1
CarlosOsorio <carlos.aosorio89@gmail.com>
2017-03-01 14:30:21 -0600

    changes views

--- resources/views/suAdmin/users/forms/admin-update-form.blade.php
+++ resources/views/suAdmin/users/forms/admin-update-form.blade.php
@@ -1,37 +1,51 @@
 {!! Form::model($admin,['url' => "sa/dashboard/administradores/editar/{$admin->id}", "class" => "form-horizontal"]) !!}
-
-<p>
-  <label>Nombre</label>
-  {{Form::text('name', null, ["class" => "form-control"])}}
-  @if($errors->has('name'))
-    <strong>{{$errors->first('name')}}</strong>
-  @endif
-</p>
-
-<p>
-  <label>Correo</label>
-  {{Form::text('email', null, ["class" => "form-control"])}}
-  @if($errors->has('email'))
-    <strong>{{$errors->first('email')}}</strong>
-  @endif
-</p>
-
-<p>
-  <label>Contraseña</label>
-  {{Form::password('password', ['class' => 'form-control'])}}
-  @if($errors->has('password'))
-    <strong>{{$errors->first('password')}}</strong>
-  @endif
-</p>
-
-<p>
-  <label>Confirmar Contraseña</label>
-  {{Form::password('password-confirm', ['class' => 'form-control'])}}
-  @if($errors->has('password-confirm'))
-    <strong>{{$errors->first('password-confirm')}}</strong>
-  @endif
-</p>
-
-<p>{{Form::submit('Actualizar administrador', ['class' => 'btn'])}}</p>
-
+<div class="row">
+  <div class="col-sm-12">
+    <p>
+      <label>Nombre</label>
+      {{Form::text('name', null, ["class" => "form-control"])}}
+      @if($errors->has('name'))
+      <strong>{{$errors->first('name')}}</strong>
+      @endif
+    </p>
+  </div>
+</div>
+<div class="row">
+  <div class="col-sm-12">
+    <p>
+      <label>Correo</label>
+      {{Form::text('email', null, ["class" => "form-control"])}}
+      @if($errors->has('email'))
+      <strong>{{$errors->first('email')}}</strong>
+      @endif
+    </p>
+  </div>
+</div>
+<div class="row">
+  <div class="col-sm-12">
+    <p>
+      <label>Contraseña</label>
+      {{Form::password('password', ['class' => 'form-control'])}}
+      @if($errors->has('password'))
+      <strong>{{$errors->first('password')}}</strong>
+      @endif
+    </p>
+  </div>
+</div>
+<div class="row">
+  <div class="col-sm-12">
+    <p>
+      <label>Confirmar Contraseña</label>
+      {{Form::password('password-confirm', ['class' => 'form-control'])}}
+      @if($errors->has('password-confirm'))
+      <strong>{{$errors->first('password-confirm')}}</strong>
+      @endif
+    </p>
+  </div>
+</div>
+<div class="row">
+  <div class="col-sm-12">
+    <p>{{Form::submit('Actualizar administrador', ['class' => 'btn'])}}</p>
+  </div>
+</div>
 {!! Form::close() !!}
