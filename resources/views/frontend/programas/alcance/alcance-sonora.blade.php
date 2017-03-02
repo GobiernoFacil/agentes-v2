a8090ab6fecd3a8d4909f8acec8198e9a6b70863
hugovom <hugo.om@gmail.com>
2017-03-01 18:07:09 -0600

    add sonora

--- resources/views/frontend/programas/alcance/alcance-sonora.blade.php
+++ resources/views/frontend/programas/alcance/alcance-sonora.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.frontend.master')
 @section('title', 'Sonora, ejercicio local de Gobierno Abierto')
-@section('description', 'Sonora')
+@section('description', 'El 20 de junio de 2016, el estado de Sonora se integró formalmente a los Ejercicios Locales de Gobierno Abierto a través de la firma de la Declaración Conjunta para la Implementación de las Acciones para un Gobierno Abierto')
 @section('body_class', 'programa alcance sonora')
 @section('canonical', url('programa-gobierno-abierto/alcance/sonora') )
 @section('breadcrumb', 'layouts.frontend.breadcrumb.bread_programa')
@@ -10,12 +10,37 @@
 	<div class="col-sm-10 col-sm-offset-1">
 		<h1><strong>Sonora</strong>, ejercicio local de gobierno abierto</h1>
 		<h2>¿Quiénes conforman el ejercicio?</h2>
+		<p>El Secretariado Técnico Local (STL), está conformado por: </p>
+		<ul>
+			<li>
+				<h3>1 representante de la <strong>Sociedad Civil</strong></h3>				 
+				<ol>
+					<li>Sonora Ciudadana A.C.</li>
+				</ol>
+			</li>
+			<li><h3>1 representante del Gobierno								 </h3>
+				<ol>
+					<li>Secretaría de la Contraloría General del Estado de Sonora</li>
+				</ol>
+			</li>
+			<li><h3>1 representante del Órgano Garante de Acceso a la Información</h3>
+				<ol>
+					<li>Instituto Sonorense de Transparencia, Acceso a la Información Pública y Protección de Datos Personales</li>
+				</ol>
+			</li>
+		</ul>
 		
-		
+		<h3>Facilitador:</h3>
+		<p>Dr. Víctor Samuel Peña Mancillas</p>
 		<!---contexto-->
 		<h2>Contexto</h2>
+			<p>El 20 de junio de 2016, el estado de Sonora se integró formalmente a los Ejercicios Locales de Gobierno Abierto a través de la firma de la <strong>Declaración Conjunta para la Implementación de las Acciones para un Gobierno Abierto</strong>. </p>
+			<p>En esta Declaración, firmada por un representante del gobierno del Estado, un representante de sociedad civil y el Organismo Garante de Acceso a la información, Sonora se comprometió a impulsar esquemas de apertura gubernamental y a la promoción de una agenda compartida entre sociedad y gobierno en la búsqueda de soluciones colaborativas a problemas públicos identificados, así como a instituir un Secretariado Técnico Local como mecanismo responsable de coordinar, incorporar y orientar los intereses de los actores que participen en el ejercicio de Gobierno Abierto Local. </p>
+			<p>El 20 y 21 de septiembre de 2016, se llevaron a cabo en Sonora los <strong>“Foros de Gobierno Abierto, Co Creación Desde lo Local”</strong> impulsados por el grupo promotor de gobierno abierto en la entidad. Es de destacar que dichos foros se llevaron a cabo en tres ciudades: Ciudad Obregón, Hermosillo y Nogales. En estas sesiones de sensibilización intervinieron integrantes del Secretariado Técnico Tripartita a nivel nacional y participaron cerca de 600 personas. </p>
+			<p>Posterior al proceso de sensibilización, el día 25 de noviembre de 2016 se instaló formalmente el Secretariado Técnico Local y en cuya acta quedó plasmada la firma de la Gobernadora Constitucional del Estado de Sonora, la Comisionada Presidente del Instituto Sonorense de Transparencia, Acceso a la Información Pública y Protección de Datos Personales y la Directora Ejecutiva de Sonora Ciudadana, A.C. </p>
 		
 		<h2>Estatus</h2>
+		<p>Reuniones preparatorias para iniciar la construcción del Plan de Acción Local. </p>
 
 	</div>
 </div>
