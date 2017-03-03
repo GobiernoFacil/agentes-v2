4037604f772bab3fbac47d930fe1ef7c570188c0
CarlosOsorio <carlos.aosorio89@gmail.com>
2017-03-03 13:41:26 -0600

    adds evaluation form

--- resources/views/admin/aspirants/form/evaluation-form.blade.php
+++ resources/views/admin/aspirants/form/evaluation-form.blade.php
@@ -31,7 +31,10 @@
   <div class="col-sm-12">
     <p>
       <label>¿El aspirante acredita experiencia en proyectos, investigaciones o intervenciones relacionadas con los componentes de Gobierno Abierto (transparencia y participación)?</label>
-      {{Form::checkbox('experience','Sí', ['class' => 'form-control'])}}
+      <label>Sí</label>
+      {{Form::checkbox('experience[]','1', null,['class' => 'form-control'])}}
+      <label>No</label>
+      {{Form::checkbox('experience[]','0', null,['class' => 'form-control'])}}
       @if($errors->has('experience'))
       <strong>{{$errors->first('experience')}}</strong>
       @endif
@@ -42,7 +45,18 @@
   <div class="col-sm-12">
     <p>
       <label>En una escala de 0 a 10, donde cero es nada relevante y diez es muy relevante ¿qué tan relevante considera la experiencia del aspirante en el desarrollo de proyectos, investigaciones o intervenciones relacionadas con los componentes de Gobierno Abierto? </label>
-      {{Form::checkbox('relevant','no',['class' => 'form-control'])}}
+      <label>0 1 2 3 4 5 6 7 8 9 10</label>
+      {{Form::checkbox('relevant[]','0', null,['class' => 'form-control'])}}
+      {{Form::checkbox('relevant[]','1', null,['class' => 'form-control'])}}
+      {{Form::checkbox('relevant[]','2', null,['class' => 'form-control'])}}
+      {{Form::checkbox('relevant[]','3', null,['class' => 'form-control'])}}
+      {{Form::checkbox('relevant[]','4', null,['class' => 'form-control'])}}
+      {{Form::checkbox('relevant[]','5', null,['class' => 'form-control'])}}
+      {{Form::checkbox('relevant[]','6', null,['class' => 'form-control'])}}
+      {{Form::checkbox('relevant[]','7', null,['class' => 'form-control'])}}
+      {{Form::checkbox('relevant[]','8', null,['class' => 'form-control'])}}
+      {{Form::checkbox('relevant[]','9', null,['class' => 'form-control'])}}
+      {{Form::checkbox('relevant[]','10', null,['class' => 'form-control'])}}
       @if($errors->has('relevant'))
       <strong>{{$errors->first('relevant')}}</strong>
       @endif
@@ -66,9 +80,12 @@
   <div class="col-sm-12">
     <p>
       <label>¿El aspirante acredita experiencia en proyectos, investigaciones o intervenciones relacionadas con temas relacionados con el desarrollo sostenible?</label>
-      {{Form::checkbox('relevant','no',['class' => 'form-control'])}}
-      @if($errors->has('relevant'))
-      <strong>{{$errors->first('relevant')}}</strong>
+      <label>Sí</label>
+      {{Form::checkbox('acredita[]','1', null,['class' => 'form-control'])}}
+      <label>No</label>
+      {{Form::checkbox('acredita[]','0', null,['class' => 'form-control'])}}
+      @if($errors->has('acredita'))
+      <strong>{{$errors->first('acredita')}}</strong>
       @endif
     </p>
   </div>
@@ -78,9 +95,20 @@
   <div class="col-sm-12">
     <p>
       <label>En una escala de 0 a 10, donde cero es nada relevante y diez es muy relevante ¿qué tan relevante considera la experiencia del aspirante en el desarrollo de proyectos, investigaciones o intervenciones relacionadas con el desarrollo sostenible? </label>
-      {{Form::checkbox('relevant','no',['class' => 'form-control'])}}
-      @if($errors->has('relevant'))
-      <strong>{{$errors->first('relevant')}}</strong>
+      <label>0 1 2 3 4 5 6 7 8 9 10</label>
+      {{Form::checkbox('experience2[]','0', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','1', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','2', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','3', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','4', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','5', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','6', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','7', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','8', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','9', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','10', null,['class' => 'form-control'])}}
+      @if($errors->has('experience2'))
+      <strong>{{$errors->first('experience2')}}</strong>
       @endif
     </p>
   </div>
@@ -99,7 +127,7 @@
 
 <div class="row">
   <div class="col-sm-12">
-    <h2>Valoración de ensayo</h2>}
+    <h2>Valoración de ensayo</h2>
     <p>En una escala de 0 a 10, donde 0 es nada de acuerdo y 10 es muy de acuerdo, evalúe las siguientes afirmaciones con base en el análisis del ensayo y el video enviado por el aspirante</p>
   </div>
 </div>
@@ -108,7 +136,18 @@
   <div class="col-sm-12">
     <p>
       <label>El aspirante expresa con claridad las razones para participar en el programa</label>
-      {{Form::checkbox('relevant','no',['class' => 'form-control'])}}
+      <label>0 1 2 3 4 5 6 7 8 9 10</label>
+      {{Form::checkbox('experience2[]','0', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','1', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','2', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','3', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','4', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','5', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','6', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','7', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','8', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','9', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','10', null,['class' => 'form-control'])}}
       @if($errors->has('relevant'))
       <strong>{{$errors->first('relevant')}}</strong>
       @endif
@@ -120,7 +159,18 @@
   <div class="col-sm-12">
     <p>
       <label>El aspirante expresa con claridad las aportaciones que puede brindar a la agenda de gobierno abierto y desarrollo sostenible en su entidad</label>
-      {{Form::checkbox('relevant','no',['class' => 'form-control'])}}
+      <label>0 1 2 3 4 5 6 7 8 9 10</label>
+      {{Form::checkbox('experience2[]','0', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','1', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','2', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','3', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','4', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','5', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','6', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','7', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','8', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','9', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','10', null,['class' => 'form-control'])}}
       @if($errors->has('relevant'))
       <strong>{{$errors->first('relevant')}}</strong>
       @endif
@@ -132,7 +182,18 @@
   <div class="col-sm-12">
     <p>
       <label>El aspirante es capaz de presentar ideas y argumentos escritos de forma eficaz</label>
-      {{Form::checkbox('relevant','no',['class' => 'form-control'])}}
+      <label>0 1 2 3 4 5 6 7 8 9 10</label>
+      {{Form::checkbox('experience2[]','0', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','1', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','2', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','3', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','4', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','5', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','6', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','7', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','8', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','9', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','10', null,['class' => 'form-control'])}}
       @if($errors->has('relevant'))
       <strong>{{$errors->first('relevant')}}</strong>
       @endif
@@ -144,7 +205,18 @@
   <div class="col-sm-12">
     <p>
       <label>El aspirante muestra un conocimiento amplio de los debates actuales sobre gobierno abierto y desarrollo sostenible</label>
-      {{Form::checkbox('relevant','no',['class' => 'form-control'])}}
+      <label>0 1 2 3 4 5 6 7 8 9 10</label>
+      {{Form::checkbox('experience2[]','0', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','1', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','2', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','3', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','4', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','5', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','6', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','7', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','8', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','9', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','10', null,['class' => 'form-control'])}}
       @if($errors->has('relevant'))
       <strong>{{$errors->first('relevant')}}</strong>
       @endif
@@ -156,7 +228,18 @@
   <div class="col-sm-12">
     <p>
       <label>El aspirante muestra conocimiento y sensibilidad sobre los principales desafíos de desarrollo que enfrenta su entidad federativa</label>
-      {{Form::checkbox('relevant','no',['class' => 'form-control'])}}
+      <label>0 1 2 3 4 5 6 7 8 9 10</label>
+      {{Form::checkbox('experience2[]','0', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','1', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','2', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','3', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','4', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','5', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','6', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','7', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','8', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','9', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','10', null,['class' => 'form-control'])}}
       @if($errors->has('relevant'))
       <strong>{{$errors->first('relevant')}}</strong>
       @endif
@@ -175,7 +258,18 @@
   <div class="col-sm-12">
     <p>
       <label>El aspirante presenta una idea de proyecto que integra adecuadamente las perspectivas de gobierno abierto y desarrollo sostenible</label>
-      {{Form::checkbox('relevant','no',['class' => 'form-control'])}}
+      <label>0 1 2 3 4 5 6 7 8 9 10</label>
+      {{Form::checkbox('experience2[]','0', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','1', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','2', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','3', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','4', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','5', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','6', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','7', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','8', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','9', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','10', null,['class' => 'form-control'])}}
       @if($errors->has('relevant'))
       <strong>{{$errors->first('relevant')}}</strong>
       @endif
@@ -187,7 +281,18 @@
   <div class="col-sm-12">
     <p>
       <label>La idea desarrollada por el aspirante cuenta con el potencial de transformar una problemática relevante de su entidad federativa</label>
-      {{Form::checkbox('relevant','no',['class' => 'form-control'])}}
+      <label>0 1 2 3 4 5 6 7 8 9 10</label>
+      {{Form::checkbox('experience2[]','0', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','1', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','2', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','3', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','4', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','5', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','6', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','7', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','8', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','9', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','10', null,['class' => 'form-control'])}}
       @if($errors->has('relevant'))
       <strong>{{$errors->first('relevant')}}</strong>
       @endif
@@ -199,7 +304,18 @@
   <div class="col-sm-12">
     <p>
       <label>La idea desarrollada por el aspirante es factible de ser implementada en el mediano plazo en el marco de los ejercicios locales de gobierno abierto</label>
-      {{Form::checkbox('relevant','no',['class' => 'form-control'])}}
+      <label>0 1 2 3 4 5 6 7 8 9 10</label>
+      {{Form::checkbox('experience2[]','0', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','1', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','2', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','3', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','4', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','5', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','6', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','7', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','8', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','9', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','10', null,['class' => 'form-control'])}}
       @if($errors->has('relevant'))
       <strong>{{$errors->first('relevant')}}</strong>
       @endif
@@ -211,7 +327,18 @@
   <div class="col-sm-12">
     <p>
       <label>El aspirante explica de forma clara y con soltura su idea frente a la cámara</label>
-      {{Form::checkbox('relevant','no',['class' => 'form-control'])}}
+      <label>0 1 2 3 4 5 6 7 8 9 10</label>
+      {{Form::checkbox('experience2[]','0', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','1', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','2', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','3', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','4', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','5', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','6', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','7', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','8', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','9', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','10', null,['class' => 'form-control'])}}
       @if($errors->has('relevant'))
       <strong>{{$errors->first('relevant')}}</strong>
       @endif
@@ -223,7 +350,18 @@
   <div class="col-sm-12">
     <p>
       <label>El aspirante logra persuadir con respecto a la importancia y relevancia de su idea</label>
-      {{Form::checkbox('relevant','no',['class' => 'form-control'])}}
+      <label>0 1 2 3 4 5 6 7 8 9 10</label>
+      {{Form::checkbox('experience2[]','0', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','1', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','2', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','3', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','4', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','5', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','6', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','7', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','8', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','9', null,['class' => 'form-control'])}}
+      {{Form::checkbox('experience2[]','10', null,['class' => 'form-control'])}}
       @if($errors->has('relevant'))
       <strong>{{$errors->first('relevant')}}</strong>
       @endif
