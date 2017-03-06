e720a9edd4550a0d0a99b297318e11a94b162a1a
hugovom <hugo.om@gmail.com>
2017-03-06 07:59:36 -0600

    update #24

--- resources/views/frontend/programas/que-es.blade.php
+++ resources/views/frontend/programas/que-es.blade.php
@@ -9,28 +9,36 @@
 <div class="row">
 	<div class="col-sm-10 col-sm-offset-1">
 		<h1>PROGRAMA DE FORMACIÓN DE <strong>AGENTES LOCALES DE CAMBIO</strong> EN <strong>GOBIERNO ABIERTO</strong> Y DESARROLLO SOSTENIBLE</h1>
-		<p>El objetivo del proyecto es contribuir al fortalecimiento de una buena gobernanza en México a partir de prácticas de gobierno abierto, participación ciudadana, transparencia y anticorrupción. Durante los tres años de implementación de las estrategias, se buscará contribuir al cumplimiento de la agenda de prevención de la corrupción del Estado mexicano, así como de los Objetivos de Desarrollo Sostenible (ODS). </p>
+		<p>El programa tiene como propósitos el fortalecimiento de capacidades, la vinculación y empoderamiento de una <strong>red de Agentes Locales de Cambio en Gobierno Abierto y Desarrollo Sostenible</strong> que promuevan la consolidación de acciones orientadas a fortalecer prácticas de transparencia y participación ciudadana a nivel local en México.  </p>
 		
-		<p>Para alcanzar el objetivo, el componente se implementará en apoyo a las estrategias de gobierno abierto del INAI, con la colaboración de tres organizaciones de la sociedad civil (Gesoc, Gobierno Fácil, y ProSociedad Hacer Bien el Bien, A.C), que cuentan con experiencia en la implementación de prácticas de gobierno abierto y rendición de cuentas.</p>
+		<p>Este programa ofrecerá una plataforma única para el aprendizaje, la reflexión, el intercambio de ideas y experiencias, así como para la realización de proyectos que puedan ponerse en marcha en el corto plazo, y que utilicen las perspectivas de gobierno abierto y desarrollo sostenible para transformar alguna realidad de su localidad o región.</p>
+		<p>Al concluir la etapa formativa, se esperaría que los egresados se integren y participen activamente en los trabajos que se realizan en su entidad federativa en el marco de los ejercicios locales de Gobierno Abierto promovidos por el INAI.</p>
 		<div class="row">
 			<div class="col-sm-9">
-			<h2>Objetivo específico </h2>
-			<p>El objetivo específico del componente es contribuir al ejercicio pleno de los derechos de las personas y abatir los factores de riesgo que propician la corrupción a nivel local, por medio de la puesta en marcha de esquemas innovadores de fortalecimiento de capacidades y de vinculación social que permitan el empoderamiento de agentes de cambio de gobierno abierto, así como del fortalecimiento de espacios de diálogo y co creación a nivel subnacional. Este objetivo se alcanzará a partir de la implementación de principios y prácticas de gobierno abierto que fortalezcan la participación de organizaciones de la sociedad civil (OSC), así como la creación de plataformas sociales que promuevan el compromiso cívico. De acuerdo con varias investigaciones, las prácticas de gobierno abierto proporcionan información para el diseño de políticas públicas, mejoran la provisión de los servicios, actúan como un enlace entre el gobierno y la ciudadanía mejorando la rendición de cuentas y fortalecen la democracia. </p>
+				<h2>Perfil de Egreso </h2>
+				<p>Al concluir la etapa formativa, se espera que los participantes:</p>
+				<ul>
+					<li>Cuenten con una formación sólida en temas de gobierno abierto y desarrollo sostenible que les permitirán incidir en debates y en el desarrollo de acciones innovadoras orientadas a resolver problemas puntuales de su localidad y región.				</li>
+					<li>Formen parte de una red de líderes y expertos a través de la cual podrán intercambiar ideas y desarrollar proyectos en conjunto que permitan enfrentar retos de desarrollo de alcance local y regional, desde una perspectiva de apertura gubernamental.</li>
+					<li>Sean capaces de elaborar proyectos colaborativos que generen soluciones concretas e innovadoras para atender problemas públicos con base en los principios de gobierno abierto. 																		</li>
+					<li>Participen activamente en los ejercicios locales de gobierno abierto promovidos por el INAI en colaboración con los Secretariados Técnicos de cada una de las entidades federativas consideradas en esta convocatoria. 									</li>
+					<li>Se conviertan en replicadores de las capacidades y habilidades adquiridas durante el programa en su contexto, y que permita la consolidación de la perspectiva de gobierno abierto a nivel local.</li>
+				</ul>
+			
+				<h2>Metodología y Modalidad</h2>
+				<p>Durante los siete meses de duración del programa, los contenidos y actividades se realizarán fundamentalmente en una plataforma en línea. Además, se tiene prevista la realización de dos rondas de seminarios y talleres presenciales – conducidos por expertos reconocidos en temas de Gobierno Abierto y Desarrollo Sostenible –a finales del mes de mayo y durante el mes de agosto en sedes por definir en la Ciudad de México y/o en algún estado de la República Mexicana. Para complementar su formación y desarrollar competencias blandas las y los participantes contarán con el acompañamiento de un mentor que contribuirá a la potencialización de los conocimientos y habilidades adquiridas a través de los demás mecanismos de formación del programa.</p>
+				<p>Además de adquirir capacidades y de vincularse a la red de Agentes Locales de Cambio, se espera que los participantes elaboren y presenten al final de los siete meses un proyecto en equipo, concreto y factible que, a través del uso de las herramientas adquiridas durante el programa, enfrente un reto local de desarrollo por medio de la realización de acciones que promuevan la transparencia, la participación, la rendición de cuentas, y la innovación cívica y tecnológica. Aquellos participantes que cumplan satisfactoriamente con todos los requisitos académicos al concluir el programa, recibirán una constancia de participación.</p>
+				
+				<h2>Evaluación y Reconocimiento</h2>
+				<p>El curso se desarrollará con la guía de los Coordinadores y mentores por medio de la Plataforma de Formación. Semanalmente se presentarán actividades y cuestionarios que serán calificados. Así mismo, el participante al cierre del curso presentará un proyecto en equipo, concreto y factible que, a través del uso de las herramientas adquiridas durante el programa, enfrente un reto local de desarrollo por medio de la realización de acciones que promuevan la transparencia, la participación, la rendición de cuentas, y la innovación cívica y tecnológica.</p>
+				<p>La evaluación final se integrará de la siguiente manera:</p>
+				<ul>
+					<li>Actividades y ensayos 30%</li>
+					<li>Cuestionarios de conocimientos 20%</li>
+					<li>Proyecto final 50%</li>
+				</ul>
+A quienes acrediten el 80% de los puntos a evaluar serán acreedores a un reconocimiento que acredite su participación en el programa.
 
-			<p>La igualdad de género se considera un aspecto transversal del programa de fortalecimiento de capacidades de las y los agentes de cambio en gobierno abierto y desarrollo sostenible, así como en el diseño de los planes de acción. Asimismo, y como parte del proceso de institucionalización de los espacios locales de diálogo y co creación, se buscará integrar Secretariados Técnicos plurales que privilegien una membresía equitativa entre hombres y mujeres, y se procurará la paridad en el marco del fellowship que se describirá más adelante.</p>
-			
-			<p>Por último, dada la orientación última del proyecto al cumplimiento de los Objetivos de Desarrollo Sostenible, se espera que el diseño y puesta en marcha de Planes de Acción de gobierno abierto robustos y con metas ambiciosas abonen a la atención efectiva de grupos tradicionalmente excluidos en el ámbito local como mujeres, jóvenes, indígenas, personas con discapacidad, entre otros. </p>
-			
-			
-			<h2>Producto #1. Programa de fortalecimiento de capacidades para agentes de cambio del gobierno abierto (fellowship)</h2>
-			
-			<h2>Producto #2. Plataforma en línea de vinculación, aprendizaje continuo, intercambio de experiencias y acción coordinada de los agentes de cambio formados en el fellowship.</h2>
-				
-			<h2>Producto #3. Fortalecimiento e institucionalización de espacios de diálogo y co-creación subnacional (Secretariados Técnicos)</h2>
-				
-			<h2>Producto #4. Planes de Acción de gobierno abierto locales robustos, diseñados a través de espacios institucionalizados de diálogo y co creación; y que se orientan a la resolución integral de problemas de desarrollo y corrupción</h2>
-				
-				
 			</div>
 			<div class="col-sm-3">
 				<h2></h2>
