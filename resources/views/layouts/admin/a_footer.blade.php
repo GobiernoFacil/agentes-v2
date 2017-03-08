ac5b0189aabba31c3e0b3365f6d770d928fde41e
hugovom <hugo.om@gmail.com>
2017-03-08 11:04:40 -0600

    add admin layouts

---
+++ resources/views/layouts/admin/a_footer.blade.php
@@ -0,0 +1,24 @@
+<footer>
+	<div class="container">
+			<div class="row">
+				<div class="col-sm-6">
+					<p>® 2017-2020 Gobierno Abierto desde lo local para el Desarrollo Sostenible.<br>
+					<a href="{{url('aviso-privacidad')}}">Aviso de Privacidad</a></p>
+				</div>
+				<div class="col-sm-6 right">
+					<p>Forjado Artesanalmente por:<br>
+					<a href="https://www.gobiernofacil.com" class="gobiernofacil">Gobierno Fácil</a></p>
+				</div>
+			</div>
+	</div>
+</footer>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-45473222-13', 'auto');
+  ga('send', 'pageview');
+
+</script>
