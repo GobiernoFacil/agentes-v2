6d48ed3f62f7f4cca2dc5b2af9e76f2a5365c480
hugovom <hugo.om@gmail.com>
2017-03-08 12:38:25 -0600

    update dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,1 +1,13 @@
-<h1>Admin Dashboard</h1>
+@extends('layouts.admin.a_master')
+@section('title', 'Tablero de Control')
+@section('description', 'Tablero de control de la plataforma del Programa de Formación de Agentes Locales de Cambio en Gobierno Abierto y Desarrollo Sostenible')
+@section('body_class', 'dashboard')
+
+@section('content')
+
+<h1>Tablero de control</h1>
+<p>En este tablero podrás consultar de manera inmediata si existe alguna aplicación de los aspirantes en el <strong>Programa 
+de Formación de Agentes Locales de Cambio en Gobierno Abierto y Desarrollo Sostenible</strong>. Así como los usuarios
+que tienen acceso. </p>
+
+@endsection
