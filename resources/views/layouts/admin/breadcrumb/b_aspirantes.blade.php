d39f86e759e188b58c6039d92660e1d48e297235
hugovom <hugo.om@gmail.com>
2017-03-13 16:46:33 -0600

    add breadcrumb

---
+++ resources/views/layouts/admin/breadcrumb/b_aspirantes.blade.php
@@ -0,0 +1,8 @@
+<ul>
+	<li>Estás en:</li>
+	<li><a href="{{url('dashboard')}}">Tablero</a></li>
+	@if ($__env->yieldContent('breadcrumb_type') =="aspirantes ver")
+	<li>Lista de Aspirantes</li>
+	@endif
+
+</ul>
