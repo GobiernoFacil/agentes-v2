fa0a62d1e316f4d011a9460e7803702d79366326
hugovom <hugo.om@gmail.com>
2017-03-14 12:07:19 -0600

    update breadcrumb

--- resources/views/layouts/admin/breadcrumb/b_aspirantes.blade.php
+++ resources/views/layouts/admin/breadcrumb/b_aspirantes.blade.php
@@ -4,8 +4,13 @@
 	@if ($__env->yieldContent('breadcrumb_type') =="aspirantes list")
 	<li>Lista de Aspirantes</li>
 	@endif
-	@if ($__env->yieldContent('breadcrumb_type') =="aspirantes ver")
+	@if ($__env->yieldContent('breadcrumb_type') =="aspirantes ver" || $__env->yieldContent('breadcrumb_type') =="aspirantes evaluar")
 	<li><a href="{{url('dashboard/aspirantes')}}">Lista de Aspirantes</a></li>
+	@endif
+	@if ($__env->yieldContent('breadcrumb_type') =="aspirantes ver")
 	<li>Ver Aspirante</li>
+	@endif
+	@if ($__env->yieldContent('breadcrumb_type') =="aspirantes evaluar")
+	<li>Evaluar Aspirante</li>
 	@endif
 </ul>
