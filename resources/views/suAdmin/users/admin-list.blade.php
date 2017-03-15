ee59795d3400a6b2c8708f04b8b2936c552171e2
hugovom <hugo.om@gmail.com>
2017-03-15 17:30:41 -0600

    add admin list

--- resources/views/suAdmin/users/admin-list.blade.php
+++ resources/views/suAdmin/users/admin-list.blade.php
@@ -1,22 +1,43 @@
-<table class="table">
-  <thead>
-    <tr>
-      <th>Nombre</th>
-      <th>Email</th>
-      <th>Acciones</th>
-    </tr>
-  </thead>
-  <tbody>
-    @foreach ($admins as $user)
-      <tr>
-        <td>{{$user->name}}</td>
-        <td>{{$user->email}}</td>
-        <td>
-          <a href="{{ url('sa/dashboard/administradores/ver/' . $user->id) }}" class="btn xs view">Ver</a>
-          <a href ="{{ url('sa/dashboard/administradores/eliminar' . $user->id) }}"  id ="{{$user->id}}" class="btn xs danger" onclick="return confirm('¿Estás seguro?');">Eliminar</a></td>
-    </tr>
-    @endforeach
-  </tbody>
-</table>
-
-{{ $admins->links() }}
+@extends('layouts.admin.a_master')
+@section('title', 'Lista de usuarios')
+@section('description', 'Lista de usuarios de la plataforma del Programa de Formación de Agentes Locales de Cambio en Gobierno Abierto y Desarrollo Sostenible')
+@section('body_class', 'users')
+@section('breadcrumb_type', 'users list')
+@section('breadcrumb', 'layouts.suAdmin.breadcrumbs.b_users')
+@section('content')
+<div class="row">
+	<div class="col-sm-12">
+		<h1>Lista de usuarios</h1>
+	</div>
+</div>
+<div class="box">
+	<div class="row">
+		<div class="col-sm-12">
+			<table class="table">
+			  <thead>
+			    <tr>
+			      <th>Nombre</th>
+			      <th>Email</th>
+			      <th>Institución</th>
+			      <th>Acciones</th>
+			    </tr>
+			  </thead>
+			  <tbody>
+			    @foreach ($admins as $admin)
+			      <tr>
+			        <td><h4>{{$admin->name}}</h4></td>
+			        <td>{{$admin->email}}</td>
+			        <td></td>
+			        <td>
+			          <a href="{{ url('sa/dashboard/administradores/ver/' . $admin->id) }}" class="btn xs view">Ver</a>
+			          <a href ="{{ url('sa/dashboard/administradores/eliminar' . $admin->id) }}"  id ="{{$admin->id}}" class="btn xs danger" onclick="return confirm('¿Estás seguro?');">Eliminar</a></td>
+			    </tr>
+			    @endforeach
+			  </tbody>
+			</table>
+			
+			{{ $admins->links() }}
+		</div>
+	</div>
+</div>
+@endsection
