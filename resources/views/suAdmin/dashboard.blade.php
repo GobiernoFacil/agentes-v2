5bfcafa98a1d3b31a482d94d8198bd8d2466f53a
hugovom <hugo.om@gmail.com>
2017-03-15 17:18:47 -0600

    update dashboard

--- resources/views/suAdmin/dashboard.blade.php
+++ resources/views/suAdmin/dashboard.blade.php
@@ -1,13 +1,19 @@
-<ul class="dropdown-menu" role="menu">
-    <li>
-        <a href="{{ url('/logout') }}"
-            onclick="event.preventDefault();
-                     document.getElementById('logout-form').submit();">
-            Logout
-        </a>
+@extends('layouts.admin.a_master')
+@section('title', 'Tablero de Control')
+@section('description', 'Tablero de control de la plataforma del Programa de Formación de Agentes Locales de Cambio en Gobierno Abierto y Desarrollo Sostenible')
+@section('body_class', 'dashboard')
 
-        <form id="logout-form" action="{{ url('/logout') }}" method="POST" style="display: none;">
-            {{ csrf_field() }}
-        </form>
-    </li>
-</ul>
+@section('content')
+<div class="row">
+	<div class="col-sm-10 col-sm-offset-1">
+		<h1>Tablero de control</h1>
+	</div>
+</div>
+<div class="box">
+	<div class="row">	
+		<div class="col-sm-10 col-sm-offset-1">
+			<p>En este tablero podrás consultar, crear y eliminar usuarios de la plataforma del <strong>Programa de Formación de Agentes Locales de Cambio en Gobierno Abierto y Desarrollo Sostenible</strong>.</p>
+		</div>
+	</div>
+</div>
+@endsection
