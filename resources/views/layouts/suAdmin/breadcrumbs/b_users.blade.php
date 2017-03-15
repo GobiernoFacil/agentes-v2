d07b3a6a34955e8d851b6080b9b244204ffb779e
hugovom <hugo.om@gmail.com>
2017-03-15 17:43:38 -0600

    update breadcrumb

--- resources/views/layouts/suAdmin/breadcrumbs/b_users.blade.php
+++ resources/views/layouts/suAdmin/breadcrumbs/b_users.blade.php
@@ -4,11 +4,14 @@
 	@if ($__env->yieldContent('breadcrumb_type') =="users list")
 	<li>Lista de usuarios</li>
 	@endif
-	@if ($__env->yieldContent('breadcrumb_type') =="users view")
+	@if ($__env->yieldContent('breadcrumb_type') =="users view" || $__env->yieldContent('breadcrumb_type') =="users edit")
 	<li><a href="{{ url('sa/dashboard/administradores') }}">Lista de usuarios</a></li>
 	@endif
 	@if ($__env->yieldContent('breadcrumb_type') =="users view")
 	<li>Ver usuario</li>
+	@endif
+	@if ($__env->yieldContent('breadcrumb_type') =="users edit")
+	<li>Editar usuario</li>
 	@endif
 
 </ul>
