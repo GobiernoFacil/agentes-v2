6cc828f47d493c4f21700ef05a31f569b1487a5d
hugovom <hugo.om@gmail.com>
2017-03-15 13:41:14 -0600

    add profile view

--- resources/views/admin/profile/profile-view.blade.php
+++ resources/views/admin/profile/profile-view.blade.php
@@ -1,9 +1,32 @@
 @extends('layouts.admin.a_master')
-@section('title', '')
-@section('description', '')
-@section('body_class', '')
+@section('title', 'Perfil')
+@section('description', 'Ver perfil')
+@section('body_class', 'profile')
 
 @section('content')
-
-{{$user->name}}
+<div class="row">
+	<div class="col-sm-12">
+		<h1>Tu Perfil</h1>
+	</div>
+</div>
+<div class="box">
+	<div class="row">
+		<div class="col-sm-5 col-sm-offset-1">
+			<ul class="profile list">
+				<li><h2>{{$user->name}}</h2></li>
+				<li><span>email</span>{{$user->email}}</li>
+				<li><span>Asociación</span> Gobierno Fácil</li>
+				<li><span>Tipo de usuario</span>{{$user->type == "admin" ? "Administrador" : ''}}</li>
+				<li><span>Fecha de creación</span>{{ date("d-m-Y, H:i", strtotime($user->created_at)) }} hrs.</li>
+				<li><span>Última actualización</span>{{ date("d-m-Y, H:i", strtotime($user->updated_at)) }} hrs.</li>
+			</ul>
+		</div>
+		<div class="col-sm-5">
+			<ul class="profile list">
+				<li class="right">
+				<a href="{{ url('dashboard/perfil/editar') }}" class="btn xs view">Editar Perfil</a></li>
+			</ul>
+		</div>
+	</div>
+</div>
 @endsection
