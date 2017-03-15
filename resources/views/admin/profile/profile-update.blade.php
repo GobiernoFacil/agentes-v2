1531c6177ac04a3ab01e31ddab7c79fb13a25660
hugovom <hugo.om@gmail.com>
2017-03-15 13:58:16 -0600

    add profile view

--- resources/views/admin/profile/profile-update.blade.php
+++ resources/views/admin/profile/profile-update.blade.php
@@ -1,1 +1,21 @@
-@include('admin.profile.forms.profile-update-form')
+@extends('layouts.admin.a_master')
+@section('title', 'Editar Perfil')
+@section('description', 'Edita tu perfil')
+@section('body_class', 'profile')
+@section('breadcrumb_type', 'profile edit')
+@section('breadcrumb', 'layouts.admin.breadcrumb.b_profile')
+
+@section('content')
+<div class="row">
+	<div class="col-sm-12">
+		<h1>Edita tu perfil</h1>
+	</div>
+</div>
+<div class="box">
+	<div class="row">
+		<div class="col-sm-8 col-sm-offset-2">
+			@include('admin.profile.forms.profile-update-form')
+		</div>
+	</div>
+</div>
+@endsection
