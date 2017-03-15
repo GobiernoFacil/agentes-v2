0b8b71265f240691f4d041053a918db20b20a8e0
hugovom <hugo.om@gmail.com>
2017-03-15 13:16:08 -0600

    fix #31

--- resources/views/layouts/admin/a_master.blade.php
+++ resources/views/layouts/admin/a_master.blade.php
@@ -29,7 +29,7 @@
 
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	
-	<link rel="icon" type="image/png" sizes="32x32" href="{{url('img/icon_ga.png')}}" />
+	<link rel="icon" type="image/png" sizes="32x32" href="{{url('img/favicon_admin.png')}}" />
 	
 
 	<!--css-custom-->
