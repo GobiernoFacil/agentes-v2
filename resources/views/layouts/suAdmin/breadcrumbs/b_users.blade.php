0728b16469c8416464be0594734237658070fce4
hugovom <hugo.om@gmail.com>
2017-03-15 17:34:01 -0600

    update breadcrumb

--- resources/views/layouts/suAdmin/breadcrumbs/b_users.blade.php
+++ resources/views/layouts/suAdmin/breadcrumbs/b_users.blade.php
@@ -4,7 +4,11 @@
 	@if ($__env->yieldContent('breadcrumb_type') =="users list")
 	<li>Lista de usuarios</li>
 	@endif
-
-
+	@if ($__env->yieldContent('breadcrumb_type') =="users view")
+	<li><a href="{{ url('sa/dashboard/administradores') }}">Lista de usuarios</a></li>
+	@endif
+	@if ($__env->yieldContent('breadcrumb_type') =="users view")
+	<li>Ver usuario</li>
+	@endif
 
 </ul>
