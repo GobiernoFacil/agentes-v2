a124cf81363255bb60084393fb76d6b551b1e1cc
hugovom <hugo.om@gmail.com>
2017-03-15 17:42:31 -0600

    add view

--- resources/views/suAdmin/users/admin-update.blade.php
+++ resources/views/suAdmin/users/admin-update.blade.php
@@ -1,1 +1,21 @@
-@include('suAdmin.users.forms.admin-update-form')
+@extends('layouts.admin.a_master')
+@section('title', 'Editar usuario')
+@section('description', 'Editar usuario de la plataforma del Programa de Formación de Agentes Locales de Cambio en Gobierno Abierto y Desarrollo Sostenible')
+@section('body_class', 'users')
+@section('breadcrumb_type', 'users edit')
+@section('breadcrumb', 'layouts.suAdmin.breadcrumbs.b_users')
+
+@section('content')
+<div class="row">
+	<div class="col-sm-12">
+		<h1>Editar usuario</h1>
+	</div>
+</div>
+<div class="box">
+	<div class="row">
+		<div class="col-sm-8 col-sm-offset-2">
+			@include('suAdmin.users.forms.admin-update-form')
+		</div>
+	</div>
+</div>
+@endsection
