57fe4e27c2a2aaf4063feafca3081e15819076e2
hugovom <hugo.om@gmail.com>
2017-03-16 13:57:37 -0600

    update mail message

--- resources/views/emails/new_user.blade.php
+++ resources/views/emails/new_user.blade.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Bienvenido al Programa de Formación de Agentes Locales de Cambio en Gobierno Abierto y Desarrollo Sostenible</title>
+  <title>Bienvenido a la plataforma del Programa de Formación de Agentes Locales de Cambio en Gobierno Abierto y Desarrollo Sostenible</title>
 </head>
 <body>
 <p>Bienvenido al Programa de Formación de Agentes Locales de Cambio en Gobierno Abierto y Desarrollo Sostenible</p><br>
-<p>Su información de usuario es:<br>
+<p>Tu información de usuario es:<br>
 Usuario: {{$user->email}}<br>
 Contraseña:{{$request->password}}<br>
 </p>
 
+<p>Ingresa a la plataforma en <a href="http://apertus.org.mx/login">apertus.org.mx/login</a>.
 <p><strong>Saludos.</strong></p>
 </body>
 </html>
