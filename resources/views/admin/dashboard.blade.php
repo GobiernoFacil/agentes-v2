908cc6506f53dae8b12478d73aaea49f3e242628
hugovom <hugo.om@gmail.com>
2017-03-16 12:23:04 -0600

    update dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -4,9 +4,60 @@
 @section('body_class', 'dashboard')
 
 @section('content')
+<div class="row">
+	<div class="col-sm-12">
+		<h1>Tablero de control</h1>
+	</div>
+</div>
 
-<h1>Tablero de control</h1>
-<p>En este tablero podrás consultar de si existen aspirantes a la convocatoria del <strong>Programa 
-de Formación de Agentes Locales de Cambio en Gobierno Abierto y Desarrollo Sostenible</strong>.  </p>
+<div class="row">
+	<div class="col-sm-3">
+		<div class="box blue">
+			<h3 class="sa_title">Aspirantes</h3>
+			<a class="count_link"  href="{{ url('dashboard/aspirantes') }}">{{$aspirants}}</a>
+			<a href="{{ url('sa/dashboard/super-administradores/agregar') }}" class="btn gde">Lista de Aspirantes</a>
+		</div>
+		<div class="box">
+			<p>En este tablero podrás consultar si existen aspirantes a la convocatoria del <strong>Programa de Formación de Agentes Locales de Cambio en Gobierno Abierto y Desarrollo Sostenible</strong>.</p>
+		</div>
+	</div>
+	<div class="col-sm-9">
+		<div class="row">
+			
+			<div class="col-sm-4 center">
+				<div class="box ">
+					<h3 class="sa_title">Chihuahua</h3>
+					<div class="count_link">{{$chihuahua_number}}</div>
+				</div>
+			</div>
+			<div class="col-sm-4 center">
+				<div class="box ">
+					<h3 class="sa_title">Morelos</h3>
+					<div class="count_link">{{$morelos_number}}</div>
+				</div>
+			</div>
+			<div class="col-sm-4 center">
+				<div class="box ">
+					<h3 class="sa_title">Nuevo León</h3>
+					<div class="count_link">{{$leon_number}}</div>
+				</div>
+			</div>
+			<div class="col-sm-4 center">
+				<div class="box ">
+					<h3 class="sa_title">Oaxaca</h3>
+					<div class="count_link">{{$oaxaca_number}}</div>
+				</div>
+			</div>
+			<div class="col-sm-4 center">
+				<div class="box ">
+					<h3 class="sa_title">Sonora</h3>
+					<div class="count_link">{{$sonora_number}}</div>
+				</div>
+			</div>
+			
+		</div>
+	</div>
+	
+</div>
 
 @endsection
