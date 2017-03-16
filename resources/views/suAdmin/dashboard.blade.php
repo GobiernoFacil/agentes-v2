af144462a31b8cc4f86d19192749487ec3f5acef
hugovom <hugo.om@gmail.com>
2017-03-15 22:48:31 -0600

    update dashboard

--- resources/views/suAdmin/dashboard.blade.php
+++ resources/views/suAdmin/dashboard.blade.php
@@ -5,13 +5,33 @@
 
 @section('content')
 <div class="row">
-	<div class="col-sm-10 col-sm-offset-1">
+	<div class="col-sm-12">
 		<h1>Tablero de control</h1>
 	</div>
 </div>
-<div class="box">
-	<div class="row">	
-		<div class="col-sm-10 col-sm-offset-1">
+
+
+<div class="row">
+	<div class="col-sm-9">
+		<div class="row">
+			<div class="col-sm-6 center">
+				<div class="box ">
+					<h3 class="sa_title">Super administradores</h3>
+					<a href="{{ url('sa/dashboard/super-administradores') }}" class="count_link">{{ $sum_suAdmin }}</a>
+					<a href="{{ url('sa/dashboard/super-administradores/agregar') }}" class="btn gde">Agregar superadministrador</a>
+				</div>
+			</div>
+			<div class="col-sm-6 center">
+				<div class="box">
+					<h3 class="sa_title">Administradores</h3>
+					<a href="{{ url('sa/dashboard/administradores') }}" class="count_link">{{ $sum_Admin }}</a>
+					<a href="{{ url('sa/dashboard/administradores/agregar') }}" class="btn gde">Agregar administrador</a>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="col-sm-3">
+		<div class="box">
 			<p>En este tablero podrás consultar, crear y eliminar usuarios de la plataforma del <strong>Programa de Formación de Agentes Locales de Cambio en Gobierno Abierto y Desarrollo Sostenible</strong>.</p>
 		</div>
 	</div>
