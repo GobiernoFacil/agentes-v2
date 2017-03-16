244d11386d5ad60652be3b83611acc12bcb1bf57
hugovom <hugo.om@gmail.com>
2017-03-16 10:52:39 -0600

    add profife update view

--- resources/views/suAdmin/profile/profile-update.blade.php
+++ resources/views/suAdmin/profile/profile-update.blade.php
@@ -1,1 +1,21 @@
-@include('suAdmin.profile.forms.profile-update-form')
+@extends('layouts.admin.a_master')
+@section('title', 'Editar tu Perfil')
+@section('description', 'Edita tu perfil')
+@section('body_class', 'profile')
+@section('breadcrumb_type', 'profile edit')
+@section('breadcrumb', 'layouts.suAdmin.breadcrumbs.b_profile')
+
+@section('content')
+<div class="row">
+	<div class="col-sm-12">
+		<h1>Edita tu perfil</h1>
+	</div>
+</div>
+<div class="box">
+	<div class="row">
+		<div class="col-sm-8 col-sm-offset-2">
+			@include('suAdmin.profile.forms.profile-update-form')
+		</div>
+	</div>
+</div>
+@endsection
