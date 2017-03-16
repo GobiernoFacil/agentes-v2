cb3eefc7e2f19bea276a606b742d444253fc64c6
CarlosOsorio <carlos.aosorio89@gmail.com>
2017-03-16 12:18:54 -0600

    adds send email users

---
+++ resources/views/emails/new_user.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Bienvenido al Programa de Formación de Agentes Locales de Cambio en Gobierno Abierto y Desarrollo Sostenible</title>
+</head>
+<body>
+<p>Bienvenido al Programa de Formación de Agentes Locales de Cambio en Gobierno Abierto y Desarrollo Sostenible</p><br>
+<p>Su información de usuario es:<br>
+Usuario: {{$user->email}}<br>
+Contraseña:{{$request->password}}<br>
+</p>
+
+<p><strong>Saludos.</strong></p>
+</body>
+</html>
