12e49413ecf9758d2ff33fd3be24bf01fe9940fc
hugovom <hugo.om@gmail.com>
2017-03-15 22:56:32 -0600

    add super admin list

--- resources/views/suAdmin/users/suAdmin-list.blade.php
+++ resources/views/suAdmin/users/suAdmin-list.blade.php
@@ -1,3 +1,11 @@
+@extends('layouts.admin.a_master')
+@section('title', 'Lista de usuarios')
+@section('description', 'Lista de usuarios de la plataforma del Programa de Formación de Agentes Locales de Cambio en Gobierno Abierto y Desarrollo Sostenible')
+@section('body_class', 'users')
+@section('breadcrumb_type', 'users sua list')
+@section('breadcrumb', 'layouts.suAdmin.breadcrumbs.b_users')
+@section('content')
+
 <table class="table">
   <thead>
     <tr>
@@ -7,13 +15,20 @@
     </tr>
   </thead>
   <tbody>
-    @foreach ($suAdmins as $user)
+	  <tr>
+		  <td><h3>{{$user->name}}</h3></td>
+		  <td>{{$user->email}}</td>
+		  <td>
+          <a href="{{ url('sa/dashboard/perfil') }}" class="btn xs view">Ver tu perfil</a>
+     </td>
+	  </tr>
+    @foreach ($suAdmins as $user_a)
       <tr>
-        <td>{{$user->name}}</td>
-        <td>{{$user->email}}</td>
+        <td><h3>{{$user_a->name}}</h3></td>
+        <td>{{$user_a->email}}</td>
         <td>
-          <a href="{{ url('sa/dashboard/super-administradores/ver/' . $user->id) }}" class="btn xs view">Ver</a>
-          <a href ="{{ url('sa/dashboard/super-administradores/eliminar' . $user->id) }}"  id ="{{$user->id}}" class="btn xs danger" onclick="return confirm('¿Estás seguro?');">Eliminar</a></td>
+          <a href="{{ url('sa/dashboard/super-administradores/ver/' . $user_a->id) }}" class="btn xs view">Ver</a>
+          <a href ="{{ url('sa/dashboard/super-administradores/eliminar' . $user_a->id) }}"  id ="{{$user_a->id}}" class="btn xs danger" onclick="return confirm('¿Estás seguro?');">Eliminar</a></td>
     </tr>
     @endforeach
   </tbody>
@@ -21,3 +36,4 @@
 
 
 {{ $suAdmins->links() }}
+@endsection
