7e9af6c6ddba232bc1c3c11e506a18acea8f0912
hugovom <hugo.om@gmail.com>
2017-03-16 11:19:19 -0600

    update breadcrumb

--- resources/views/layouts/suAdmin/breadcrumbs/b_suAdmins.blade.php
+++ resources/views/layouts/suAdmin/breadcrumbs/b_suAdmins.blade.php
@@ -2,19 +2,19 @@
 	<li>Estás en:</li>
 	<li><a href="{{url('sa/dashboard')}}">Tablero</a></li>
 	@if ($__env->yieldContent('breadcrumb_type') =="users sua list")
-	<li>Lista de usuarios</li>
+	<li>Super Administradores</li>
 	@endif
 	@if ($__env->yieldContent('breadcrumb_type') =="users sua view" || $__env->yieldContent('breadcrumb_type') =="users sua edit" || $__env->yieldContent('breadcrumb_type') =="users sua add")
-	<li><a href="{{ url('sa/dashboard/administradores') }}">Lista de usuarios</a></li>
+	<li><a href="{{ url('sa/dashboard/super-administradores') }}">Super Adminsitradores</a></li>
 	@endif
 	@if ($__env->yieldContent('breadcrumb_type') =="users sua view")
-	<li>Ver usuario</li>
+	<li>Ver super administrador</li>
 	@endif
 	@if ($__env->yieldContent('breadcrumb_type') =="users sua edit")
-	<li>Editar usuario</li>
+	<li>Editar super administrador</li>
 	@endif
 	@if ($__env->yieldContent('breadcrumb_type') =="users sua add")
-	<li>Agregar usuario</li>
+	<li>Agregar super administrador</li>
 	@endif
 
 </ul>
