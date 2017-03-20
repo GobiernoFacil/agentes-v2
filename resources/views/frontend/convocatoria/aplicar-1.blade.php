be1daab89352a151b5c572ada56660071da84ce1
hugovom <hugo.om@gmail.com>
2017-03-20 11:22:34 -0600

    fix #42

--- resources/views/frontend/convocatoria/aplicar-1.blade.php
+++ resources/views/frontend/convocatoria/aplicar-1.blade.php
@@ -16,14 +16,14 @@
 	  	@endif
 	</div>
 	<div class="col-sm-8 col-sm-offset-2">
-		<p>El plazo de aplicación se inicia el día <strong>13 de marzo y termina el 20 de abril de 2017 a las 15:00 horas</strong> (tiempo de la Ciudad de México). Para poder aplicar es necesario que radiques en alguna de las 5 entidades federativas participantes (<strong>Chihuahua, Morelos, Nuevo León, Oaxaca y Sonora.</strong>). Antes de aplicar, ten preparados los requisitos mencionados en las <a href="{{url('convocatoria')}}">bases de la convocatoria</a>:</p>
+		<p>El plazo de aplicación se inicia el día <strong>21 de marzo y termina el 28 de abril de 2017 a las 15:00 horas</strong> (tiempo de la Ciudad de México). Para poder aplicar es necesario que radiques en alguna de las 5 entidades federativas participantes (<strong>Chihuahua, Morelos, Nuevo León, Oaxaca y Sonora.</strong>). Antes de aplicar, ten preparados los requisitos mencionados en las <a href="{{url('convocatoria')}}">bases de la convocatoria</a>:</p>
 		<ol>
 			<li>Ensayo (no mayor a cinco cuartillas).</li>
 			<li>Video breve (enlace).</li>
 			<li>Perfil curricular actualizado.</li>
 			<li>Carta membretada.</li>
 			<li>Copia de comprobante de domicilio.</li>
-			<li>Descargar, leer, firmar (en caso de aceptar) y enviar el <a href="aviso-privacidad">Aviso de Privacidad</a> por medio del cual otorguen el consentimiento relativo al tratamiento de sus datos personales, disponible en <a href="{{url('archivos/ConsentimientoDatosPersonales.pdf')}}" download>{{url('archivos/ConsentimientoDatosPersonales.docx')}}</a>.</li>
+			<li>Descargar, leer, firmar (en caso de aceptar) y enviar el <a href="{{ url('aviso-privacidad')}}">Aviso de Privacidad</a> por medio del cual otorguen el consentimiento relativo al tratamiento de sus datos personales, disponible en <a href="{{url('archivos/ConsentimientoDatosPersonales.pdf')}}" download>{{url('archivos/ConsentimientoDatosPersonales.docx')}}</a>.</li>
 		</ol>
 		<h2>Paso 1 de 2</h2>
 		<p>Todos los campos son obligatorios.</p>
