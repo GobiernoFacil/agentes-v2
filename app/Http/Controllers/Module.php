b89054fa500e99ca9f26f28afdc797cc2d3a14f3
CarlosOsorio <carlos.aosorio89@gmail.com>
2017-04-03 11:42:22 -0500

    adds basic CRUD modules

--- app/Http/Controllers/Module.php
+++ app/Http/Controllers/Module.php
@@ -7,4 +7,92 @@ use Illuminate\Http\Request;
 class Module extends Controller
 {
     //
+
+          /**
+           * Búsqueda de módulo
+           *
+           * @return \Illuminate\Http\Response
+           */
+          public function search()
+          {
+              //
+          }
+
+          /**
+           * Muestra lista de módulos
+           *
+           * @return \Illuminate\Http\Response
+           */
+          public function index()
+          {
+              //
+          }
+
+          /**
+           * Agregar módulo
+           *
+           * @return \Illuminate\Http\Response
+           */
+          public function add()
+          {
+              //
+          }
+
+          /**
+           * Guarda nuevo módulo
+           *
+           * @param  \Illuminate\Http\Request  $request
+           * @return \Illuminate\Http\Response
+           */
+          public function save(Request $request)
+          {
+              //
+          }
+
+          /**
+           * Muestra módulo
+           *
+           * @param  int  $id
+           * @return \Illuminate\Http\Response
+           */
+          public function view($id)
+          {
+              //
+          }
+
+          /**
+           * Muestra contenido para actualizar un módulo
+           *
+           * @param  int  $id
+           * @return \Illuminate\Http\Response
+           */
+          public function edit($id)
+          {
+              //
+          }
+
+          /**
+           * Actualiza módulo
+           *
+           * @param  \Illuminate\Http\Request  $request
+           * @param  int  $id
+           * @return \Illuminate\Http\Response
+           */
+          public function update(Request $request, $id)
+          {
+              //
+          }
+
+          /**
+           * Deshabilita módulo
+           *
+           * @param  int  $id
+           * @return \Illuminate\Http\Response
+           */
+          public function delete($id)
+          {
+              //
+          }
+
+
 }
