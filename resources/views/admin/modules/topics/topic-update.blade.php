e7b1156aa0e21616c8238051b8969807ab459602
hugovom <hugo.om@gmail.com>
2017-04-20 16:03:34 -0600

    update view

--- resources/views/admin/modules/topics/topic-update.blade.php
+++ resources/views/admin/modules/topics/topic-update.blade.php
@@ -1,14 +1,14 @@
 @extends('layouts.admin.a_master')
-@section('title', 'Actualizar temática')
-@section('description', 'Actualizar  temática')
-@section('body_class', '')
-@section('breadcrumb_type', '')
-@section('breadcrumb', '')
+@section('title', 'Actualizar objetivos particulares de la sesión')
+@section('description', 'Actualizar  objetivos particulares de la sesión')
+@section('body_class', 'modulos session topic')
+@section('breadcrumb_type', 'module session topic edit')
+@section('breadcrumb', 'layouts.admin.breadcrumb.b_modules')
 
 @section('content')
 <div class="row">
   <div class="col-sm-12">
-    <h1>Actualizar temática</h1>
+    <h1>Actualizar Objetivos Particulares de la Sesión {{$session->order}}</h1>
   </div>
 </div>
 <div class="box">
