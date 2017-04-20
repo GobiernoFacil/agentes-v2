bf547f36c4ccabc9feba95fc2ca59f6e383094a1
hugovom <hugo.om@gmail.com>
2017-04-20 10:00:47 -0600

    update breadcrumb

--- resources/views/layouts/admin/breadcrumb/b_modules.blade.php
+++ resources/views/layouts/admin/breadcrumb/b_modules.blade.php
@@ -5,7 +5,7 @@
 	<li>Módulos</li>
 	@endif
 	
-	@if ($__env->yieldContent('breadcrumb_type') =="module add" || $__env->yieldContent('breadcrumb_type') =="module view")
+	@if ($__env->yieldContent('breadcrumb_type') =="module add" || $__env->yieldContent('breadcrumb_type') =="module view" || $__env->yieldContent('breadcrumb_type') =="module session view")
 	<li><a href="{{url('dashboard/modulos')}}">Módulos</a></li>
 	@endif
 	
@@ -22,5 +22,10 @@
 	@if ($__env->yieldContent('breadcrumb_type') =="module edit")
 	<li>Editar módulo</li>
 	@endif
-
+	
+	@if ($__env->yieldContent('breadcrumb_type') =="module session view")
+	<!-- sesión --->
+	<li><a href="{{ url('dashboard/modulos/ver/'.$session->module->id)}}">{{$session->module->title}}</a></li>	
+	<li>Ver sesión {{$session->order}}</li>
+	@endif
 </ul>
