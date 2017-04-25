9aee6d2d00492d56afe92d33e21b5b910cab650e
hugovom <hugo.om@gmail.com>
2017-04-25 17:19:40 -0500

    update list

--- resources/views/admin/modules/sessions/sessions-facilitators-list.blade.php
+++ resources/views/admin/modules/sessions/sessions-facilitators-list.blade.php
@@ -1,28 +1,23 @@
-<div class="box">
-	<div class="row">
-		<table class="table">
-			<thead>
-				<tr>
-					<th>Nombre</th>
-					<th>email</th>
-					<th>Institution</th>
-					<th>Acciones</th>
-				</tr>
-			</thead>
-			<tbody>
-				@foreach ($session->facilitators as $facilitator)
-					<tr>
-						<td><h4>{{$facilitator->user->name}}</h4></td>
-						<td>{{$facilitator->user->email}}</td>
-						<td>{{$facilitator->user->institution}}</td>
-						<td>
-							<a href='{{ url("dashboard/facilitadores/ver/" .$facilitator->user->id) }}' class="btn xs view">Ver</a>
-							<a href='{{ url("dashboard/sesiones/facilitadores/asignar/$session->id") }}' class="btn xs view">Remover</a>
-						 <!-- <a href ="{{ url('dashboard/requerimientos/eliminar' . $facilitator->id) }}"  id ="{{$facilitator->id}}" class="btn xs danger" onclick="return confirm('¿Estás seguro?');">Eliminar</a>-->
-						 </td>
-				</tr>
-				@endforeach
-			</tbody>
-		</table>
+<div class="row">
+	@foreach ($session->facilitators as $facilitator)
+	<div class="col-sm-6">
+		<div class="row">
+			<div class="col-sm-3">
+				@if($facilitator->user->image)
+				<img src='{{url("img/users/{$facilitator->user->image->name}")}}'>
+				@else
+				Sin imagen
+				@endif
+			</div>
+			<div class="col-sm-9">
+				<h4>{{$facilitator->user->name}}</h4>
+				<p>{{$facilitator->user->institution}}<br>
+					<a href='{{ url("dashboard/facilitadores/ver/" .$facilitator->user->id) }}' class="btn xs view">Ver</a>
+					<a href='{{ url("dashboard/sesiones/facilitadores/asignar/$session->id") }}' class="btn xs danger">Remover</a>
+					<!-- <a href ="{{ url('dashboard/requerimientos/eliminar' . $facilitator->id) }}"  id ="{{$facilitator->id}}" class="btn xs danger" onclick="return confirm('¿Estás seguro?');">Eliminar</a>-->
+				</p>
+			</div>
+		</div>
 	</div>
+	@endforeach
 </div>
