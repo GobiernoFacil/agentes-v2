51b6861c74f11289fe804b20f0cabceb0e6cb4bf
hugovom <hugo.om@gmail.com>
2017-04-25 10:31:03 -0500

    add facilitator view

--- resources/views/admin/users/facilitator-view.blade.php
+++ resources/views/admin/users/facilitator-view.blade.php
@@ -1,8 +1,9 @@
 @extends('layouts.admin.a_master')
 @section('title', 'Ver facilitador')
 @section('description', 'Ver facilitador de la plataforma del Programa de Formación de Agentes Locales de Cambio en Gobierno Abierto y Desarrollo Sostenible')
-@section('body_class', '')
-@section('breadcrumb_type', '')
+@section('body_class', 'facilitadores')
+@section('breadcrumb_type', 'facilitadores view')
+@section('breadcrumb', 'layouts.admin.breadcrumb.b_facilitadores')
 
 @section('content')
 <div class="row">
@@ -11,33 +12,59 @@
 	</div>
 </div>
 <div class="box">
-	<div class="row">
-		<div class="col-sm-5 col-sm-offset-1">
-			<ul class="profile list">
-				<li><h2>{{$facilitator->name}}</h2></li>
-				<li><span>email</span>{{$facilitator->email}}</li>
-				<li><span>Asociación</span> {{$facilitator->institution}}</li>
-				<li><span>Grado de estudios</span>{{$facilitator->FacilitatorData->degree}}</li>
-				<li><span>Sitio Web</span>{{$facilitator->FacilitatorData->web ? $facilitator->FacilitatorData->web : "Sin información" }}</li>
-				<li><span>Twitter</span>{{$facilitator->FacilitatorData->twitter ? $facilitator->FacilitatorData->twitter : "Sin información" }}</li>
-				<li><span>Facebook</span>{{$facilitator->FacilitatorData->facebook ? $facilitator->FacilitatorData->facebook : "Sin información" }}</li>
-				<li><span>Linkedin</span>{{$facilitator->FacilitatorData->linkedin ? $facilitator->FacilitatorData->linkedin : "Sin información" }}</li>
-				<li><span>Otra</span>{{$facilitator->FacilitatorData->other ? $facilitator->FacilitatorData->other : "Sin información" }}</li>
-				<li><span>Semblanza</span>{{$facilitator->FacilitatorData->semblance ? $facilitator->FacilitatorData->semblance : "Sin información" }}</li>
-				<li><span>Fecha de creación</span>{{ date("d-m-Y, H:i", strtotime($facilitator->created_at)) }} hrs.</li>
-				<li><span>Última actualización</span>{{ date("d-m-Y, H:i", strtotime($facilitator->updated_at)) }} hrs.</li>
-				@if($facilitator->image)
-				<li><span>Foto</span><img src='{{url("img/users/{$facilitator->image->name}")}}'></li>
-				@else
-				<li><span>Foto</span> Sin imagen </li>
-				@endif
-			</ul>
-		</div>
-		<div class="col-sm-5">
+		<div class="row">
+
+	<div class="col-sm-12">
 			<ul class="profile list">
 				<li class="right">
 				<a href="{{ url('dashboard/facilitadores/editar/' . $facilitator->id ) }}" class="btn xs view">Editar Facilitador</a></li>
 			</ul>
+		</div>
+
+		<div class="col-sm-10 col-sm-offset-1 center">
+			<p class="">
+				@if($facilitator->image)
+				<img src='{{url("img/users/{$facilitator->image->name}")}}'>
+				@else
+				Sin imagen
+				@endif
+			</p>
+			<h2 >{{$facilitator->name}}</h2>
+			
+			<h3><strong>{{$facilitator->institution}}</strong></h3>
+			<div class="divider"></div>
+			<ul class="profile list row">
+				<li class="col-sm-4"><span>Grado de estudios</span>{{$facilitator->FacilitatorData->degree}}</li>
+				<li class="col-sm-4"><span>email</span>{{$facilitator->email}}</li>
+				<li class="col-sm-4"><span>Sitio Web</span>{{$facilitator->FacilitatorData->web ? $facilitator->FacilitatorData->web : "Sin información" }}</li>	
+			
+			</ul>
+			<p>
+			@if($facilitator->FacilitatorData->twitter)
+			<a href="#" class="facilitador_i tw"></a> 	
+			@endif
+			@if($facilitator->FacilitatorData->facebook)
+			<a href="#" class="facilitador_i fb"></a> 	
+			@endif
+			@if($facilitator->FacilitatorData->linkedin)
+			<a href="#" class="facilitador_i lk"></a> 	
+			@endif
+			@if($facilitator->FacilitatorData->other)
+			{{$facilitator->FacilitatorData->other}}
+			@endif
+			</p>
+			
+			<h3>Semblanza</h3>
+			<p>{{$facilitator->FacilitatorData->semblance ? $facilitator->FacilitatorData->semblance : "Sin información" }}</p>
+			<div class="divider"></div>
+			<div class="row notes">
+				<div class="col-sm-6">
+				<p>Fecha de creación: {{ date("d-m-Y, H:i", strtotime($facilitator->created_at)) }} hrs.</p>
+				</div>
+				<div class="col-sm-6">
+				<p>Última actualización: {{ date("d-m-Y, H:i", strtotime($facilitator->updated_at)) }} hrs.</p>
+				</div>
+			</div>
 		</div>
 	</div>
 </div>
