9e742f3f6a0028a0eae2c1e439c1f9778e57f33d
hugovom <hugo.om@gmail.com>
2017-04-27 17:36:36 -0500

    update activities list

--- resources/views/admin/modules/sessions/sessions-activities-list.blade.php
+++ resources/views/admin/modules/sessions/sessions-activities-list.blade.php
@@ -1,29 +1,19 @@
-<div class="box">
-	<div class="row">
-		<table class="table">
-			<thead>
-				<tr>
-					<th>Nombre</th>
-					<th>Número de actividad</th>
-					<th>Duración</th>
-					<th>Descripción</th>
-					<th>Acciones</th>
-				</tr>
-			</thead>
-			<tbody>
-				@foreach ($session->activities as $activity)
-					<tr>
-						<td><h4><a href="{{ url('dashboard/sesiones/actividades/ver/' . $activity->id) }}">{{$activity->name}}</a></h4></td>
-						<td>{{$activity->order}}</td>
-						<td>{{$activity->duration}} hrs.</td>
-						<td>{{$activity->description}}</td>
-						<td>
-							<a href="{{ url('dashboard/sesiones/actividades/ver/' . $activity->id) }}" class="btn xs view">Ver</a>
-							<a href="{{ url('dashboard/sesiones/actividades/editar/' . $activity->id) }}" class="btn xs view">Actualizar</a>
-						 <!-- <a href ="{{ url('dashboard/modulos/eliminar' . $activity->id) }}"  id ="{{$activity->id}}" class="btn xs danger" onclick="return confirm('¿Estás seguro?');">Eliminar</a></td>-->
-				</tr>
-				@endforeach
-			</tbody>
-		</table>
-	</div>
+<div class="row">
+	@foreach ($session->activities as $activity)
+	<!--icono-->	
+		<div class="col-sm-1 right">
+			<b class="icon_h i_lecturas list_s"></b>
+		</div>
+		<div class="col-sm-9">
+			<p><a href="{{ url('dashboard/sesiones/actividades/ver/' . $activity->id) }}">{{$activity->name}}</a> <span class="notes">({{$activity->duration}})</span></p>		
+		</div>
+		<div class="col-sm-2">
+			<p><a href="{{ url('dashboard/sesiones/actividades/ver/' . $activity->id) }}" class="btn xs ev">Ver</a>
+			<a href="{{ url('dashboard/sesiones/actividades/editar/' . $activity->id) }}" class="btn xs view">Actualizar</a>
+			 <!-- <a href ="{{ url('dashboard/modulos/eliminar' . $activity->id) }}"  id ="{{$activity->id}}" class="btn xs danger" onclick="return confirm('¿Estás seguro?');">Eliminar</a>--></p>
+		</div>
+		<div class="col-sm-12">
+			<div class="line"></div>
+		</div>
+	@endforeach
 </div>
