d2247d8c4d62f199ab8e00edd14f45e43a52d56c
hugovom <hugo.om@gmail.com>
2017-05-02 18:52:48 -0500

    update activities list

--- resources/views/admin/modules/sessions/sessions-activities-list.blade.php
+++ resources/views/admin/modules/sessions/sessions-activities-list.blade.php
@@ -2,7 +2,7 @@
 	@foreach ($session->activities as $activity)
 	<!--icono-->	
 		<div class="col-sm-1 right">
-			<b class="icon_h i_lecturas list_s"></b>
+			<b class="icon_h {{$activity->type ? $activity->type  : 'default'}} list_s width_s"></b>
 		</div>
 		<div class="col-sm-9">
 			<p><a href="{{ url('dashboard/sesiones/actividades/ver/' . $activity->id) }}">{{$activity->name}}</a> <span class="notes">({{$activity->duration}})</span></p>		
@@ -13,7 +13,7 @@
 			 <!-- <a href ="{{ url('dashboard/modulos/eliminar' . $activity->id) }}"  id ="{{$activity->id}}" class="btn xs danger" onclick="return confirm('¿Estás seguro?');">Eliminar</a>--></p>
 		</div>
 		<div class="col-sm-12">
-			<div class="line"></div>
+			<div class="line activities"></div>
 		</div>
 	@endforeach
 </div>
