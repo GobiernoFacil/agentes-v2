ccc668cb3a41233ff24ae5decf60b0734fe6a1bc
hugovom <hugo.om@gmail.com>
2017-05-08 17:10:35 -0500

    update breadcrumb

--- resources/views/layouts/fellow/breadcrumb/b_modules.blade.php
+++ resources/views/layouts/fellow/breadcrumb/b_modules.blade.php
@@ -4,11 +4,21 @@
 	@if ($__env->yieldContent('breadcrumb_type') =="module list")
 	<li>Módulos de aprendizaje</li>
 	@endif
-	@if ($__env->yieldContent('breadcrumb_type') =="module view")
+	@if ($__env->yieldContent('breadcrumb_type') =="module view" || $__env->yieldContent('breadcrumb_type') =="session view")
 	<li><a href="{{url('tablero/aprendizaje')}}">Módulos de aprendizaje</a></li>
 	@endif
+	
 	@if ($__env->yieldContent('breadcrumb_type') =="module view")
-	<li>Ver Módulo</li>
+	<!--ver módulo.-->
+	<li>{{$module->title}}</li>
+	@endif
+	
+	
+	@if ($__env->yieldContent('breadcrumb_type') =="session view")
+	<!--ver módulo.-->
+	<li><a href="{{ url('tablero/aprendizaje/' .$session->module->slug ) }}">{{$session->module->title}}</a></li>
+	<!--ver sessión.-->
+	<li>Sesión {{$session->order . ': ' . $session->name}}</a></li>
 	@endif
 
 </ul>
