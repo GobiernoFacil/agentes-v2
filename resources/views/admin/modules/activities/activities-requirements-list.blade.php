85cdd42ddd865739cb7c5171967f48581d9c9467
hugovom <hugo.om@gmail.com>
2017-05-10 13:15:35 -0500

    remove links

--- resources/views/admin/modules/activities/activities-requirements-list.blade.php
+++ resources/views/admin/modules/activities/activities-requirements-list.blade.php
@@ -12,11 +12,11 @@
 		@foreach ($activity->activityRequirements as $requirement)
 			<tr>
 				<td>{{$requirement->order}}</td>
-				<td><h4><a href="{{ url('dashboard/sesiones/actividades/requerimientos/ver/' . $requirement->id) }}">{{$requirement->name}}</a></h4></td>
+				<td><h4>{{$requirement->name}}</h4></td>
 				<td>{{$requirement->description}}</td>
     <td>{{$requirement->material_link ? $requirement->material_link : 'Sin información'}}</td>
 				<td>
-					<a href="{{ url('dashboard/sesiones/actividades/requerimientos/ver/' . $requirement->id) }}" class="btn xs ev">Ver</a>
+					<!--<a href="{{ url('dashboard/sesiones/actividades/requerimientos/ver/' . $requirement->id) }}" class="btn xs ev">Ver</a>-->
 					<a href="{{ url('dashboard/sesiones/actividades/requerimientos/editar/' . $requirement->id) }}" class="btn xs view">Actualizar</a>
 				 <!-- <a href ="{{ url('dashboard/modulos/eliminar' . $requirement->id) }}"  id ="{{$requirement->id}}" class="btn xs danger" onclick="return confirm('¿Estás seguro?');">Eliminar</a></td>-->
 		</tr>
