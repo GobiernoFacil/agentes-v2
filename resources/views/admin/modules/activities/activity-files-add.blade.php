4bfa9c09fec47409b384aa8e4cf6a8cd18b81ce1
hugovom <hugo.om@gmail.com>
2017-05-10 13:51:12 -0500

    update view

--- resources/views/admin/modules/activities/activity-files-add.blade.php
+++ resources/views/admin/modules/activities/activity-files-add.blade.php
@@ -1,13 +1,14 @@
 @extends('layouts.admin.a_master')
 @section('title', 'Agregar archivos a actividad')
 @section('description', 'Agregar archivos a actividad')
-@section('body_class', '')
-@section('breadcrumb_type', '')
+@section('body_class', 'modulos')
+@section('breadcrumb_type', 'module session add files')
+@section('breadcrumb', 'layouts.admin.breadcrumb.b_modules')
 
 @section('content')
 <div class="row">
   <div class="col-sm-12">
-    <h1>Agregar archivos a actividad: {{$activity->name}} </h1>
+    <h1><strong>Agregar archivo</strong> a actividad: {{$activity->name}} </h1>
   </div>
 </div>
 <div class="box">
