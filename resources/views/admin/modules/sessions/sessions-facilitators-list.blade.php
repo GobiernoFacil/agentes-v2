4ca8342c607e738af9d27c7c13d858b65466330d
hugovom <hugo.om@gmail.com>
2017-05-10 12:24:15 -0500

    update facilitators list w fellow user type

--- resources/views/admin/modules/sessions/sessions-facilitators-list.blade.php
+++ resources/views/admin/modules/sessions/sessions-facilitators-list.blade.php
@@ -11,9 +11,13 @@
 			<div class="col-sm-9">
 				<h4>{{$facilitator->user->name}}</h4>
 				<p>{{$facilitator->user->institution}}<br>
+					@if($user->type == "admin")
 					<a href='{{ url("dashboard/facilitadores/ver/" .$facilitator->user->id) }}' class="btn xs view">Ver</a>
 					<a href='{{ url("dashboard/sesiones/facilitadores/asignar/$session->id") }}' class="btn xs danger">Remover</a>
 					<!-- <a href ="{{ url('dashboard/requerimientos/eliminar' . $facilitator->id) }}"  id ="{{$facilitator->id}}" class="btn xs danger" onclick="return confirm('¿Estás seguro?');">Eliminar</a>-->
+					@else 
+					<a href='{{ url("tablero/facilitadores/ver/" .$facilitator->user->id) }}' class="btn xs view">Ver</a>
+					@endif					
 				</p>
 			</div>
 		</div>
