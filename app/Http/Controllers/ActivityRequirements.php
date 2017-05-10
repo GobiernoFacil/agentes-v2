36484bc27fc670f2ff90b7d24029e2456f988856
hugovom <hugo.om@gmail.com>
2017-05-10 13:34:49 -0500

    update edit method

--- app/Http/Controllers/ActivityRequirements.php
+++ app/Http/Controllers/ActivityRequirements.php
@@ -100,10 +100,12 @@ class ActivityRequirements extends Controller
             $user      = Auth::user();
             $activityR  = ActivityRequirement::where('id',$id)->firstOrFail();
             $activity   = Activity::where('id',$activityR->activity_id)->firstOrFail();
+            $session   = ModuleSession::where('id',$activity->session_id)->firstOrFail();
             return view('admin.modules.activities.activity-requirement-update')->with([
               "user"      => $user,
               "activity"  => $activity,
-              "activityR" => $activityR
+              "activityR" => $activityR,
+              "session"	  => $session,
             ]);
         }
 
