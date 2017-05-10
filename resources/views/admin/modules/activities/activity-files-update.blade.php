edb1f699da61336d43eeecf13b8b5e81752e7b2d
hugovom <hugo.om@gmail.com>
2017-05-10 17:02:56 -0500

    add breadcrumb

--- resources/views/admin/modules/activities/activity-files-update.blade.php
+++ resources/views/admin/modules/activities/activity-files-update.blade.php
@@ -1,13 +1,14 @@
 @extends('layouts.admin.a_master')
 @section('title', 'Actualizar archivos a actividad')
 @section('description', 'Actualizar archivos a actividad')
-@section('body_class', '')
-@section('breadcrumb_type', '')
+@section('body_class', 'modulos')
+@section('breadcrumb_type', 'module session update files')
+@section('breadcrumb', 'layouts.admin.breadcrumb.b_modules')
 
 @section('content')
 <div class="row">
   <div class="col-sm-12">
-    <h1>Actualizar archivo de actividad: {{$file->activity->name}} </h1>
+    <h1><strong>Actualizar archivo</strong> de actividad: {{$file->activity->name}} </h1>
   </div>
 </div>
 <div class="box">
