28a468d8fef390a4cc6ec1f95b581838e65e054e
hugovom <hugo.om@gmail.com>
2017-05-15 11:23:09 -0500

    update requirements list

--- resources/views/admin/modules/activities/activities-requirements-list.blade.php
+++ resources/views/admin/modules/activities/activities-requirements-list.blade.php
@@ -4,21 +4,26 @@
 			<th>#</th>
 			<th>Nombre</th>
 			<th>Descripción</th>
-  <th>URL contenido</th>
+			<th>URL contenido</th>
+			@if($user->type=="admin")
 			<th>Acciones</th>
+			@endif
 		</tr>
 	</thead>
 	<tbody>
 		@foreach ($activity->activityRequirements as $requirement)
-			<tr>
-				<td>{{$requirement->order}}</td>
-				<td><h4>{{$requirement->name}}</h4></td>
-				<td>{{$requirement->description}}</td>
-    <td>{{$requirement->material_link ? $requirement->material_link : 'Sin información'}}</td>
-				<td>
-					<!--<a href="{{ url('dashboard/sesiones/actividades/requerimientos/ver/' . $requirement->id) }}" class="btn xs ev">Ver</a>-->
-					<a href="{{ url('dashboard/sesiones/actividades/requerimientos/editar/' . $requirement->id) }}" class="btn xs view">Actualizar</a>
-				 <!-- <a href ="{{ url('dashboard/modulos/eliminar' . $requirement->id) }}"  id ="{{$requirement->id}}" class="btn xs danger" onclick="return confirm('¿Estás seguro?');">Eliminar</a></td>-->
+		<tr>
+			<td>{{$requirement->order}}</td>
+			<td><h4>{{$requirement->name}}</h4></td>
+			<td>{{$requirement->description}}</td>
+			<td>{{$requirement->material_link ? $requirement->material_link : 'Sin información'}}</td>
+			@if($user->type=="admin")
+			<td>
+				<!--<a href="{{ url('dashboard/sesiones/actividades/requerimientos/ver/' . $requirement->id) }}" class="btn xs ev">Ver</a>-->
+				<a href="{{ url('dashboard/sesiones/actividades/requerimientos/editar/' . $requirement->id) }}" class="btn xs view">Actualizar</a>
+				<!-- <a href ="{{ url('dashboard/modulos/eliminar' . $requirement->id) }}"  id ="{{$requirement->id}}" class="btn xs danger" onclick="return confirm('¿Estás seguro?');">Eliminar</a>-->
+			 </td>
+			 @endif
 		</tr>
 		@endforeach
 	</tbody>
