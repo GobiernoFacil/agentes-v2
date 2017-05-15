9b06b5ec1445452b8c0fbc6fd7ed3e2e11101b30
hugovom <hugo.om@gmail.com>
2017-05-15 11:31:13 -0500

    update breadcrumb

--- resources/views/layouts/fellow/breadcrumb/b_modules.blade.php
+++ resources/views/layouts/fellow/breadcrumb/b_modules.blade.php
@@ -4,7 +4,7 @@
 	@if ($__env->yieldContent('breadcrumb_type') =="module list")
 	<li>Módulos de aprendizaje</li>
 	@endif
-	@if ($__env->yieldContent('breadcrumb_type') =="module view" || $__env->yieldContent('breadcrumb_type') =="session view")
+	@if ($__env->yieldContent('breadcrumb_type') =="module view" || $__env->yieldContent('breadcrumb_type') =="session view" || $__env->yieldContent('breadcrumb_type') =="activity view")
 	<li><a href="{{url('tablero/aprendizaje')}}">Módulos de aprendizaje</a></li>
 	@endif
 	
@@ -14,11 +14,20 @@
 	@endif
 	
 	
-	@if ($__env->yieldContent('breadcrumb_type') =="session view")
+	@if ($__env->yieldContent('breadcrumb_type') =="session view" || $__env->yieldContent('breadcrumb_type') =="activity view")
 	<!--ver módulo.-->
 	<li><a href="{{ url('tablero/aprendizaje/' .$session->module->slug ) }}">{{$session->module->title}}</a></li>
+	@endif
+	
+	@if ($__env->yieldContent('breadcrumb_type') =="session view" )
 	<!--ver sessión.-->
-	<li>Sesión {{$session->order . ': ' . $session->name}}</a></li>
+	<li>Sesión {{$session->order . ': ' . $session->name}}</li>
 	@endif
 
+	@if ($__env->yieldContent('breadcrumb_type') =="activity view" )
+	<!--ver sessión.-->
+	<li><a href="{{ url('tablero/aprendizaje/' .$session->module->slug .'/' . $session->slug ) }}">Sesión {{$session->order . ': ' . $session->name}}</a></li>
+	<li>Actividad: {{$activity->name}}</li>
+	@endif
+	
 </ul>
