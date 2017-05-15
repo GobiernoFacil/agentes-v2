1116f9a6e2da34e0014f44efa31f2523f88da56f
CarlosOsorio <carlos.aosorio89@gmail.com>
2017-05-15 16:23:03 -0500

    enable delete activities

--- resources/views/admin/modules/sessions/sessions-activities-list.blade.php
+++ resources/views/admin/modules/sessions/sessions-activities-list.blade.php
@@ -1,23 +1,23 @@
 <div class="row">
 	@foreach ($session->activities as $activity)
-	<!--icono-->	
+	<!--icono-->
 		<div class="col-sm-1 right">
 			<b class="icon_h {{$activity->type ? $activity->type  : 'default'}} list_s width_s"></b>
 		</div>
 		<div class="col-sm-9">
 			<p>@if($user->type == "admin")
-				<a href="{{ url('dashboard/sesiones/actividades/ver/' . $activity->id) }}">{{$activity->name}}</a> 
+				<a href="{{ url('dashboard/sesiones/actividades/ver/' . $activity->id) }}">{{$activity->name}}</a>
 				 @else
 				 <a href="{{ url('tablero/aprendizaje/'. $session->module->slug .'/'. $session->slug .'/' . $activity->id) }}">{{$activity->name}}</a>
 				 @endif
-				<span class="notes">({{$activity->duration}})</span></p>		
+				<span class="notes">({{$activity->duration}})</span></p>
 		</div>
 		<div class="col-sm-2">
 			<p>
 			@if($user->type == "admin")
 			<a href="{{ url('dashboard/sesiones/actividades/ver/' . $activity->id) }}" class="btn xs ev">Ver</a>
 			<a href="{{ url('dashboard/sesiones/actividades/editar/' . $activity->id) }}" class="btn xs view">Actualizar</a>
-			 <!-- <a href ="{{ url('dashboard/modulos/eliminar' . $activity->id) }}"  id ="{{$activity->id}}" class="btn xs danger" onclick="return confirm('¿Estás seguro?');">Eliminar</a>-->
+			 <a href ="{{ url('dashboard/sesiones/actividades/eliminar/' . $activity->id) }}"  id ="{{$activity->id}}" class="btn xs danger" onclick="return confirm('¿Estás seguro?');">Eliminar</a>
 			 @else
 			 <a href="{{ url('tablero/aprendizaje/'. $session->module->slug .'/'. $session->slug .'/' . $activity->id) }}" class="btn xs ev">Ver</a>
 			 @endif
