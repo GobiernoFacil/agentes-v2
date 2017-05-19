72f014577e3267f896f6a6cbabc4fb295f219a39
hugovom <hugo.om@gmail.com>
2017-05-18 22:29:34 -0500

    add 2017 results

--- routes/web.php
+++ routes/web.php
@@ -26,6 +26,7 @@ Route::get('redes-sociales', 'Front@redes');
 /*@NoticeFront Controller */
 /*********  Enlaces convocatoria ******** */
 Route::get('convocatoria', 'NoticeFront@convocatoria');
+Route::get('convocatoria/resultados-2017', 'NoticeFront@resultado17');
 //Route::get('convocatoria/proceso-de-seleccion', 'NoticeFront@bases'); oculto por acuerdo del equipo
 
 /** se acabó la convocatoria
