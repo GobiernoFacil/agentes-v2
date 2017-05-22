eb5925c14d82dca4bac46b70038aa39dba91a3c8
hugovom <hugo.om@gmail.com>
2017-05-22 13:13:29 -0500

    update links

--- resources/views/admin/modules/sessions/sessions-monitoring-list.blade.php
+++ resources/views/admin/modules/sessions/sessions-monitoring-list.blade.php
@@ -6,6 +6,7 @@
 					<th>Conocimiento</th>
 					<th>Competencia</th>
 					<th>Actitud</th>
+					<th>Rol del mentor</th>
 					<th>Acciones</th>
 				</tr>
 			</thead>
@@ -15,10 +16,12 @@
 						<td><h4><a href="{{ url('dashboard/sesiones/mecanismos-monitoreo/ver/' . $evaluation->id) }}">{{$evaluation->knowledge}}</a></h4></td>
 						<td>{{$evaluation->competitions}}</td>
 						<td>{{$evaluation->attitude}}</td>
+						<td>{{$evaluation->role}}</td>
 						<td>
-							<a href="{{ url('dashboard/sesiones/mecanismos-monitoreo/ver/' . $evaluation->id) }}" class="btn xs view">Ver</a>
+							<!--<a href="{{ url('dashboard/sesiones/mecanismos-monitoreo/ver/' . $evaluation->id) }}" class="btn xs view">Ver</a>-->
 							<a href="{{ url('dashboard/sesiones/mecanismos-monitoreo/editar/' . $evaluation->id) }}" class="btn xs view">Actualizar</a>
-						 <!-- <a href ="{{ url('dashboard/mecanismos-monitoreo/eliminar' . $evaluation->id) }}"  id ="{{$evaluation->id}}" class="btn xs danger" onclick="return confirm('¿Estás seguro?');">Eliminar</a></td>-->
+						 <!-- <a href ="{{ url('dashboard/mecanismos-monitoreo/eliminar' . $evaluation->id) }}"  id ="{{$evaluation->id}}" class="btn xs danger" onclick="return confirm('¿Estás seguro?');">Eliminar</a>-->
+						</td>
 				</tr>
 				@endforeach
 			</tbody>
