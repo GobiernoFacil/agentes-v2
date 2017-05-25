c622a91cf685e71802ed2e4942d806a3de13354c
hugovom <hugo.om@gmail.com>
2017-05-25 17:01:41 -0500

    update breadcrumb

--- resources/views/layouts/fellow/breadcrumb/b_messages.blade.php
+++ resources/views/layouts/fellow/breadcrumb/b_messages.blade.php
@@ -4,13 +4,18 @@
 	@if ($__env->yieldContent('breadcrumb_type') =="messages list")
 	<li>Mensajes</li>
 	@endif
-	@if ($__env->yieldContent('breadcrumb_type') =="message view" || $__env->yieldContent('breadcrumb_type') =="message add")
+	@if ($__env->yieldContent('breadcrumb_type') =="message view" || $__env->yieldContent('breadcrumb_type') =="message add"  || $__env->yieldContent('breadcrumb_type') =="message send")
 	<li><a href="{{url('tablero/mensajes')}}">Mensajes</a></li>
 	@endif
 	@if ($__env->yieldContent('breadcrumb_type') =="message view")
-	<li>Ver Conversación con {{$conversation->user_to->name}}</li>
+	<li>Conversación con {{$conversation->user_to->name}}</li>
 	@endif
 	@if ($__env->yieldContent('breadcrumb_type') =="message add")
+	<li>Enviar Mensaje</li>
+	@endif
+	
+	@if ( $__env->yieldContent('breadcrumb_type') =="message send")
+	<li><a href="{{ url('tablero/mensajes/ver/' . $conversation->id)}}">Conversación con {{$conversation->user_to->name}}</a></li>
 	<li>Enviar Mensaje</li>
 	@endif
 
