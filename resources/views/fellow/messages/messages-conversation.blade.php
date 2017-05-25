2ad5e912a86b640bc64db1d47ce0259dc5ad4ac2
hugovom <hugo.om@gmail.com>
2017-05-25 16:46:51 -0500

    update conversations view

--- resources/views/fellow/messages/messages-conversation.blade.php
+++ resources/views/fellow/messages/messages-conversation.blade.php
@@ -8,36 +8,56 @@
 @section('content')
 <div class="row">
   <div class="col-sm-9">
-    @if($conversation->to_id != $user->id)
-    <h1>Mensaje privado con {{$conversation->user_to->name}} </h1>
-    @else
-    <h1>Mensaje privado con {{$conversation->user->name}} </h1>
-    @endif
+	    <h1>Conversación con  
+		@if($conversation->to_id != $user->id)
+		   {{$conversation->user_to->name}}
+		@else
+		   {{$conversation->user->name}}
+		@endif
+		</h1>
   </div>
   <div class="col-sm-3 center">
-    <a href='{{ url("tablero/mensajes/conversacion/agregar/$conversation->id") }}' class="btn gde"><strong>+</strong> Agregar Mensaje</a>
+    <a href='{{ url("tablero/mensajes/conversacion/agregar/$conversation->id") }}' class="btn gde"><strong>+</strong> Escribir Mensaje</a>
   </div>
 </div>
 <div class="box">
-  @if($conversation->messages->count() > 0)
-    @foreach($conversation->messages as $message)
-      <div class="row">
+	<div class="row">
+		<div class="col-sm-9">
+			<h5>Asunto</h5>
+			<h2 class="title">{{$conversation->title}}</h2>
+		</div>
+		<div class="col-sm-3">
+			<p class="right">{{$conversation->messages->count() == 1 ? $conversation->messages->count() . ' mensaje' : $conversation->messages->count() . ' mensajes' }}</p>
+		</div>
+		<div class="col-sm-12 divider"></div>
+	</div>
+	@if($conversation->messages->count() > 0)
+	<div class="row">
         <div class="col-sm-8 col-sm-offset-2">
-          <p>{{$message->message}}</p>
+		@foreach($conversation->messages as $message)
+			<div class="row">
+			  <div class="col-sm-8 {{$message->user_id == $user->id ? 'col-sm-offset-4' : ''}}">
+			    <div class="message_box {{$message->user_id == $user->id ? 'me' : 'not_me'}}">
+			    <p>{{$message->message}}</p>
+			    </div>
+			    <p><span>{{$message->updated_at->diffForHumans()}}</span></p>
+			  </div>
+			</div>
+		@endforeach
         </div>
-      </div>
-    @endforeach
-    <div class="row">
-      <div class="col-sm-3 col-sm-offset-2 center">
-        <a href='{{ url("tablero/mensajes/conversacion/agregar/$conversation->id") }}' class="btn gde"><strong>+</strong> Agregar Mensaje</a>
-      </div>
-    </div>
-  @else
-  <div class="row">
-    <div class="col-sm-8 col-sm-offset-2">
-      <p>No existen mensajes</p>
-    </div>
-  </div>
-  @endif
+  	</div>
+  	
+	@else
+	<div class="row">
+    	<div class="col-sm-8 col-sm-offset-2">
+			<p>No existen mensajes</p>
+    	</div>
+  	</div>
+  	@endif
 </div>
+<div class="row">
+		<div class="col-sm-8 col-sm-offset-2 center">
+		    <a href='{{ url("tablero/mensajes/conversacion/agregar/$conversation->id") }}' class="btn gde"><strong>+</strong> Escribir Mensaje</a>
+		</div>
+	</div>
 @endsection
