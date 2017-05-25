7700c7e433eb83a82da5abb24a9cf5bfb4ecbe54
hugovom <hugo.om@gmail.com>
2017-05-25 17:01:55 -0500

    update view

--- resources/views/fellow/messages/messages-single-add.blade.php
+++ resources/views/fellow/messages/messages-single-add.blade.php
@@ -1,23 +1,25 @@
 @extends('layouts.admin.a_master')
-@section('title', '')
-@section('description', '')
-@section('body_class', '')
-@section('breadcrumb_type', '')
+@section('title', 'Enviar mensaje privado')
+@section('description', 'Enviar mensaje privado')
+@section('body_class', 'fellow mensajes')
+@section('breadcrumb_type', 'message send')
+@section('breadcrumb', 'layouts.fellow.breadcrumb.b_messages')
 
 @section('content')
 <div class="row">
   <div class="col-sm-12">
     @if($conversation->to_id != $user->id)
-    <h1>Enviar mensaje privado con {{$conversation->user_to->name}} </h1>
+    <h1>Enviar mensaje privado a {{$conversation->user_to->name}} </h1>
     @else
-    <h1>Enviar mensaje privado con {{$conversation->user->name}} </h1>
+    <h1>Enviar mensaje privado a {{$conversation->user->name}} </h1>
     @endif
   </div>
 </div>
 <div class="box">
   <div class="row">
     <div class="col-sm-8 col-sm-offset-2">
-      @include('fellow.messages.form.messages-add-single-form')
+	    <h2>Asunto: {{$conversation->title}}</h2>
+		@include('fellow.messages.form.messages-add-single-form')
     </div>
   </div>
 </div>
