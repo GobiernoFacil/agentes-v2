70315ca0f8958669bbdb11af10d3f6d1422325e2
hugovom <hugo.om@gmail.com>
2017-05-26 10:52:50 -0500

    update breadcrumb

--- resources/views/layouts/facilitator/breadcrumb/b_messages.blade.php
+++ resources/views/layouts/facilitator/breadcrumb/b_messages.blade.php
@@ -4,11 +4,13 @@
 	@if ($__env->yieldContent('breadcrumb_type') =="messages list")
 	<li>Mensajes</li>
 	@endif
-	@if ($__env->yieldContent('breadcrumb_type') =="message view")
+	@if ($__env->yieldContent('breadcrumb_type') =="message view" || $__env->yieldContent('breadcrumb_type') =="message add")
 	<li><a href="{{url('tablero-facilitador/mensajes')}}">Mensajes</a></li>
 	@endif
 	@if ($__env->yieldContent('breadcrumb_type') =="message view")
 	<li>Ver Mensaje</li>
 	@endif
-
+	@if ($__env->yieldContent('breadcrumb_type') =="message add")
+	<li>Enviar Mensaje</li>
+	@endif
 </ul>
