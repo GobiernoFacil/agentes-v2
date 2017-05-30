c60c4d3b0630cab94808ffe8328ab65273babe1a
hugovom <hugo.om@gmail.com>
2017-05-30 10:45:46 -0500

    add view

---
+++ resources/views/facilitator/messages/messages-conversation.blade.php
@@ -0,0 +1,63 @@
+@extends('layouts.admin.a_master')
+@section('title', 'Mensaje privado con ' . $conversation->user_to->name)
+@section('description', 'Mensaje privado con ' . $conversation->user_to->name)
+@section('body_class', 'mensajes')
+@section('breadcrumb_type', 'message view')
+@section('breadcrumb', 'layouts.facilitator.breadcrumb.b_messages')
+
+@section('content')
+<div class="row">
+  <div class="col-sm-9">
+	    <h1>Conversación con  
+		@if($conversation->to_id != $user->id)
+		   {{$conversation->user_to->name}}
+		@else
+		   {{$conversation->user->name}}
+		@endif
+		</h1>
+  </div>
+  <div class="col-sm-3 center">
+    <a href='{{ url("tablero-facilitador/mensajes/conversacion/agregar/$conversation->id") }}' class="btn gde"><strong>+</strong> Escribir Mensaje</a>
+  </div>
+</div>
+<div class="box">
+	<div class="row">
+		<div class="col-sm-9">
+			<h5>Asunto</h5>
+			<h2 class="title">{{$conversation->title}}</h2>
+		</div>
+		<div class="col-sm-3">
+			<p class="right">{{$conversation->messages->count() == 1 ? $conversation->messages->count() . ' mensaje' : $conversation->messages->count() . ' mensajes' }}</p>
+		</div>
+		<div class="col-sm-12 divider"></div>
+	</div>
+	@if($conversation->messages->count() > 0)
+	<div class="row">
+        <div class="col-sm-8 col-sm-offset-2">
+		@foreach($conversation->messages as $message)
+			<div class="row">
+			  <div class="col-sm-8 {{$message->user_id == $user->id ? 'col-sm-offset-4' : ''}}">
+			    <div class="message_box {{$message->user_id == $user->id ? 'me' : 'not_me'}}">
+			    <p>{{$message->message}}</p>
+			    </div>
+			    <p><span>{{$message->updated_at->diffForHumans()}}</span></p>
+			  </div>
+			</div>
+		@endforeach
+        </div>
+  	</div>
+  	
+	@else
+	<div class="row">
+    	<div class="col-sm-8 col-sm-offset-2">
+			<p>No existen mensajes</p>
+    	</div>
+  	</div>
+  	@endif
+</div>
+<div class="row">
+		<div class="col-sm-8 col-sm-offset-2 center">
+		    <a href='{{ url("tablero-facilitador/mensajes/conversacion/agregar/$conversation->id") }}' class="btn gde"><strong>+</strong> Escribir Mensaje</a>
+		</div>
+	</div>
+@endsection
