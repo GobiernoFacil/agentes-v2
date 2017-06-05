c4bfc2bf4827273f6fa0be9d5d75490070a57f99
hugovom <hugo.om@gmail.com>
2017-06-05 11:21:27 -0500

    update forums list

--- resources/views/fellow/modules/sessions/forums/forums-list.blade.php
+++ resources/views/fellow/modules/sessions/forums/forums-list.blade.php
@@ -43,36 +43,37 @@
 @if($forums->count()>0)
 <div class="box">
 	<div class="row">
-		<div class="col-sm-12">
-			<table class="table">
-			  <thead>
-			    <tr>
-			      <th>Tema</th>
-			      <th>Descripción</th>
-			      <th>Acciones</th>
-			    </tr>
-			  </thead>
-			  <tbody>
-			    @foreach ($forums as $conversation)
-			      <tr>
-							@if($session)
-			        <td><h4> <a href="{{ url('tablero/foros/pregunta/'.$session->slug.'/'.$conversation->slug.'/ver') }}">{{$conversation->topic}}</a></h4></td>
-							@else
-							<td><h4> <a href="{{ url('tablero/foros/'.$user->FellowData->state.'/'.$conversation->slug.'/ver') }}">{{$conversation->topic}}</a></h4></td>
-							@endif
-              <td>{{str_limit($conversation->description, $limit = 20, $end = '...')}}</td>
-			        <td>
-								@if($session)
-			          <a href="{{ url('tablero/foros/pregunta/'.$session->slug.'/'.$conversation->slug.'/ver') }}" class="btn xs view">Ver</a>
-								@else
-								<a href="{{ url('tablero/foros/'.$user->FellowData->state.'/'.$conversation->slug.'/ver') }}" class="btn xs view">Ver</a>
-								@endif
-					</tr>
-			    @endforeach
-			  </tbody>
-			</table>
-
+		<div class="col-sm-10 col-sm-offset-1">
+			@foreach ($forums as $conversation)
+				<div class="row forum_list">
+					<div class="divider b"></div>
+					<div class="col-sm-1">
+						<img src='{{url("img/users/default.png")}}' width="100%">
+					</div>
+					<div class="col-sm-9">
+						@if($session)
+						<h2> <a href="{{ url('tablero/foros/pregunta/'.$session->slug.'/'.$conversation->slug.'/ver') }}">{{$conversation->topic}}</a></h2>
+						@else
+						<h2><a href="{{ url('tablero/foros/'.$user->FellowData->state.'/'.$conversation->slug.'/ver') }}">{{$conversation->topic}}</a></h2>
+						@endif
+						<p class="author">Por {{$conversation->user_id}} <span>{{$conversation->created_at->diffForHumans()}}</span></p>
+					</div>
+					<div class="col-sm-2">
+						<h3 class="count_messages">{{$conversation->messages->count()}}</h3>
+					</div>
+				</div>				 
+			@endforeach
 			{{ $forums->links() }}
+		</div>
+		<div class="col-sm-10 col-sm-offset-1">
+			<div class="divider b"></div>
+		</div>
+		<div class="col-sm-8 col-sm-offset-2 center">
+			@if($session)
+			<a href='{{ url("tablero/foros/{$session->slug}/pregunta/crear") }}' class="btn gde download">Agregar Pregunta o Tema al foro [<strong>+</strong>]</a>
+			@else
+			<a href='{{ url("tablero/foros/pregunta/estado/{$user->FellowData->state}/crear") }}' class="btn gde download">Agregar Pregunta o Tema [<strong>+</strong>]</a>
+			@endif
 		</div>
 	</div>
 </div>
