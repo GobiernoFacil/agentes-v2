f7cebfe0731e45238534e0da640611c320f08739
hugovom <hugo.om@gmail.com>
2017-06-05 10:36:06 -0500

    update view

--- resources/views/fellow/modules/sessions/forums/forums-add-question.blade.php
+++ resources/views/fellow/modules/sessions/forums/forums-add-question.blade.php
@@ -7,7 +7,7 @@
 @section('content')
 <div class="row">
   <div class="col-sm-12">
-    <h1>Crear nueva pregunta</h1>
+    <h1>Agregar tema o nueva pregunta a <strong>{{$session->forums->topic}}</strong></h1>
   </div>
 </div>
 <div class="box">
