0ffef4d3cb029646aea3f8361402de3b78b8c157
hugovom <hugo.om@gmail.com>
2017-06-05 10:19:20 -0500

    update forums list

--- resources/views/fellow/modules/sessions/forums/forums-list.blade.php
+++ resources/views/fellow/modules/sessions/forums/forums-list.blade.php
@@ -1,24 +1,40 @@
 @extends('layouts.admin.a_master')
-@section('title', '' )
+@section('title', $forum->topic )
 @section('description','' )
 @section('body_class', 'fellow foros')
 @section('breadcrumb_type', '')
 
 @section('content')
 <div class="row">
+	<!-- título-->
 	<div class="col-sm-9">
+		<h1>{{$forum->topic}}</h1>		
+		
+	</div>
+	<!-- agregar pregunta-->
+	<div class="col-sm-3 center">
 		@if($session)
-		<h1>Preguntas de la sesión: {{$session->name}}</h1>
+		<a href='{{ url("tablero/foros/{$session->slug}/pregunta/crear") }}' class="btn gde download">Agregar Pregunta o Tema al foro [<strong>+</strong>]</a>
 		@else
-		<h1>Preguntas del {{$forum->topic}}</h1>
+		<a href='{{ url("tablero/foros/pregunta/estado/{$user->FellowData->state}/crear") }}' class="btn gde download">Agregar Pregunta o Tema [<strong>+</strong>]</a>
 		@endif
 	</div>
-  <div class="col-sm-3 center">
+	<!-- descripción-->
+	<div class="col-sm-12 forum_list">
+		<div class="divider top"></div>
 		@if($session)
-		<a href='{{ url("tablero/foros/{$session->slug}/pregunta/crear") }}' class="btn gde"><strong>+</strong> Agregar Pregunta</a>
+		<p><span class="type module_session">{{$forum->session->module->title}} / {{$forum->session->name}}</span></p>
 		@else
-		<a href='{{ url("tablero/foros/pregunta/estado/{$user->FellowData->state}/crear") }}' class="btn gde"><strong>+</strong> Agregar Pregunta</a>
+		<p><span class="type state">Estado</span></p>
 		@endif
+		<p class="author">Creado por <strong>{{!empty($forum->user->institution) ? $forum->user->institution : ''}}</strong> <span>{{$forum->created_at->diffForHumans()}}</span></p>
+		<div class="divider top"></div>
+	</div>
+	<!-- descripción-->
+	<div class="col-sm-10 col-sm-offset-1">
+		
+		<h4>Descripción</h4>
+		<p>{{$forum->description}}</p>
 	</div>
 </div>
 
@@ -60,10 +76,19 @@
 	</div>
 </div>
 @else
-<div class="row">
-  <div class="col-sm-12">
-    <p>Sin preguntas</p>
-  </div>
+<div class="box">
+	<div class="row center">
+		<div class="col-sm-12">
+			<h2>Sin preguntas o temas en el foro</h2>
+		</div>
+		<div class="col-sm-6 col-sm-offset-3">
+			@if($session)
+				<a href='{{ url("tablero/foros/{$session->slug}/pregunta/crear") }}' class="btn gde download">Agregar Pregunta o Tema al foro[<strong>+</strong>]</a>
+			@else
+				<a href='{{ url("tablero/foros/pregunta/estado/{$user->FellowData->state}/crear") }}' class="btn gde download">Agregar Pregunta o Tema al foro[<strong>+</strong>]</a>
+			@endif
+  		</div>
+	</div>
 </div>
 @endif
 @endsection
