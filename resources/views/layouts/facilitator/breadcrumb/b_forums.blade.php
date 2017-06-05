44785d0a13f06cada47fceda505fb640f3fd3ed0
hugovom <hugo.om@gmail.com>
2017-06-05 16:44:01 -0500

    add breadcrumb

---
+++ resources/views/layouts/facilitator/breadcrumb/b_forums.blade.php
@@ -0,0 +1,11 @@
+<ul>
+	<li>Estás en:</li>
+	<li><a href="{{url('tablero-facilitador')}}">Tablero</a></li>
+	@if ($__env->yieldContent('breadcrumb_type') =="forums list")
+	<li>Foros</li>
+	@endif
+	@if ($__env->yieldContent('breadcrumb_type') =="forum view" || $__env->yieldContent('breadcrumb_type') =="question view")
+	<li><a href="{{url('tablero-facilitador/foros')}}">Foros</a></li>
+	@endif
+
+</ul>
