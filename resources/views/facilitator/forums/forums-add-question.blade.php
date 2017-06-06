55dd8a1aeb3f67e9ef6eee32f73e8a46d8c6ee9c
hugovom <hugo.om@gmail.com>
2017-06-05 22:57:10 -0500

    update view

--- resources/views/facilitator/forums/forums-add-question.blade.php
+++ resources/views/facilitator/forums/forums-add-question.blade.php
@@ -7,7 +7,11 @@
 @section('content')
 <div class="row">
   <div class="col-sm-12">
-    <h1>Crear nueva pregunta</h1>
+	  @if(isset($forum->session))
+    <h1>Agregar tema o nueva pregunta a <strong>{{$forum->topic}}</strong></h1>
+    @else
+    <h1>Agregar tema o nueva pregunta a <strong>foro del estado de {{$forum->session}}</strong></h1>
+    @endif
   </div>
 </div>
 <div class="box">
