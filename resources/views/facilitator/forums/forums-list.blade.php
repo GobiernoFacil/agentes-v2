a0b9ca64f7a47178ad45863ad9158e33bc17efd2
hugovom <hugo.om@gmail.com>
2017-06-05 22:46:45 -0500

    update forums list

--- resources/views/facilitator/forums/forums-list.blade.php
+++ resources/views/facilitator/forums/forums-list.blade.php
@@ -1,16 +1,34 @@
 @extends('layouts.admin.a_master')
-@section('title', 'Foros' )
+@section('title', $forum->topic )
 @section('description','Foros del Programa de Gobierno Abierto desde lo local' )
-@section('body_class', 'facilitator foros')
+@section('body_class', 'foros')
 @section('breadcrumb_type', '')
 
 @section('content')
 <div class="row">
+<!-- título-->
 	<div class="col-sm-9">
-		<h1>Preguntas del {{$forum->topic}}</h1>
+		<h1>{{$forum->topic}}</h1>		
 	</div>
-  <div class="col-sm-3 center">
-		<a href='{{ url("tablero-facilitador/foros/pregunta/crear/$forum->id") }}' class="btn gde"><strong>+</strong> Agregar Pregunta</a>
+	<!-- agregar pregunta-->
+	<div class="col-sm-3 center">
+		<a href='{{ url("tablero-facilitador/foros/pregunta/crear/$forum->id") }}' class="btn gde">Agregar Pregunta o Tema al foro [<strong>+</strong>]</a>
+	</div>
+	<!-- descripción-->
+	<div class="col-sm-12 forum_list">
+		<div class="divider top"></div>
+		@if($forum->session)
+		<p><span class="type module_session">{{$forum->session->module->title}} / {{$forum->session->name}}</span></p>
+		@else
+		<p><span class="type state">Estado</span></p>
+		@endif
+		<p class="author">Creado por <strong>{{!empty($forum->user->institution) ? $forum->user->institution : ''}}</strong> <span>{{$forum->created_at->diffForHumans()}}</span></p>
+		<div class="divider top"></div>
+	</div>
+	<!-- descripción-->
+	<div class="col-sm-10 col-sm-offset-1">
+		<h4>Descripción</h4>
+		<p>{{$forum->description}}</p>
 	</div>
 </div>
 
@@ -18,29 +36,32 @@
 @if($forums->count()>0)
 <div class="box">
 	<div class="row">
-		<div class="col-sm-12">
-			<table class="table">
-			  <thead>
-			    <tr>
-			      <th>Tema</th>
-			      <th>Descripción</th>
-			      <th>Acciones</th>
-			    </tr>
-			  </thead>
-			  <tbody>
-			    @foreach ($forums as $conversation)
-			      <tr>
-			        <td><h4> <a href='{{ url("tablero-facilitador/foros/pregunta/ver/$forum->id") }}'>{{$conversation->topic}}</a></h4></td>
-              <td>{{str_limit($conversation->description, $limit = 20, $end = '...')}}</td>
-			        <td>
-			          <a href='{{ url("tablero-facilitador/foros/pregunta/ver/$forum->id") }}' class="btn xs view">Ver</a>
-					</tr>
-			    @endforeach
-			  </tbody>
-			</table>
-
+		<div class="col-sm-10 col-sm-offset-1">
+			@foreach ($forums as $conversation)
+				<div class="row forum_list">
+					<div class="divider b"></div>
+					<div class="col-sm-1">
+						<img src='{{url("img/users/default.png")}}' width="100%">
+					</div>
+					<div class="col-sm-9">
+						
+						<h2><a href='{{ url("tablero-facilitador/foros/pregunta/ver/$forum->id") }}'>{{$conversation->topic}}</a></h2>
+						<p class="author">Por {{$conversation->user_id}} <span>{{$conversation->created_at->diffForHumans()}}</span></p>
+					</div>
+					<div class="col-sm-2">
+						<h3 class="count_messages">{{$conversation->messages->count()}}</h3>
+					</div>
+				</div>				 
+			@endforeach
 			{{ $forums->links() }}
 		</div>
+		<div class="col-sm-10 col-sm-offset-1">
+			<div class="divider b"></div>
+		</div>
+		<div class="col-sm-8 col-sm-offset-2 center">
+		<a href='{{ url("tablero-facilitador/foros/pregunta/crear/$forum->id") }}' class="btn gde">Agregar Pregunta o Tema al foro [<strong>+</strong>]</a>
+		</div>
+		
 	</div>
 </div>
 @else
