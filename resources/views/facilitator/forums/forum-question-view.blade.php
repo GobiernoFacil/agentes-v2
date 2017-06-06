27d581eea88c7bbdcf2f4720a2f8ad7ac0a8c1a2
hugovom <hugo.om@gmail.com>
2017-06-06 13:02:37 -0500

    update forum question view

--- resources/views/facilitator/forums/forum-question-view.blade.php
+++ resources/views/facilitator/forums/forum-question-view.blade.php
@@ -5,34 +5,7 @@
 @section('breadcrumb_type', 'forum view')
 
 @section('content')
-<div class="row">
-  <div class="col-sm-9">
-    <h1>{{$question->topic}} </h1>
-  </div>
-  <div class="col-sm-3 center">
-    <a href='{{ url("tablero-facilitador/foros/pregunta/mensajes/agregar/$question->id") }}' class="btn gde"><strong>+</strong> Agregar Mensaje</a>
-  </div>
-</div>
-<div class="box">
-  @if($question->messages)
-    @foreach($question->messages as $message)
-      <div class="row">
-        <div class="col-sm-8 col-sm-offset-2">
-          <p>{{$message->message}}</p>
-        </div>
-      </div>
-    @endforeach
-    <div class="row">
-      <div class="col-sm-3 col-sm-offset-2 center">
-        <a href='{{ url("tablero-facilitador/foros/pregunta/mensajes/agregar/$question->id") }}' class="btn gde"><strong>+</strong> Agregar Mensaje</a>
-      </div>
-    </div>
-  @else
-  <div class="row">
-    <div class="col-sm-8 col-sm-offset-2">
-      <p>No existen mensajes</p>
-    </div>
-  </div>
-  @endif
-</div>
+
+@include('layouts.forums.question-view')
+
 @endsection
