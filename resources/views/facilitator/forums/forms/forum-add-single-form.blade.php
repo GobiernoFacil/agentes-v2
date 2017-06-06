867b9e32de17cd16b7934ef3b2741404c4acefac
hugovom <hugo.om@gmail.com>
2017-06-06 13:15:09 -0500

    update form

--- resources/views/facilitator/forums/forms/forum-add-single-form.blade.php
+++ resources/views/facilitator/forums/forms/forum-add-single-form.blade.php
@@ -2,14 +2,14 @@
 <div class="divider"></div>
 <div class="row">
   <div class="col-sm-12">
-    <h2 class="sa_title">Información del mensaje</h2>
+    <h2 class="sa_title">Información de la respuesta</h2>
   </div>
 </div>
 <!-- situación  -->
 <div class="row">
   <div class="col-sm-12">
     <p>
-      <label><strong>Mensaje</strong> <br>
+      <label><strong>Respuesta</strong> <br>
       {{Form::textarea('message',null, ["class" => "form-control"])}} </label>
       @if($errors->has('message'))
       <strong class="danger">{{$errors->first('message')}}</strong>
@@ -22,7 +22,7 @@
 
 <div class="row">
   <div class="col-sm-12">
-    <p>{{Form::submit('Enviar', ['class' => 'btn gde'])}}</p>
+    <p>{{Form::submit('Enviar respuesta', ['class' => 'btn gde'])}}</p>
   </div>
 </div>
 {!! Form::close() !!}
