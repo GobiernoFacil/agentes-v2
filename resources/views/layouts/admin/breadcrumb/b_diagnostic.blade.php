1ed06b930fd0e2bb704878d623ddb50d05a697cf
hugovom <hugo.om@gmail.com>
2017-06-08 16:32:44 -0500

    update title and add breadcrumb #76

--- resources/views/layouts/admin/breadcrumb/b_diagnostic.blade.php
+++ resources/views/layouts/admin/breadcrumb/b_diagnostic.blade.php
@@ -4,10 +4,13 @@
 	@if ($__env->yieldContent('breadcrumb_type') =="diagnostic list")
 	<li>Lista de Fellows</li>
 	@endif
-	@if ($__env->yieldContent('breadcrumb_type') =="diagnostic view")
+	@if ($__env->yieldContent('breadcrumb_type') == "diagnostic view" || $__env->yieldContent('breadcrumb_type') == "diagnostic evaluation 1")
 	<li><a href="{{url('dashboard/evaluacion/diagnostico')}}">Lista de Fellows</a></li>
 	@endif
 	@if ($__env->yieldContent('breadcrumb_type') =="diagnostic view")
 	<li>Respuestas de examen de diagnóstico</li>
+	@endif
+	@if ($__env->yieldContent('breadcrumb_type') =="diagnostic evaluation 1")
+	<li>Evaluación de examen de diagnóstico (1/2)</li>
 	@endif
 </ul>
