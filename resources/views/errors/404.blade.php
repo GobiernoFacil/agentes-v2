54ffa6ad178e511a49ce971c7b3b393c75550b92
hugovom <hugo.om@gmail.com>
2017-06-09 16:37:51 -0500

    fix #94

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.frontend.master')
+@section('title', 'Página no encontrada')
+@section('description', 'Página no encontrada')
+@section('content')
+<div class="row">
+	<div class="col-sm-10 col-sm-offset-1">
+		<div class="e404">
+			<h1>4<b class="f404">0</b>4</h1>
+			<h2>Página no encontrada</h2>
+			<p>La página que buscas no está disponible en el sitio. Tal vez ha cambiado su dirección URL, vuelve a intentarlo</p>
+		</div>
+	</div>
+</div>
+@endsection
