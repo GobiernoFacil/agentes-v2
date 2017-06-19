cfc73d44fd02000b0146203f56e539a37de50b3b
hugovom <hugo.om@gmail.com>
2017-06-19 17:18:08 -0500

    add news @ fellow

---
+++ resources/views/fellow/news/newsEvent-view.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.admin.a_master')
+@section('title', $content->title)
+@section('description', $content->title)
+@section('body_class', 'news fellow')
+@section('breadcrumb_type', 'news view')
+@section('breadcrumb', 'layouts.admin.breadcrumb.b_news')
+
+@section('content')
+
+<div class="box">
+	<div class="row">
+		@include('layouts.news.view-news')
+	</div>
+</div>
+@endsection
---
+++ resources/views/fellow/news/newsEvents-list.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.admin.a_master')
+@section('title', 'Noticas y Eventos')
+@section('description', 'Noticias y eventos')
+@section('body_class', 'news fellow')
+@section('breadcrumb_type', 'news list')
+@section('breadcrumb', 'layouts.admin.breadcrumb.b_news')
+@section('content')
+<div class="row">
+	<div class="col-sm-9">
+		<h1>Noticias y eventos</h1>
+	</div>
+</div>
+
+@if($news->count() > 0)
+<div class="row">
+	<div class="box">
+		<ul class="list line">
+		@foreach($news as $article)
+			@include('layouts.news.all-list-news')
+		@endforeach
+		</ul>
+	</div>
+</div>
+@else
+<div class="row">
+	<div class="box">
+		<h2>Aún no hay noticias</h2>
+	</div>
+</div>
+@endif
+@endsection
