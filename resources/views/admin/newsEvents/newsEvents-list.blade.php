be4ab7fc1f88a70ad1baa1c6031adf114e7830f4
hugovom <hugo.om@gmail.com>
2017-06-19 17:18:23 -0500

    update news list @ admin

--- resources/views/admin/newsEvents/newsEvents-list.blade.php
+++ resources/views/admin/newsEvents/newsEvents-list.blade.php
@@ -19,25 +19,7 @@
 	<div class="box">
 		<ul class="list line">
 		@foreach($news as $article)
-			<li>
-				<div class="row">
-					 <div class="col-sm-9">
-				@if($article->type==='event')
-				<h4 class="type_n {{$article->type}}">Evento</h4>
-				@elseif($article->type==='news')
-				<h4 class="type_n {{$article->type}}">Noticia</h4>
-				@else
-				<h4 class="type_n {{$article->type}}">Aviso</h4>
-				@endif
-					 </div>
-					  <div class="col-sm-3 right">
-					  	<p class="author">{!! $article->public == 1 ? '<span class="published_ s">Publicado</span>' : '<span class="published_ n">Sin publicar</span>' !!} <a href="{{url('dashboard/noticias-eventos/editar/' . $article->id)}}" class="btn view">Editar {{$article->type==='event' ? "evento" : "noticia"}}</a></p>
-					  </div>
-				</div>
-			<h2><a href="{{url('dashboard/noticias-eventos/ver/' . $article->id)}}">{{$article->title}}</a></h2>
-			<p class="author">Por {{$article->user->name}} <span>{{$article->created_at->diffForHumans()}}</span></p>
-			 {!! \Illuminate\Support\Str::words($article->brief,50,'…') !!}
-			</li>
+			@include('layouts.news.all-list-news')
 		@endforeach
 		</ul>
 	</div>
