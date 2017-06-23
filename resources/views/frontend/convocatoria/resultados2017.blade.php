9f1f9418c4a733bbe857a9f454d6408b39d9dfab
hugovom <hugo.om@gmail.com>
2017-06-23 12:34:57 -0500

    add footnote

--- resources/views/frontend/convocatoria/resultados2017.blade.php
+++ resources/views/frontend/convocatoria/resultados2017.blade.php
@@ -10,7 +10,7 @@
 	<div class="col-sm-10 col-sm-offset-1">
 		<h1><strong>Lista de Candidatos Seleccionados de la Convocatoria 2017</strong> del PROGRAMA DE FORMACIÓN DE <strong>AGENTES LOCALES DE CAMBIO</strong> EN <strong>GOBIERNO ABIERTO</strong> Y DESARROLLO SOSTENIBLE</strong>
 		</h1>
-		<h2>Candidatos seleccionados</h2>
+		<h2>Candidatos seleccionados <a href="#nota"><sup>1</sup></a></h2>
 		
 	</div>
 	<div class="col-sm-8 col-sm-offset-2">
@@ -42,8 +42,12 @@
 				<div class="col-sm-6 col-sm-offset-2">
 					<p class="center"><a href="{{url('convocatoria/metodologia-2017')}}" class="btn gde download">Criterios y metodología de selección &gt;&gt;</a></p>
 				</div>
-				
-				
+				<div class="col-sm-8 col-sm-offset-2">
+				<a id="nota"></a>
+				<div class="notes">
+					<p><sup>1</sup>  <strong>Nota</strong>: Dos candidatos declinaron su participación en el Programa por motivos personales, y a un candidato no se le formalizó su candidatura al no cumplir con los requisitos establecidos en la convocatoria y la documentación solicitada en el marco del proyecto. Por lo tanto, los tres candidatos que los sustituyeron fueron seleccionados de acuerdo al criterio establecido en la Base 4 de la Convocatoria “<em>…En caso de que un participante decida abandonar el programa, dicho espacio será ocupado por el aspirante rechazado con la calificación más alta en el proceso de selección</em>”.</p>
+				</div>
+				</div>
 				
 			</div>
 		</div>
