3b591ac793b0ccb3705d8b7037b815b9f5c23b61
hugovom <hugo.om@gmail.com>
2017-06-24 22:58:07 -0500

    update evaluation view

--- resources/views/admin/modules/quiz/evaluation-add.blade.php
+++ resources/views/admin/modules/quiz/evaluation-add.blade.php
@@ -1,38 +1,49 @@
 @extends('layouts.admin.a_master')
 @section('title', 'Agregar sesión')
 @section('description', 'Agregar preguntas a evaluación')
-@section('body_class', '')
+@section('body_class', 'quiz')
 @section('breadcrumb_type', '')
 @section('breadcrumb', '')
 
 @section('content')
-  <div class="row">
-    <div class="col-sm-12">
-      <h1>Agregar preguntas a evaluación de {{$activity->name}}</h1>
-    </div>
-  </div>
-  <div class="box">
+<div class="row">
+	<div class="col-sm-12">
+		<h1>Agregar preguntas a evaluación de {{$activity->name}}</h1>
+		<p>Da clic en <strong>Agregar pregunta</strong> para comenzar. Cuando estén todas las preguntas da clic en finalizar</p>
+	</div>
+</div>
+<div class="box">
     <div class="row">
-      <div class="col-sm-10 col-sm-offset-1">
+    	<div class="col-sm-10 col-sm-offset-1">
         @if(Session::has('error'))
   			<div class="message error">
-  		      {{ Session::get('error') }}
+  		    	{{ Session::get('error') }}
   		  	</div>
-  		  	@endif
-          <div id="GF-PNUD-EVALUATION-APP">
-            <h1 id="title"></h1>
-            <form  id = "form" role="form"  action="" >
-              {{Form::hidden('quizId',$activity->quizInfo->id, ["class" => "form-control","id"=>"quizInfoId"])}}
-           </form>
-           <ul id="questions-list"></ul>
-           <p><a href="#" id="add-question" class ="btn xs ev">Agregar pregunta</a></p>
-          </div>
-          <div class="col-sm-8 col-sm-offset-2">
-           <a href='{{url("dashboard/sesiones/actividades/evaluacion/checkAnswers/{$activity->quizInfo->id}/{$activity->id}")}}' class="btn gde">FINALIZAR</a>
-          </div>
+  		@endif
+      	</div>
+        <div id="GF-PNUD-EVALUATION-APP">
+			<div class="col-sm-10 col-sm-offset-1">
+        	 	<h1 id="title"></h1>
+        	 	<form  id = "form" role="form"  action="" >
+        	 	   {{Form::hidden('quizId',$activity->quizInfo->id, ["class" => "form-control","id"=>"quizInfoId"])}}
+        	 	</form>
+			 	<!-- lista de preguntas-->
+        	 	<ol id="questions-list"></ol>
+			 	<!-- agregar pregunta-->
+			 	<div class="divider b"></div>
+        	 	<h3><a href="#" id="add-question" class ="btn  ev">Agregar pregunta a evaluación [+]</a></h3>
+        	</div>
+        	
         </div>
-      </div>
+        <!-- finalizar-->
+        <div class="col-sm-12">
+        	<div class="divider"></div>
+    	</div>
+		<div class="col-sm-8 col-sm-offset-2 center">
+				<a href='{{url("dashboard/sesiones/actividades/evaluacion/checkAnswers/{$activity->quizInfo->id}/{$activity->id}")}}' class="btn gde">FINALIZAR</a>
+        </div>
     </div>
+</div>
 @endsection
 
 @section('js-content')
