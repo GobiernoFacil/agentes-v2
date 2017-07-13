b9c323089ae9f2af3b5be21b83920ef1ca4c7895
El Santo <hugo.om@gmail.com>
2017-07-13 10:53:37 -0500

    update evaluation list

--- resources/views/fellow/evaluation/evaluation-list-view.blade.php
+++ resources/views/fellow/evaluation/evaluation-list-view.blade.php
@@ -8,104 +8,106 @@
 <div class="row">
 	<div class="col-sm-12">
 		<h1>Evaluaciones/Tareas</h1>
-    {{ $modules->links() }}
-    <?php $count_modules = 1; ?>
-    @foreach ($modules as $module)
-    <h2><strong>Módulo {{$count_modules}}</strong> {{$module->title}}</h2>
-      @foreach($module->sessions as $session)
-        <h3>Sesión {{$session->order}} {{$session->name}}</h3>
-        <!--lista evaluaciones-->
-       <div class="box session_list">
-         <div class="row">
-           <div class="col-sm-offset-5 col-sm-2">
-             <p>Tipo de Evaluación</p>
-           </div>
-           <div class="col-sm-2">
-             <p>Fecha límite</p>
-           </div>
-           <div class="col-sm-3">
-             <p>Estatus</p>
-           </div>
-
-         </div>
-         @if($session->activity_eval($session->id)->count() > 0)
-          @foreach($session->activity_eval($session->id) as $activity)
-          <div class="row">
-            <!--- título-->
-            <div class="col-sm-5">
-              <h5>Actividad {{$activity->order}}</h5>
-              <h2><a href='{{url("tablero/aprendizaje/{$activity->session->slug}/{$activity->session->slug}/$activity->id")}}'>{{$activity->name}}</a></h2>
-            </div>
-            <!--- tipo de evaluación-->
-            <div class="col-sm-2">
-              <p>{{$activity->files === "Sí" ? 'Ensayo' : 'Examen'}}
-            </div>
-            <!--fecha-->
-            <div class="col-sm-2">
-              <p><strong><span>{{!empty($activity->end) ? \Carbon\Carbon::createFromTimeStamp(strtotime($activity->end))->diffForHumans() : 'Sin fecha'}}</span></strong></p>
-                            <p>{{ !empty($activity->end) ? date("j/m/Y", strtotime($activity->end)) : 'Sin fecha'}}</p>
-            </div>
-            <!--- status-->
-            <div class="col-sm-3">
-              @if($activity->files==='Sí')
-                @if($user->FellowFileUp($user->id,$activity->id))
-                  <p><strong>Completada</strong></p>
-                @else
-                  <div class="header_note">
-                    <p><strong>Sin archivos</strong></p>
-                  </div>
-                  @if($activity->end >= $today )
-                  <a class="btn view block " href='{{ url("tablero/archivos/{$activity->slug}/agregar")}}'>Subir archivo</a>
-                  @else
-                    <div class="box blue center">
-                        <p>El tiempo de la actividad ha terminado</p>
-                          </div>
-                  @endif
-                @endif
-              @else
-                @if($activity->quizInfo)
-                  @if($user->FellowScoreActivity($user->id,$activity->quizInfo->id))
-                    <p><strong>Completada</strong></p>
-                  @else
-                    <div class="header_note">
-                    <p>Sin completar</p>
-                    </div>
-                    @if($activity->end >= $today )
-                    <a class="btn view block " href='{{ url("tablero/evaluacion/{$activity->slug}")}}'>Comenzar evaluación</a>
-                    @else
-                    <div class="box blue center">
-                        <p>El tiempo de la actividad ha terminado</p>
-                          </div>
-                    @endif
-                  @endif
-                @else
-                  <div class="header_note">
-                    <p>Sin completar</p>
-                  </div>
-                  @if($activity->end >= $today )
-                    <a class="btn view block " href='{{ url("tablero/evaluacion/{$activity->slug}")}}'>Comenzar evaluación</a>
-                    @else
-                    <div class="box blue center">
-                     <p>El tiempo de la actividad ha terminado</p>
-                          </div>
-                  @endif
-                @endif
-              @endif
-            </div>
-
-            <div class="col-sm-12">
-              <div class="divider"></div>
-            </div>
-          </div>
-
-          @endforeach
-          @else
-          <h4>Sin evaluaciones</h4>
-          @endif
-            </div>
-      @endforeach
-      <?php $count_modules++;?>
-    @endforeach
+		{{ $modules->links() }}
+		<?php $count_modules = 1; ?>
+		@foreach ($modules as $module)
+		<div class="box">
+			<h1 class="center">Módulo {{$count_modules}}: <strong>{{$module->title}}</strong></h1>
+			<div class="row table_t">
+				<div class="col-sm-offset-5 col-sm-2">
+				  <p>Tipo de Evaluación</p>
+				</div>
+				<div class="col-sm-2">
+				  <p>Fecha límite</p>
+				</div>
+				<div class="col-sm-3">
+				  <p>Estatus</p>
+				</div>
+			</div><!--row ends-->
+			<div class="divider"></div>
+			@foreach($module->sessions as $session)
+			<h2 class="title">Sesión {{$session->order}}: <strong>{{$session->name}}</strong></h2>
+			<!--lista evaluaciones-->
+			<div class="session_list">
+				@if($session->activity_eval($session->id)->count() > 0)
+					@foreach($session->activity_eval($session->id) as $activity)
+					<div class="row">
+						<!--divider-->
+						<div class="col-sm-11 col-sm-offset-1">
+						  <div class="divider"></div>
+						</div>
+						<!--- título-->
+						<div class="col-sm-4 col-sm-offset-1">
+            			  <h5>Actividad {{$activity->order}}</h5>
+            			  <h3><a href='{{url("tablero/aprendizaje/{$activity->session->slug}/{$activity->session->slug}/$activity->id")}}'>{{$activity->name}}</a></h3>
+            			</div>
+            			<!--- tipo de evaluación-->
+            			<div class="col-sm-2">
+            			  <p>{{$activity->files === "Sí" ? 'Ensayo' : 'Examen'}}
+            			</div>
+            			<!--fecha-->
+            			<div class="col-sm-2">
+            				<p><strong><span>{{!empty($activity->end) ? \Carbon\Carbon::createFromTimeStamp(strtotime($activity->end))->diffForHumans() : 'Sin fecha'}}</span></strong><br>
+	            				{{ !empty($activity->end) ? date("j/m/Y", strtotime($activity->end)) : 'Sin fecha'}}</p>
+            			</div>
+						<!--- status-->
+						<div class="col-sm-3">
+						  @if($activity->files==='Sí')
+						    @if($user->FellowFileUp($user->id,$activity->id))
+						      <p><strong>Completada</strong></p>
+						    @else
+						      <div class="header_note">
+						        <p><strong>Sin archivos</strong></p>
+						      </div>
+						      @if($activity->end >= $today )
+						      <a class="btn view block " href='{{ url("tablero/archivos/{$activity->slug}/agregar")}}'>Subir archivo</a>
+						      @else
+						        <div class="footnote">
+						            <p>El tiempo de la actividad ha terminado</p>
+						        </div>
+						      @endif
+						    @endif
+						  @else
+						    @if($activity->quizInfo)
+						      @if($user->FellowScoreActivity($user->id,$activity->quizInfo->id))
+						        <p><strong>Completada</strong></p>
+						      @else
+						        <div class="header_note">
+						        <p>Sin completar</p>
+						        </div>
+						        @if($activity->end >= $today )
+						        <a class="btn view block " href='{{ url("tablero/evaluacion/{$activity->slug}")}}'>Comenzar evaluación</a>
+						        @else
+						        <div class="footnote">
+						            <p>El tiempo de la actividad ha terminado</p>
+						              </div>
+						        @endif
+						      @endif
+						    @else
+						      <div class="header_note">
+						        <p>Sin completar</p>
+						      </div>
+						      @if($activity->end >= $today )
+						        <a class="btn view block " href='{{ url("tablero/evaluacion/{$activity->slug}")}}'>Comenzar evaluación</a>
+						        @else
+						        <div class="footnote">
+									<p>El tiempo de la actividad ha terminado</p>
+						        </div>
+						      @endif
+						    @endif
+						  @endif
+						</div>
+						
+					</div><!--row ends-->
+					@endforeach
+				@else
+				<h4>Sin evaluaciones</h4>
+				@endif
+            </div><!--lista ends-->
+			@endforeach
+			<?php $count_modules++;?>
+		</div><!-- box ends-->
+		@endforeach
    </div>
 </div>
 @else
