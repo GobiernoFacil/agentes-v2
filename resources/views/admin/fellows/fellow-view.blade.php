d2af8dd3a14ec363d647d1230c502e319458c427
El Santo <hugo.om@gmail.com>
2017-07-17 16:47:26 -0500

    update view #108

--- resources/views/admin/fellows/fellow-view.blade.php
+++ resources/views/admin/fellows/fellow-view.blade.php
@@ -8,28 +8,27 @@
 @section('content')
 <div class="row">
 	<div class="col-sm-12">
-		<h1>Perfil de Aspirante</h1>
+		<h1>Perfil de Fellow</h1>
 	</div>
 </div>
 <div class="box">
 	<div class="row">
-		<div class="col-sm-6">
-			<ul class="profile list">
-				<li><span>Nombre:</span>
+		<div class="col-sm-10 col-sm-offset-1 center">
+			<p class="">
 				@if($fellow->image)
-				<img src='{{url("img/users/{$fellow->image->name}")}}' height="50px">
+				<img src='{{url("img/users/{$fellow->image->name}")}}' height="150px">
 				@else
-				<img src='{{url("img/users/default.png")}}' height="50px">
+				<img src='{{url("img/users/default.png")}}' height="150px">
 				@endif
-				
-				<h2>{{$fellow->name." ".$fellow->fellowData->surname." ".$fellow->fellowData->lastname}}</h2></li>
-				<li><span>Email:</span> {{$fellow->email}}</li>
-				<li><span>Nivel de estudios:</span> {{$fellow->fellowData->degree}}</li>
-				<li><span>Procedencia:</span> {{$fellow->fellowData->origin ? $fellow->fellowData->origin : "Sin información"}}</li>
-				<li><span>Ciudad:</span> {{$fellow->fellowData->city}}</li>
-				<li><span>Estado:</span> {{$fellow->fellowData->state}}</li>
-				<li><span>Fecha de creación</span>{{ date("d-m-Y, H:i", strtotime($fellow->created_at)) }} hrs.</li>
-				
+			</p>
+			<h2>{{$fellow->name." ".$fellow->fellowData->surname." ".$fellow->fellowData->lastname}}</h2>
+			<h3>Procedencia: <strong>{{$fellow->fellowData->origin ? $fellow->fellowData->origin : "Sin información"}}</strong></h3>
+			<div class="divider"></div>
+			<ul class="profile list row">
+				<li class="col-sm-4"><span>Nivel de estudios</span>{{$fellow->fellowData->degree}}</li>
+				<li class="col-sm-4"><span>Email</span>{{$fellow->email}}</li>
+				<li class="col-sm-4"><span>Ciudad, Estado</span>{{$fellow->fellowData->city}}, {{$fellow->fellowData->state}}</li>
+
 			</ul>
 		</div>
 	</div>
