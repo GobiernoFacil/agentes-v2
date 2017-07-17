e6df12a1fd7a6db9b8f5457b706c9de2d8161f10
El Santo <hugo.om@gmail.com>
2017-07-17 17:20:31 -0500

    update activities files list

--- resources/views/admin/evaluations/activities-files-list.blade.php
+++ resources/views/admin/evaluations/activities-files-list.blade.php
@@ -1,19 +1,22 @@
 @extends('layouts.admin.a_master')
-@section('title', '')
+@section('title', $activity->name)
 @section('description', 'plataforma del Programa de Formación de Agentes Locales de Cambio en Gobierno Abierto y Desarrollo Sostenible')
-@section('body_class', '')
+@section('body_class', 'evaluation')
 
 @section('content')
 
 @if($fellows->count() > 0)
 <div class="row">
 	<div class="col-sm-12">
-		<h1>Lista de usuarios con archivos para evaluar</h1>
+		<h1>{{$activity->name}}</h1>
+		<h2>Módulo: {{$activity->session->module->title}}</h2>
+		<h3>Sesión: {{$activity->session->name}}</h3>
 	</div>
 </div>
 <div class="box">
 	<div class="row">
 		<div class="col-sm-12">
+			<h3 class="title center">Lista de usuarios con archivos para evaluar</h3>
 			<table class="table">
 			  <thead>
 			    <tr>
@@ -49,13 +52,17 @@
 @else
 <div class="row">
 	<div class="col-sm-9">
-		<h1>Lista de usuarios con archivos para evaluar</h1>
+		<h1>{{$activity->name}}</h1>
+		<h2>Módulo: {{$activity->session->module->title}}</h2>
+		<h3>Sesión: {{$activity->session->name}}</h3>
 	</div>
 </div>
 <div class="box">
 	<div class="row center">
 		<div class="col-sm-10 col-sm-offset-1">
-		<h2>Sin usuarios con archivos</h2>
+			<h3 class="title center">Lista de usuarios con archivos para evaluar</h3>
+			<div class="divider b"></div>
+			<h2>Sin usuarios con archivos</h2>
 		</div>
 	</div>
 </div>
