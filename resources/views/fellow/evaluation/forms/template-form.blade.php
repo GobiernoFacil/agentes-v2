6db5c5d9d21b831dfb16b0ff760a2f3660715bac
El Santo <hugo.om@gmail.com>
2017-07-21 16:41:16 -0500

    update form

--- resources/views/fellow/evaluation/forms/template-form.blade.php
+++ resources/views/fellow/evaluation/forms/template-form.blade.php
@@ -5,35 +5,38 @@
 		<div class="divider b"></div>
 	</div>
 </div>
-<ol>
-  <?php $countP =1;?>
-  @foreach($activity->quizInfo->question as $question)
-  <li class="row">
-    <div class="col-sm-12">
-      <h3>{{$question->question}}</h3>
-    </div>
-    @if($errors->has('answer_q'.$countP))
-      <strong class="danger">{{$errors->first('answer_q'.$countP)}}</strong>
-      @endif
-    <div class="col-sm-10 col-sm-offset-1">
-        <?php $count =0;?>
-				@if($question->count_correct($question->id)>1)
-				<a hred="#" class="btn xs view" id='{{"delete".$countP."_".$count}}'>Borrar respuestas seleccionadas</a>
-				@endif
+<ol class="list line">
+	<?php $countP =1;?>
+	@foreach($activity->quizInfo->question as $question)
+	<li class="row">
+    	<div class="col-sm-12">
+    		<h3>{{$question->question}}</h3>
+    	</div>
+    	<div class="col-sm-12">
+    	@if($errors->has('answer_q'.$countP))
+    	<strong class="danger">{{$errors->first('answer_q'.$countP)}}</strong>
+    	@endif
+    	</div>
+		<div class="col-sm-10 col-sm-offset-1">
+        	<?php $count =0;?>
+			<ul class="list line">
 	        @foreach($question->answer as $answer)
-	          <p>
-	          <label>{{Form::radio('answer_q'.$countP.'['.$count.']',$answer->id, null,['class' => 'form-control answer_q'.$countP,'id'=>'answer_'.$countP.'_'.$count])}}{{$answer->value}} </label>
+	          	<li><label>{{Form::radio('answer_q'.$countP.'['.$count.']',$answer->id, null,['class' => 'form-control answer_q'.$countP,'id'=>'answer_'.$countP.'_'.$count])}}{{$answer->value}} </label>
 	            <?php $count++;?>
-	            </p>
+				</li>
 	        @endforeach
-    </div>
+			</ul>
+	        @if($question->count_correct($question->id)>1)
+			<p><a hred="#" class="btn xs view" id='{{"delete".$countP."_".$count}}'>Borrar respuestas seleccionadas</a></p>
+			@endif
+    	</div>
   </li>
     <?php $countP++;?>
   @endforeach
 </ol>
 <div class="row">
 	<div class="col-sm-6 col-sm-offset-3">
-		<p>{{Form::submit('Guardar', ['class' => 'btn gde'])}}</p>
+		<p>{{Form::submit('Evaluar respuestas', ['class' => 'btn gde'])}}</p>
 	</div>
 </div>
 {!! Form::close() !!}
