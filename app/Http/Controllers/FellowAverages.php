dd461e0d7cb40c6cf6d0059d256c24c67304f000
CarlosOsorio <carlos.aosorio89@gmail.com>
2017-07-24 16:15:57 -0500

    adds new fellow averga

--- app/Http/Controllers/FellowAverages.php
+++ app/Http/Controllers/FellowAverages.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Auth;
+use App\Models\Activity;
+use App\Models\FellowScore;
+use App\Models\FilesEvaluation;
+use App\Models\FellowAverages;
+use App\Models\ForumLog;
+use App\Models\Module;
+use App\Models\ModuleSession;
 
 class FellowAverages extends Controller
 {
-    //
+
+
+
+
+
+    
+
+
 }
