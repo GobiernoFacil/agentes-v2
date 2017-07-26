9c087cdc9c4cbb0a1ddc327401bbe66d4a69bcdb
El Santo <hugo.om@gmail.com>
2017-07-26 16:46:37 -0500

    add session info #142

---
+++ resources/views/admin/fellows/evaluation_includes/eval_list_session.blade.php
@@ -0,0 +1,47 @@
+<li class="row">
+	<!--nombre de sesión-->
+	<span class="col-sm-8">
+	    <span class="session">Sesión {{$session->order}}</span>
+	    <h4>{{$session->name}}</h4>
+	</span>
+	<!--calificación de sesión-->
+	<span class="col-sm-3 right">
+		Calificación de la sesión: <br>
+		<strong>{{$fellow->session_average($fellow->id,$session->id) ? $fellow->session_average($fellow->id,$session->id)->type !='sin' ? number_format($fellow->session_average($fellow->id,$session->id)->average,2) : 'No aplica' : 'Sin calificación'}}</strong>
+	</span>
+	<span class="col-sm-11">
+		<div class="divider b"></div>
+	</span>
+        
+    <!--evaluaciones-->
+    <span class="col-sm-11">
+        <ul>
+	        <li class="row no-padding">
+	        	<span class="col-sm-3 col-sm-offset-6">
+					<span class="sub_tab">Tipo de evaluación</span>
+				</span>
+				<span class="col-sm-3 right">
+					<span class="sub_tab">Calificación</span>
+				</span>
+	        </li>
+			@foreach($session->activities as $activity)
+			    @if($activity->type === 'evaluation')
+					@include('admin.fellows.evaluation_includes.eval_list_session_activity')
+				@endif
+			@endforeach
+            <!--- foros-->
+            <li class="row">
+	        	<span class="col-sm-6">
+	        	 <p><strong>Foros</strong></p>
+	        	</span>
+	        	<span class="col-sm-3">
+	        	 Participaciones
+	        	</span>
+	        	<span class="col-sm-3 right">
+	        	 {{$fellow->forum_participation($fellow->id,$session->id) > 0  ? $fellow->forum_participation($fellow->id,$session->id) : 'Sin participación' }}
+	        	</span>
+	        </li>
+        </ul>
+    </span>
+    <span class="col-sm-12"></span>
+</li>
