c782e7c6e06b6895da830d77c09553ecf4eb4cf0
hugovom <hugo.om@gmail.com>
2017-08-06 22:32:05 -0500

    update survey list #147

--- resources/views/fellow/surveys/survey-list.blade.php
+++ resources/views/fellow/surveys/survey-list.blade.php
@@ -14,36 +14,37 @@
 	<div class="row">
 		<div class="col-sm-12">
 			<table class="table">
-			  <thead>
-			    <tr>
-			      <th>Encuesta</th>
-			      <th>Descripción</th>
-			      <th>Estatus</th>
-            <th>Acciones</th>
-			    </tr>
-			  </thead>
-			  <tbody>
-			    <tr>
-            <td><h4><a href='{{url("tablero/encuestas/encuesta-satisfaccion")}}'>Encuesta de satisfacción</a></h4></td>
-            <td>Encuesta de satisfacción Plataforma Apertus</td>
-            <td>{{$user->fellow_survey ? 'Completada' : 'Sin contestar'}}</td>
-            <td>
-              @if($user->fellow_survey)
-              Completada
-              @else
-              <a href="{{ url('tablero/encuestas/encuesta-satisfaccion') }}" class="btn xs view">Ver</a>
-              @endif
-            </td>
-				</tr>
-				<tr>
-					<td><h4><a href='{{url("tablero/encuestas/encuesta-satisfaccion")}}'>Encuesta de facilitadores</a></h4></td>
-					<td>Encuesta de facilitadores por sesión</td>
-					<td>No aplica</td>
-					<td>
-						<a href="{{ url('tablero/encuestas/facilitadores-modulos') }}" class="btn xs view">Ver</a>
-					</td>
-			</tr>
-			  </tbody>
+				<thead>
+			    	<tr>
+						<th>Encuesta</th>
+						<th>Descripción</th>
+						<th>Estatus</th>
+						<th>Acciones</th>
+			    	</tr>
+				</thead>
+				<tbody>
+			    	<tr>
+						<td><h4><a href='{{url("tablero/encuestas/encuesta-satisfaccion")}}'>Encuesta de satisfacción</a></h4></td>
+						<td>Encuesta de satisfacción Plataforma Apertus</td>
+						<td>{{$user->fellow_survey ? 'Completada' : 'Sin contestar'}}</td>
+						<td>
+						  @if($user->fellow_survey)
+						  Completada
+						  @else
+						  <a href="{{ url('tablero/encuestas/encuesta-satisfaccion') }}" class="btn xs view">Ver</a>
+						  @endif
+						</td>
+					</tr>
+					<tr>
+						<td><h4><a href='{{url("tablero/encuestas/facilitadores-modulos")}}'>Encuesta de facilitadores</a></h4></td>
+						<td>Encuesta de facilitadores por sesión</td>
+						<td>No aplica</td>
+						<td>
+							<a href="{{ url('tablero/encuestas/facilitadores-modulos') }}" class="btn xs view">Ver</a>
+						</td>
+					</tr>
+				
+				</tbody>
 			</table>
 		</div>
 	</div>
