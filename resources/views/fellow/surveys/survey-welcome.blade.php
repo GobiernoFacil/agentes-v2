ab1eb733b6428805d6808e180c620359e984ef78
hugovom <hugo.om@gmail.com>
2017-08-06 22:44:55 -0500

    update welcome view #147

--- resources/views/fellow/surveys/survey-welcome.blade.php
+++ resources/views/fellow/surveys/survey-welcome.blade.php
@@ -7,10 +7,9 @@
 <div class="box">
   <div class="row">
     <div class="col-sm-8 col-sm-offset-2 center">
-      <h1>Encuesta de satisfacción</h1>
+      <h1>Encuesta de satisfacción Plataforma Apertus</h1>
     </div>
     <div class="col-sm-12">
-      <p>Mensaje</p>
     </div>
     <div class="col-sm-4 col-sm-offset-4 center">
       <a href='{{ url("tablero/encuestas/encuesta-satisfaccion/1") }}' class="btn gde">Comenzar</a>
