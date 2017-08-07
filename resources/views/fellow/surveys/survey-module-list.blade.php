cfb5556348c65b6b5117538ce422a6217e64c193
hugovom <hugo.om@gmail.com>
2017-08-06 22:32:38 -0500

    update module list #147

--- resources/views/fellow/surveys/survey-module-list.blade.php
+++ resources/views/fellow/surveys/survey-module-list.blade.php
@@ -7,31 +7,78 @@
 @if($modules->count() > 0)
 <div class="box">
 	<div class="row">
-    <div class="col-sm-10 col-sm-offset-1">
-			<h3 class="title center">Lista de módulos</h3>
-			<p class= "center">Selecciona un módulo para ver sus sesiones</p>
+    	<div class="col-sm-10 col-sm-offset-1">
+			<h3 class="title center">Lista de encuestas de facilitadores por sesión</h3>
+			<p class= "center">Selecciona un facilitador para responder una encuesta anónima </p>
 			<div class="divider b"></div>
 		</div>
 		<div class="col-sm-12">
 			<table class="table">
-			  <thead>
-			    <tr>
-			      <th>Nombre</th>
-			      <th>Descripción</th>
-			      <th>Fecha</th>
-            <th>Acciones</th>
-			    </tr>
-			  </thead>
-			  <tbody>
-        @foreach($modules as $module)
-			    <tr>
-            <td><h4><a href='{{url("tablero/encuestas/facilitadores/$module->slug/sesiones")}}'>{{$module->title}}</a></h4></td>
-            <td>{{$module->objective}}</td>
-            <td> {{$module->created_at->diffForHumans()}}</td>
-            <td>
-              <a href='{{url("tablero/encuestas/facilitadores/$module->slug/sesiones")}}' class="btn xs view">Seleccionar</a>
-            </td>
-				</tr>
+				<thead>
+			    	<tr>
+						<th>Módulo</th>
+						<th>
+							<div class="row">
+								<div class="col-sm-5">	
+								Sesión
+								</div>
+								<div class="col-sm-5">	
+								Facilitador
+								</div>
+								<div class="col-sm-2">	
+								Acción
+								</div>
+							</div>
+						</th>
+						<!--
+						<th>Facilitador</th>
+						<th>Acción</th>-->
+			    	</tr>
+				</thead>
+				<tbody>
+				@foreach($modules as $module)
+			    	<tr>
+						<td><h4>
+							<!--<a href='{{url("tablero/encuestas/facilitadores/$module->slug/sesiones")}}'>-->{{$module->title}}<!--</a>--></h4></td>
+						<td>
+	            
+							<div class="row">
+							@foreach($module->sessions as $session)
+								<div class="col-sm-5">
+									<h4>{{$session->name}}  </h4>
+								</div>
+								<div class="col-sm-7">
+									<ul class="list line">
+							    	@foreach($session->facilitators as $facilitator)
+										<li class="row">
+											<div class="col-sm-2">
+											@if($facilitator->user->image)
+												<img src='{{url("img/users/{$facilitator->user->image->name}")}}' width="100%">
+											@else
+												<img src='{{url("img/users/default.png")}}' width="100%">
+											@endif
+							    			</div>
+											<div class="col-sm-6">
+												<p><a href='{{ url("tablero/encuestas/facilitadores-sesiones/{$session->slug}/w/{$facilitator->user->name}")}}'>{{$facilitator->user->name}}</a></p>
+							    			</div>
+											<div class="col-sm-4">
+											@if(!$user->facilitator_survey($session->id,$user->id,$facilitator->user->id))
+												<a href='{{ url("tablero/encuestas/facilitadores-sesiones/{$session->slug}/w/{$facilitator->user->name}")}}' class="btn xs view">Ir a encuesta</a>
+											@else
+												Completada
+											@endif
+											</div>
+										</li>
+									@endforeach
+									</ul>
+								</div>
+							@endforeach
+							</div>
+            			</td>
+            			<!--
+						<td> {{$module->created_at->diffForHumans()}}</td>
+						<td><a href='{{url("tablero/encuestas/facilitadores/$module->slug/sesiones")}}' class="btn xs view">Seleccionar</a></td>-->
+            		</tr>
       @endforeach
 			  </tbody>
 			</table>
