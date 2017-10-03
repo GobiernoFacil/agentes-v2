6f8153b35c9007f03d1987fd9e8aecf390541c3a
CarlosOsorio <carlos.aosorio89@gmail.com>
2017-10-03 17:37:28 -0500

    add label

--- resources/views/admin/indicators/survey-fac-module-list.blade.php
+++ resources/views/admin/indicators/survey-fac-module-list.blade.php
@@ -27,7 +27,7 @@
 								Facilitador
 								</div>
 								<div class="col-sm-2">
-								Percepción positiva
+								Usuarios con Percepción positiva
 								</div>
 								<div class="col-sm-2">
 								Acción
