1c9537bfd4326778bc0f0e90cbfb9b7d09a85c76
CarlosOsorio <carlos.aosorio89@gmail.com>
2017-11-27 16:37:09 -0600

    adds notice view

--- resources/views/admin/notices/notice-view.blade.php
+++ resources/views/admin/notices/notice-view.blade.php
@@ -7,14 +7,113 @@
 
 @section('content')
 <div class="row">
-	<div class="col-sm-12">
-		<h1>Convocatoria "{{$notice->title}}"</h1>
+	<div class="col-sm-12 center">
+		<h4 class="center"></h4>
+		<div class="divider b"></div>
+		<h2>Convocatoria</h2>
+		<h1 class="center">{{$notice->title}} <span class="le_link"><a href="{{ url('dashboard/convocatorias/editar/'. $notice->id ) }}" class="btn view">Editar convocatoria</a></span></h1>
+		<div class="divider"></div>
 	</div>
 </div>
+
+<!-- header -->
+<div class="row h_tag">
+	<div class="col-sm-4 center">
+		<h4><b class="icon_h i_dates_green"></b> Fecha inicio</h4>
+		<p>{{date("d-m-Y", strtotime($notice->start))}}</p>
+	</div>
+	<div class="col-sm-4 center">
+		<h4><b class="icon_h i_dates_green"></b> Fecha final</h4>
+		<p>{{date('d-m-Y', strtotime($notice->end))}}</p>
+	</div>
+  <div class="col-sm-4 center">
+		<h4><b class="icon_h i_dates_green"></b> Publicada</h4>
+		<p>{{$notice->public ? 'Sí' : 'No'}}</p>
+	</div>
+	<div class="col-sm-12">
+		<div class="divider top"></div>
+	</div>
+</div>
+
+<!--- description-->
 <div class="box">
 	<div class="row">
-		<div class="col-sm-8 col-sm-offset-2">
+		<div class="col-sm-12">
+			<h2 class="title">Descripción <span class="le_link right"><a href="{{ url('dashboard/convocatorias/editar/'. $notice->id ) }}" class="btn view">Editar convocatoria</a></span></h2>
+			<p>{{$notice->description}}</p>
 		</div>
 	</div>
 </div>
+<!--- objetivos-->
+<div class="box">
+	<div class="row">
+		<div class="col-sm-12">
+			<h2 class="title">Objetivo <span class="le_link right"><a href="{{ url('dashboard/convocatorias/editar/'. $notice->id ) }}" class="btn view">Editar convocatoria</a></span></h2>
+			<p>{{$notice->objective}}</p>
+		</div>
+	</div>
+</div>
+<!--- modality_results-->
+<div class="box">
+	<div class="row">
+		<div class="col-sm-12">
+			<h2 class="title">Modalidad y resultados esperados <span class="le_link right"><a href="{{ url('dashboard/convocatorias/editar/'. $notice->id ) }}" class="btn view">Editar convocatoria</a></span></h2>
+			<p>{{$notice->modality_results}}</p>
+		</div>
+	</div>
+</div>
+<!--- Perfil de egreso-->
+<div class="box">
+	<div class="row">
+		<div class="col-sm-12">
+			<h2 class="title">Perfil de egreso <span class="le_link right"><a href="{{ url('dashboard/convocatorias/editar/'. $notice->id ) }}" class="btn view">Editar convocatoria</a></span></h2>
+			<p>{{$notice->profile}}</p>
+		</div>
+	</div>
+</div>
+
+<!--- perfil y elegibilidad-->
+<div class="box">
+	<div class="row">
+		<div class="col-sm-12">
+			<h2 class="title">Perfil y elegibilidad de los participantes <span class="le_link right"><a href="{{ url('dashboard/convocatorias/editar/'. $notice->id ) }}" class="btn view">Editar convocatoria</a></span></h2>
+			<p>{{$notice->profile_eligibility_description}}</p>
+      <h3>Criterios Generales</h3>
+      <p>{{$notice->profile_eligibility_general}}</p>
+      <h3>Criterios Particulares</h3>
+      <p>{{$notice->profile_eligibility_particular}}</p>
+		</div>
+	</div>
+</div>
+
+<!--- Plazos-->
+<div class="box">
+	<div class="row">
+		<div class="col-sm-12">
+			<h2 class="title">Plazos y proceso de postulación <span class="le_link right"><a href="{{ url('dashboard/convocatorias/editar/'. $notice->id ) }}" class="btn view">Editar convocatoria</a></span></h2>
+			<p>{{$notice->term_process}}</p>
+		</div>
+	</div>
+</div>
+
+<!--- Casos no previstos-->
+<div class="box">
+	<div class="row">
+		<div class="col-sm-12">
+			<h2 class="title">Casos no previstos <span class="le_link right"><a href="{{ url('dashboard/convocatorias/editar/'. $notice->id ) }}" class="btn view">Editar convocatoria</a></span></h2>
+			<p>{{$notice->unforeseen_cases}}</p>
+		</div>
+	</div>
+</div>
+
+<!--- contacto-->
+<div class="box">
+	<div class="row">
+		<div class="col-sm-12">
+			<h2 class="title">Contacto <span class="le_link right"><a href="{{ url('dashboard/convocatorias/editar/'. $notice->id ) }}" class="btn view">Editar convocatoria</a></span></h2>
+			<p>{{$notice->contact}}</p>
+		</div>
+	</div>
+</div>
+
 @endsection
