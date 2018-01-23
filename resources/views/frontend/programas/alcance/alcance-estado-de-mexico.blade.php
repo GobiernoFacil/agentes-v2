864b229badf2457e2052a9817ddde91606649c68
El Santo <hugo.om@gmail.com>
2018-01-23 17:28:02 -0600

    add states views

---
+++ resources/views/frontend/programas/alcance/alcance-estado-de-mexico.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.frontend.master')
+@section('title', 'Estado de México, ejercicio local de Gobierno Abierto')
+@section('description', 'El 20 de junio de 2016, el estado de México se integró formalmente a los Ejercicios Locales de Gobierno Abierto a través de la firma de la Declaración Conjunta para la Implementación de las Acciones para un Gobierno Abierto')
+@section('body_class', 'programa alcance edo_mex')
+@section('canonical', url('programa-gobierno-abierto/alcance/estado-de-mexico') )
+@section('breadcrumb', 'layouts.frontend.breadcrumb.bread_programa')
+
+@section('content')
+<div class="row">
+	<div class="col-sm-10 col-sm-offset-1">
+		<h1><strong>Estado de México</strong>, ejercicio local de gobierno abierto</h1>
+	
+		<!---contexto-->
+		<h2>Contexto</h2>
+			<p>El 20 de junio de 2016, el Estado de México se integró formalmente a los Ejercicios Locales de Gobierno Abierto a través de la firma de la <a href="http://inicio.ifai.org.mx/doc/DGGAT/LGTAP70XLVIIID/FirmaDeclaratoriaEdoMexreu.pdf">Declaración Conjunta para la Implementación de Acciones para un Gobierno Abierto</a>.</p>
+			<p>En esta Declaración, firmada por dos representantes del Gobierno del Estado, un representante de la sociedad civil y el Organismo Garante de Acceso a la Información y Protección de Datos Personales en la entidad, el Estado de México se comprometió a establecer actividades y trabajos encaminados a consolidar y expandir la apertura institucional y la participación ciudadana.</p>
+			<p>Luego de la firma de la Declaración Conjunta, en el Estado de México se han impartido 2 sesiones de sensibilización con organizaciones de la sociedad civil y público en general, donde se informó y socializó el ejercicio Gobierno Abierto “Co-creación desde lo Local” y se capacitó a servidores públicos que mostraron interés hacia el proyecto, lo anterior, de cara a la instalación del Secretariado Técnico Local de la entidad.</p>
+			
+		<h2>Estatus</h2>
+		<p>En proceso de definición de representantes de los tres sectores (sociedad civil, autoridad y organismo garante) para la integración del Secretariado Técnico Local.</p>
+
+	</div>
+</div>
+
+@endsection
