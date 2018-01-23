8fcedd75584f1a2e7f847b92a31c9fd683ebf99d
El Santo <hugo.om@gmail.com>
2018-01-23 17:57:15 -0600

    add new states

---
+++ resources/views/frontend/programas/alcance/alcance-quintana-roo.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.frontend.master')
+@section('title', 'Quintana Roo, ejercicio local de Gobierno Abierto')
+@section('description', 'El 31 de agosto de 2017, el estado de Quintana Roo se integró formalmente a los Ejercicios Locales de Gobierno Abierto a través de la firma de la Declaración Conjunta para la Implementación de las Acciones para un Gobierno Abierto')
+@section('body_class', 'programa alcance quintana_roo')
+@section('canonical', url('programa-gobierno-abierto/alcance/quintana-roo') )
+@section('breadcrumb', 'layouts.frontend.breadcrumb.bread_programa')
+
+@section('content')
+<div class="row">
+	<div class="col-sm-10 col-sm-offset-1">
+		<h1><strong>Quintana Roo</strong>, ejercicio local de gobierno abierto</h1>
+	
+		<!---contexto-->
+		<h2>Contexto</h2>
+		<p>El 31 de agosto de 2017, el estado de Quintana Roo se integró formalmente a los Ejercicios Locales de Gobierno Abierto a través de la firma de la Declaración Conjunta para la Implementación de Acciones para un Gobierno Abierto, lo anterior, en el marco del Tercer Foro de Transparencia TransForMar 2017.</p>
+		<p>En esta Declaración, firmada por dos representantes del Gobierno del estado, un representante de gobierno municipal, dos representantes de la sociedad civil y el Organismo Garante de Acceso a la Información y Protección de Datos Personales en la entidad, el estado de Quintana Roo se comprometió a establecer actividades y trabajos encaminados a consolidar y expandir la apertura institucional y la participación ciudadana.</p>
+		
+		<!---Estatus-->	
+		<h2>Estatus</h2>
+		<p>En próximas fechas se agendarán sesiones de sensibilización con organizaciones de la sociedad civil y autoridades, con la finalidad de informar y socializar sobre el ejercicio Gobierno Abierto “Co-creación desde lo Local” y capacitar a servidores públicos que muestren interés hacia el proyecto.</p>
+	</div>
+</div>
+@endsection
