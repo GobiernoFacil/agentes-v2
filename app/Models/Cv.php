4b1c22f2a2de500e9b2d6742cbad8430030581ee
CarlosOsorio <carlos.aosorio89@gmail.com>
2018-01-25 13:24:32 -0600

    cvs models

---
+++ app/Models/Cv.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cv extends Model
+{
+    //
+    protected $fillable = ['aspirant_id', 'age', 'phone', 'mobile', 'email'];
+      //
+
+    public function cv(){
+      return $this->belongsTo('App\Models\Student');
+    }
+    public function academic_trainings(){
+      return $this->hasMany('App\Models\AcademicTraining');
+    }
+
+    public function experiences(){
+      return $this->hasMany('App\Models\Experience');
+    }
+
+    public function further_trainings(){
+      return $this->hasMany('App\Models\FurtherTraining');
+    }
+
+    public function languages(){
+      return $this->hasMany('App\Models\Language');
+    }
+
+    public function softwares(){
+      return $this->hasMany('App\Models\Software');
+    }
+}
