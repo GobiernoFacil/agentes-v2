b533237bda9d72c7b938630aff7ab098d93ac69e
El Santo <hugo.om@gmail.com>
2018-01-25 13:37:45 -0600

    update notice view

--- resources/views/frontend/convocatoria/notice-front.blade.php
+++ resources/views/frontend/convocatoria/notice-front.blade.php
@@ -9,8 +9,9 @@
   @if($notice)
     <div class="row">
     	<div class="col-sm-10 col-sm-offset-1">
-    		<h2><a href='{{url("convocatoria/aplicar/$notice->slug")}}' class="btn gde process">Aplicar a convocatoria</a></h2>
+    		
       	<h1><strong>Convocatoria</strong> {{$notice->title}}</h1>
+      	<h2><a href='{{url("convocatoria/aplicar/$notice->slug")}}' class="btn gde process">Aplicar a convocatoria</a></h2>
         <p>{{$notice->description}}</p>
         <!--files-->
         <div class="row">
@@ -102,14 +103,34 @@
           </div>
         </li>
         </ol>
-
+			<h2><a href='{{url("convocatoria/aplicar/$notice->slug")}}' class="btn gde process">Aplicar a convocatoria</a></h2>
         </div>
+      
+      <div class="col-sm-10 col-sm-offset-1">
+	      <div class="divider"></div>
+      </div>
+      <div class="col-sm-8 col-sm-offset-2">
+	      <h3>Convocatorias anteriores</h3>
+	      <ul>
+		      <li><a href="{{ url('convocatoria/2017') }}">Convocatoria 2017</a></li>
+	      </ul>
+      </div>
+        
     </div>
   @else
     <div class="row">
       <div class="col-sm-10 col-sm-offset-1">
-        <h2 class="danger">Sin convocatorias</h2>
+        <h2 class="danger">Sin convocatoria abierta</h2>
         </h1>
+      </div>
+      <div class="col-sm-10 col-sm-offset-1">
+	      <div class="divider"></div>
+      </div>
+      <div class="col-sm-8 col-sm-offset-2">
+	      <h3>Convocatorias anteriores</h3>
+	      <ul>
+		      <li><a href="{{ url('convocatoria/2017') }}">Convocatoria 2017</a></li>
+	      </ul>
       </div>
     </div>
   @endif
