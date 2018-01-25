3581d4fed29d24852aed40f42a591f3f98eea360
CarlosOsorio <carlos.aosorio89@gmail.com>
2018-01-25 13:15:57 -0600

    cvs tables

---
+++ database/migrations/2018_01_25_130202_create_table_cvs.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableCvs extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('cvs', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('aspirant_id');
+            $table->integer('age')->nullable();
+            $table->integer('phone')->nullable();
+            $table->integer('mobile')->nullable();
+            $table->integer('email')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schemma::dropIfExists('cvs');
+    }
+}
