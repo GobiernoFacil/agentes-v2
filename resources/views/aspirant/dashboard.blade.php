00be51a5cd5c0f57a30b94220506ca6f3f359e4a
El Santo <hugo.om@gmail.com>
2018-01-26 18:25:37 -0600

    add countdown

--- resources/views/aspirant/dashboard.blade.php
+++ resources/views/aspirant/dashboard.blade.php
@@ -12,18 +12,18 @@
 		<div class="divider"></div>
 		<h5 class="center">Tienes:</h5>
 	</div>
-	<div class="as_coutdown">
+	<div id="as_coutdown" class="as_coutdown">
 	<div class="col-sm-2 col-sm-offset-2">
-		<h3 class="center">39 <span>días</span></h3>
-	</div>
-	<div class="col-sm-2">
-		<h3 class="center">39 <span>horas</span></h3>
-	</div>
-	<div class="col-sm-2">
-		<h3 class="center">39 <span>minutos</span></h3>
-	</div>
-	<div class="col-sm-2">
-		<h3 class="center">39 <span>segundos</span></h3>
+		<h3 class="center"><span id="days">--</span> <span class="legend">días</span></h3>
+	</div>						    
+	<div class="col-sm-2">		    
+		<h3 class="center"><span id="hours">--</span> <span class="legend">horas</span></h3>
+	</div>						    
+	<div class="col-sm-2">		    
+		<h3 class="center"><span id="minutes">--</span> <span class="legend">minutos</span></h3>
+	</div>						    
+	<div class="col-sm-2">		    
+		<h3 class="center"><span id="seconds">--</span> <span class="legend">segundos</span></h3>
 	</div>
 	</div>
 	<div class="col-sm-12">
@@ -92,4 +92,38 @@
 	
 
 </div>
+@endsection
+@section('js-content')
+<script>
+// Set the date we're counting down to
+var countDownDate = new Date("Jan 30, 2018 15:37:25").getTime();
+
+// Update the count down every 1 second
+var x = setInterval(function() {
+
+  // Get todays date and time
+  var now = new Date().getTime();
+
+  // Find the distance between now an the count down date
+  var distance = countDownDate - now;
+
+  // Time calculations for days, hours, minutes and seconds
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+  // Display the result in the elements with id
+   document.getElementById("days").innerHTML = days;
+   document.getElementById("hours").innerHTML = hours;
+   document.getElementById("minutes").innerHTML = minutes;
+   document.getElementById("seconds").innerHTML = seconds;
+
+  // If the count down is finished, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("as_coutdown").innerHTML = "EXPIRED";
+  }
+}, 1000);
+</script>
 @endsection
