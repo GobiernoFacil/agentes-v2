61dc6544f66f55846691f9a5ca08583e118a2b05
hugovom <hugo.om@gmail.com>
2018-01-28 09:35:01 -0600

    update form and view of step4

--- resources/views/aspirant/notices/forms/apply-4.blade.php
+++ resources/views/aspirant/notices/forms/apply-4.blade.php
@@ -3,20 +3,19 @@
 
 <div class="row">
   <div class="col-sm-12">
-    <p>
-      <label><strong>Comprobante de domicilio</strong></label><br>
-      <p>Adjunta tu comprobantede domicilio, recuerda verificar antes tu documento, el peso máximo 2.5MB en formato: jpg,jpeg o PDF</p>
+	  <h2>Comprobante de domicilio</h2>
+    
+      <p><label>Adjunta tu comprobante de domicilio, recuerda verificar antes tu documento, el peso máximo 2.5MB en formato: jpg,jpeg o PDF</label></p>
       {{Form::file('proof', ['class' => ''])}} (documento no mayor a 2.5 Mb, formato jpg, jpeg, png o pdf)
       @if($errors->has('proof'))
       <strong class="error">{{$errors->first('proof')}}</strong>
       @endif
-    </p>
   </div>
 </div>
 
 
 <div class="row">
-  <div class="col-sm-12">
+  <div class="col-sm-3 col-sm-offset-9">
     <p>{{Form::submit('Continuar', ['class' => 'btn gde'])}}</p>
   </div>
 </div>
