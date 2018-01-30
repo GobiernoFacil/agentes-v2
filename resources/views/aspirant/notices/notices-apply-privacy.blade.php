bcc8661e736b1d88f5969f1083b75e52bad6d136
El Santo <hugo.om@gmail.com>
2018-01-29 18:20:45 -0600

    add some privacy content

--- resources/views/aspirant/notices/notices-apply-privacy.blade.php
+++ resources/views/aspirant/notices/notices-apply-privacy.blade.php
@@ -2,7 +2,7 @@
 @section('title', 'Aplicar a convocatoria '.$notice->title )
 @section('description', 'Aplicar a convocatoria '.$notice->title)
 @section('body_class', 'aspirante convocatoria')
-@section('breadcrumb_type', 'notice apply privacidad')
+@section('breadcrumb_type', 'notice apply aviso')
 @section('breadcrumb', 'layouts.aspirant.breadcrumb.b_notices')
 
 @section('content')
@@ -12,6 +12,12 @@
 
 <div class="row">
 	<div class="col-sm-12">
+		<h2>AVISO DE PRIVACIDAD DE LA CONVOCATORIA DEL PROGRAMA DE FORMACIÓN DE AGENTES LOCALES DE CAMBIO EN GOBIERNO ABIERTO Y DESARROLLO SOSTENIBLE. <span>{{$notice->title}}</span></h2>
+		<h3>Del responsable de tratar sus datos personales</h3>
+		<p>El Instituto Nacional de Transparencia, Acceso a la Información y Protección de Datos Personales (INAI), con domicilio en Av. Insurgentes Sur, No. 3211, Col. Insurgentes Cuicuilco, Coyoacán, C.P. 04530, Ciudad de México, es el responsable del tratamiento de los datos personales que nos proporcione, los cuales serán protegidos conforme a lo dispuesto en la Ley General de Protección de Datos Personales en Posesión de Sujetos Obligados, y demás normatividad que resulte aplicable. </p>
+
+		
+		<div class="divider"></div>
 		@include('aspirant.notices.forms.apply-5')
 	</div>
 </div>
