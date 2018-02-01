70fe4f992d73f49c4e94f746dee63f841649dcb2
El Santo <hugo.om@gmail.com>
2018-02-01 13:41:18 -0600

    update form 3

--- resources/views/aspirant/notices/forms/apply-3.blade.php
+++ resources/views/aspirant/notices/forms/apply-3.blade.php
@@ -4,11 +4,15 @@
 <div class="row">
   <div class="col-sm-12">
 	  <h2>Video Youtube</h2>
-      <p><label>Agrega el url de tu video en Youtube</label></p>
+	  <p>Elabora un vídeo breve – alrededor de 1 minuto– en donde te presente y destaques las cualidades que te distinguen como un candidato idóneo para participar en el Programa de Formación.</p>
+	  <p>El video puede realizarse con cualquier dispositivo (teléfono móvil, tableta o cámara
+digital), y deberá ser subido a la plataforma en línea <a href="http://www.youtube.com">YouTube</a>.</p>
+      <p><label>Agrega el url de tu video en Youtube
       {{Form::text('video', null, ["class" => "form-control","id"=>"video"])}}
       @if($errors->has('video'))
       <strong class="error">{{$errors->first('video')}}</strong>
       @endif
+      </label>
       <strong class="error" id ="urlError" style="display:none;">Escribe un URL válido</strong>
     </p>
   </div>
