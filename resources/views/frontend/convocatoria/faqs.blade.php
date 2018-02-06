f60a86fec1b0c160360d5f1af7cd0c588845d3da
El Santo <hugo.om@gmail.com>
2018-02-06 13:47:05 -0600

    add view

---
+++ resources/views/frontend/convocatoria/faqs.blade.php
@@ -0,0 +1,61 @@
+@extends('layouts.frontend.master')
+@section('title', 'Preguntas Frecuentes de la Convocatoria Programa de Formación de Agentes Locales de Cambio en Gobierno Abierto y Desarrollo Sostenible')
+@section('description', 'Preguntas Frecuentes de la Convocatoria al Programa de Formación de Agentes Locales de Cambio en Gobierno Abierto y Desarrollo Sostenible')
+@section('body_class', 'convocatoria faqs')
+@section('canonical', url('convocatoria/preguntas-frecuentes') )
+@section('breadcrumb', 'layouts.frontend.breadcrumb.bread_convocatoria')
+
+@section('content')
+ <div class="row">
+    <div class="col-sm-10 col-sm-offset-1">
+		<h1>Preguntas Frecuentes <a href="{{ url('convocatoria')}}">Convocatoria 2018</a></h1>
+		<ol class="toggle-view">
+			<li><h3>¿Cuál es la edad límite para participar?																				   </h3>
+Se aceptarán postulaciones de personas que tengan de 18 años en adelante. Sin embargo, se dará preferencia a las personas que se encuentren dentro del rango de 25 a 40 años señalado en la Convocatoria.</li>
+
+			<li><h3>¿Puedo participar en la Convocatoria si no soy mexicana o mexicano?														   </h3>
+No, el programa es únicamente para las personas que puedan acreditar nacionalidad mexicana.</li>
+
+			<li><h3>¿Cuáles son los diez estados de la República Mexicana que participan en la Convocatoria 2018?							   </h3>
+Campeche, Durango, Estado de México, Guanajuato, Quintana Roo, San Luis Potosí, Sinaloa, Tabasco, Tlaxcala y Veracruz.</li>
+
+			<li><h3>¿Puedo participar si pertenezco a algún estado distinto los señalados en la Convocatoria 2018?							   </h3>
+No. La convocatoria sólo estará abierta para candidatos en los 10 estados mencionados anteriormente: Campeche, Durango, Estado de México, Guanajuato, Quintana Roo, San Luis Potosí, Sinaloa, Tabasco, Tlaxcala y Veracruz.</li>
+
+			<li><h3>¿Cuál es la duración del programa de formación?																			   </h3>
+El programa tiene una duración total de 7 meses y será impartido entre junio y diciembre 2018.</li>
+
+			<li><h3>¿Cuál es la modalidad del programa de formación?																		   </h3>
+La modalidad del programa de formación es principalmente en línea en la plataforma Apertus www.apertus.org.mx. Se realizarán también al menos dos seminarios presenciales en estados de la República Mexicana.</li>
+
+			<li><h3>¿En dónde debe realizarse la postulación para el programa de formación?													   </h3>
+La postulación y registro al programa de formación se realiza única y exclusivamente en línea a través del formato de registro disponible en el portal Apertus en la siguiente liga: http://apertus.org.mx/convocatoria/2018 Ningún documento será recibido por una vía distinta. </li>
+
+			<li><h3>¿Se puede realizar la postulación por partes?																			   </h3>
+Sí, al regístrate en la plataforma se genera un nombre de usuario y contraseña con el que puedes ingresar después para completar la carga de información. La fecha límite para la recepción de postulaciones es el 25 de marzo de 2018.</li>
+
+			<li><h3>¿Qué documentos debo entregar en la postulación?																		   </h3>
+Únicamente una copia de comprobante de domicilio reciente que acredite la residencia en alguno de los 10 estados que señala la Convocatoria.</li>
+
+			<li><h3>¿Qué tengo que decir en el video?																						   </h3>
+En un minuto de duración se espera que te presentes y menciones por qué crees que eres una o un candidato ideal para ser seleccionado por el programa.</li>
+
+			<li><h3>¿Al terminar el registro recibiré un correo o mensaje de confirmación?													   </h3>
+Sí, solo recuerda agregar a tus contactos la cuenta de correo info@apertus.org.mx, para evitar que llegue a tu bandeja de “Correo no deseado”.</li>
+
+			<li><h3>¿Qué pasa si no puedo asistir a todos los seminarios presenciales del Programa de Formación?							   </h3>
+La participación en los seminarios presenciales es clave como parte de tu formación. El Grupo de Trabajo del proyecto analizará cada caso de manera individual y tomará una decisión sobre la permanencia y conclusión del programa. </li>
+
+			<li><h3>¿Al terminar el curso recibiré una constancia de participación?															   </h3>
+Sí, al completar todas las actividades del programa, se te entregará un reconocimiento por tu participación en el programa.</li>
+
+			<li><h3>¿A quién debo contactar si tengo dudas sobre la Convocatoria?															   </h3>
+Puedes enviar un correo a info@apertus.org.mx  y te responderemos a la brevedad posible.</li>
+
+			<li><h3>¿A quién debo contactar si tengo problemas técnicos con la plataforma al momento de realizar mi registro a la Convocatoria?</h3>
+A Argentina Velasco en el correo howdy@gobiernofacil.com </li>
+		</ol>
+
+    </div>
+ </div>    	
+@endsection
