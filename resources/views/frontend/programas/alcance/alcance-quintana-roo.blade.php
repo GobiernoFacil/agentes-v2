d4730996258ac9579e2cb843952bbca2841b8231
El Santo <hugo.om@gmail.com>
2018-02-21 13:09:58 -0600

    update views

--- resources/views/frontend/programas/alcance/alcance-quintana-roo.blade.php
+++ resources/views/frontend/programas/alcance/alcance-quintana-roo.blade.php
@@ -6,18 +6,20 @@
 @section('breadcrumb', 'layouts.frontend.breadcrumb.bread_programa')
 
 @section('content')
-<div class="row">
-	<div class="col-sm-10 col-sm-offset-1">
-		<h1><strong>Quintana Roo</strong>, ejercicio local de gobierno abierto</h1>
-	
-		<!---contexto-->
-		<h2>Contexto</h2>
-		<p>El 31 de agosto de 2017, el estado de Quintana Roo se integró formalmente a los Ejercicios Locales de Gobierno Abierto a través de la firma de la Declaración Conjunta para la Implementación de Acciones para un Gobierno Abierto, lo anterior, en el marco del Tercer Foro de Transparencia TransForMar 2017.</p>
-		<p>En esta Declaración, firmada por dos representantes del Gobierno del estado, un representante de gobierno municipal, dos representantes de la sociedad civil y el Organismo Garante de Acceso a la Información y Protección de Datos Personales en la entidad, el estado de Quintana Roo se comprometió a establecer actividades y trabajos encaminados a consolidar y expandir la apertura institucional y la participación ciudadana.</p>
-		
-		<!---Estatus-->	
-		<h2>Estatus</h2>
-		<p>En próximas fechas se agendarán sesiones de sensibilización con organizaciones de la sociedad civil y autoridades, con la finalidad de informar y socializar sobre el ejercicio Gobierno Abierto “Co-creación desde lo Local” y capacitar a servidores públicos que muestren interés hacia el proyecto.</p>
-	</div>
-</div>
+
+<?php $include_state = "ejercicio_quintanaroo";?>
+@include('frontend.programas.alcance.includes.card_layout')
+
+@endsection
+
+@section('js-content')
+<script src="{{ url('js/bower_components/underscore/underscore-min.js') }}"></script>
+<script src="{{ url('js/bower_components/d3/d3.min.js') }}"></script>
+<script src="{{ url('js/vue.min.js') }}"></script>
+<script>
+  var Format       = d3.format(",2"),
+      FormatDe     = d3.format(",.2f"),
+      currentState = "{{$state}}";
+  </script>
+  <script src="{{ url('js/indicadores/main.js')}}"></script>
 @endsection
--- resources/views/frontend/programas/alcance/alcance-sinaloa.blade.php
+++ resources/views/frontend/programas/alcance/alcance-sinaloa.blade.php
@@ -6,57 +6,20 @@
 @section('breadcrumb', 'layouts.frontend.breadcrumb.bread_programa')
 
 @section('content')
-<div class="row">
-	<div class="col-sm-10 col-sm-offset-1">
-		<h1><strong>Sinaloa</strong>, ejercicio local de gobierno abierto</h1>
-		<h2>¿Quiénes conforman el ejercicio?</h2>
-		<p>El Secretariado Técnico Local (STL), está conformado por: </p>
-		<ul>
-			<li>
-				<h3>6 representantes de la <strong>Sociedad Civil</strong></h3>				 
-				<ol>
-					<li>Iniciativa Sinaloa.</li>
-					<li>COPARMEX – Sinaloa.</li>
-					<li>Mexicanos Primero Sinaloa.</li>
-					<li>Vigilantes Ciudadanos por la Transparencia en Sinaloa.</li>
-					<li>Parlamento Ciudadano Sinaloa.</li>
-					<li>Movimiento Interdisciplinario de Estudiantes y Egresados de la Universidad Autónoma de Sinaloa.</li>
-				</ol>
-			</li>
-			<li><h3>3 representantes del Gobierno</h3>
-				<ol>
-					<li>Supremo Tribunal de Justicia del Estado de Sinaloa.</li>
-					<li>Congreso del Estado de Sinaloa.</li>
-					<li>Secretaría de Transparencia.</li>
-				</ol>
-			</li>
-			<li><h3>4 representantes de Organismos Autónomos</h3>
-				<ol>
-					<li>Comisión Estatal de Derechos Humanos.</li>
-					<li>Tribunal Electoral de Sinaloa.</li>
-					<li>Tribunal de Justicia Administrativa.</li>
-					<li>Instituto Electoral de Sinaloa.</li>
-				</ol>
-			</li>
-			<li><h3>2 representantes del Órgano Garante de Acceso a la Información</h3>
-				<ol>
-					<li>Comisión Estatal para el Acceso a la Información Pública.</li>
-				</ol>
-			</li>
-		</ul>
-		
-		<h3>Facilitador:</h3>
-		<p>Dra. Erika Cecilia Montoya Zavala – Universidad Autónoma de Sinaloa.</p>
-		<!---contexto-->
-		<h2>Contexto</h2>
-		<p>El 20 de junio de 2016, el estado de Sinaloa se integró formalmente a los Ejercicios Locales de Gobierno Abierto a través de la firma de la <a href="http://inicio.ifai.org.mx/doc/DGGAT/LGTAP70XLVIIID/FirmaDeclaratoriaSinaloareu.pdf">Declaración Conjunta para la Implementación de Acciones para un Gobierno Abierto</a>.</p>
-		<p>En esta Declaración, firmada por un representante del Gobierno del Estado, un representante de la sociedad civil y el Organismo Garante de Acceso a la Información y Protección de Datos Personales en la entidad, Sinaloa se comprometió a establecer actividades y trabajos encaminados a consolidar y expandir la apertura institucional y la participación ciudadana.</p>
-		<p>Luego de la firma de la Declaración Conjunta, en Sinaloa se han impartido tres sesiones de sensibilización con organizaciones de la sociedad civil y público en general, donde se informó y socializó el ejercicio Gobierno Abierto “Co-creación desde lo Local” y se capacitó a servidores públicos que mostraron interés hacia el proyecto.</p>
-		<p>Posteriormente, el 18 de abril de 2017 se instaló el Secretariado Técnico en la entidad, mismo que tiene la encomienda de coordinar el proceso de construcción del primer Plan de Acción en la entidad.</p>
-		<!---Estatus-->	
-		<h2>Estatus</h2>
-		<p>Actualmente, el estado se encuentra definiendo la ruta a seguir para la celebración de mesas participativas.</p>
 
-	</div>
-</div>
+<?php $include_state = "ejercicio_sinaloa";?>
+@include('frontend.programas.alcance.includes.card_layout')
+
+@endsection
+
+@section('js-content')
+<script src="{{ url('js/bower_components/underscore/underscore-min.js') }}"></script>
+<script src="{{ url('js/bower_components/d3/d3.min.js') }}"></script>
+<script src="{{ url('js/vue.min.js') }}"></script>
+<script>
+  var Format       = d3.format(",2"),
+      FormatDe     = d3.format(",.2f"),
+      currentState = "{{$state}}";
+  </script>
+  <script src="{{ url('js/indicadores/main.js')}}"></script>
 @endsection
