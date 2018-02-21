375d7c81134597d1f4c697888299c5ea3f20e049
El Santo <hugo.om@gmail.com>
2018-02-21 13:09:14 -0600

    add ga info

---
+++ resources/views/frontend/programas/alcance/includes/ejercicio_tabasco.blade.php
@@ -0,0 +1,40 @@
+<div class="col-sm-10 col-sm-offset-1">
+		<h2><strong>Tabasco</strong>, ejercicio local de gobierno abierto</h2>
+</div>
+<div class="col-sm-8 col-sm-offset-2">
+
+		<h2>¿Quiénes conforman el ejercicio?</h2>
+		<p>El Secretariado Técnico Local (STL), está conformado por: </p>
+		<ul>
+			<li>
+				<h3>1 representante de la <strong>Sociedad Civil</strong></h3>				 
+				<ol>
+					<li>EGES para el Desarrollo, A.C.</li>
+				</ol>
+			</li>
+			<li><h3>1 representante del Gobierno</h3>
+				<ol>
+					<li>Coordinación General de Asuntos Jurídicos del Poder Ejecutivo.</li>
+				</ol>
+			</li>
+			<li><h3>1 representante del Órgano Garante de Acceso a la Información</h3>
+				<ol>
+					<li>Instituto Tabasqueño de Transparencia y Acceso a la Información Pública.</li>
+				</ol>
+			</li>
+		</ul>
+		
+		<h3>Facilitador:</h3>
+		<p>Dra. Gabriela Gallegos Sánchez – Investigadora de la Universidad de Juárez Autónoma de Tabasco.</p>
+		<!---contexto-->
+		<h2>Contexto</h2>
+		<p>El 9 de marzo de 2015, el estado de Tabasco se integró formalmente a los Ejercicios Locales de Gobierno Abierto a través de la firma de la <a href="http://inicio.ifai.org.mx/SiteCollectionDocuments/Transparencia/Tabasco/Tabasco%2520segunda%2520Declaratoria.pdf">Declaración Conjunta para la Implementación de Acciones para un Gobierno Abierto</a>, misma que fue refrendada el 2 de marzo de 2017.</p>
+		<p>En esta Declaración, firmada por un representante del Gobierno del Estado, un representante de la sociedad civil y el Organismo Garante de Acceso a la Información y Protección de Datos Personales en la entidad, Tabasco se comprometió a establecer actividades y trabajos encaminados a consolidar y expandir la apertura institucional y la participación ciudadana.</p>
+		<p>Luego del relanzamiento formal del ejercicio mediante el refrendo de la Declaración Conjunta, en Tabasco se han impartido tres sesiones de sensibilización con organizaciones de la sociedad civil y público en general, donde se informó y socializó el ejercicio Gobierno Abierto “Co-creación desde lo Local” y se capacitó a servidores públicos que mostraron interés hacia el proyecto.</p>
+		<p>El Secretariado Técnico en la entidad fue instalado el 14 de septiembre de 2017 y actualmente tiene la encomienda de coordinar el proceso de construcción del primer Plan de Acción en la entidad.</p>
+		
+		<!---Estatus-->	
+		<h2>Estatus</h2>
+		<p>Actualmente, el estado se encuentra definiendo la ruta a seguir para la celebración de mesas participativas.</p>
+
+	</div>
