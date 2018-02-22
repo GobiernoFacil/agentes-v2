1f997b99d785627be5c5014968236ac0c9721ff9
El Santo <hugo.om@gmail.com>
2018-02-22 10:56:43 -0600

    update routes

--- routes/web.php
+++ routes/web.php
@@ -60,6 +60,7 @@ Route::get('cities', 'NoticeFront@cities');
 /*********  Enlaces programa ******** */
 /*********  programa 2017 ******** */
 Route::get('programa-gobierno-abierto/2017', 'Front@pro17');
+Route::get('programa-gobierno-abierto/2017/testimonios', 'Front@testimony17');
 /*********  programa 2018 ******** */
 Route::get('programa-gobierno-abierto/2018', 'Front@pro18');
 Route::get('programa-gobierno-abierto/antecedentes', 'Front@antecedentes');
