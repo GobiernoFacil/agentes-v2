73b80b029888638c6c2e7f2010a2079115742e96
El Santo <hugo.om@gmail.com>
2018-02-22 10:57:11 -0600

    update breadcrumb

--- resources/views/layouts/frontend/breadcrumb/bread_programa.blade.php
+++ resources/views/layouts/frontend/breadcrumb/bread_programa.blade.php
@@ -6,7 +6,7 @@
 	@endif
 	@if ($__env->yieldContent('body_class') =="programa aliados" || $__env->yieldContent('body_class') =="programa antecedentes" || $__env->yieldContent('body_class') =="programa alcance" || $__env->yieldContent('body_class') =="programa alcance chihuahua" || $__env->yieldContent('body_class') =="programa alcance sonora" || $__env->yieldContent('body_class') =="programa alcance tabasco"
 	|| $__env->yieldContent('body_class') =="programa alcance leon" || $__env->yieldContent('body_class') =="programa alcance oaxaca" || $__env->yieldContent('body_class') =="programa alcance morelos"
-	|| $__env->yieldContent('body_class') =="programa 2017" || $__env->yieldContent('body_class') =="programa 2018" || $__env->yieldContent('body_class') =="programa alcance campeche" || $__env->yieldContent('body_class') =="programa alcance durango" || $__env->yieldContent('body_class') =="programa alcance edo_mex" || $__env->yieldContent('body_class') =="programa alcance guanajuato" || $__env->yieldContent('body_class') =="programa alcance quintana_roo" || $__env->yieldContent('body_class') =="programa alcance potosi" || $__env->yieldContent('body_class') =="programa alcance sinaloa" || $__env->yieldContent('body_class') =="programa alcance tlaxcala" || $__env->yieldContent('body_class') =="programa alcance veracruz"  )
+	|| $__env->yieldContent('body_class') =="programa 2017" || $__env->yieldContent('body_class') =="testimonios 2017"  || $__env->yieldContent('body_class') =="programa 2018" || $__env->yieldContent('body_class') =="programa alcance campeche" || $__env->yieldContent('body_class') =="programa alcance durango" || $__env->yieldContent('body_class') =="programa alcance edo_mex" || $__env->yieldContent('body_class') =="programa alcance guanajuato" || $__env->yieldContent('body_class') =="programa alcance quintana_roo" || $__env->yieldContent('body_class') =="programa alcance potosi" || $__env->yieldContent('body_class') =="programa alcance sinaloa" || $__env->yieldContent('body_class') =="programa alcance tlaxcala" || $__env->yieldContent('body_class') =="programa alcance veracruz"  )
 	<li><a href="{{url('programa-gobierno-abierto')}}">Programa de Gobierno Abierto</a></li>
 	@endif
 	@if ($__env->yieldContent('body_class') =="programa aliados")
@@ -69,6 +69,10 @@
 	@endif
 	@if ($__env->yieldContent('body_class') =="programa 2017")
 	<li>2017</li>
+	@endif
+	@if ($__env->yieldContent('body_class') =="testimonios 2017")
+	<li><a href="{{url('programa-gobierno-abierto/2017')}}">2017</a>
+	<li>Testimonios</li>
 	@endif
 	@if ($__env->yieldContent('body_class') =="programa 2018")
 	<li>2018</li>
