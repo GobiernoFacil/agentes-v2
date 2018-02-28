14aa8703cb93909958afba62606c344d2c3fc03d
El Santo <hugo.om@gmail.com>
2018-02-28 15:54:37 -0600

    update view

--- resources/views/layouts/fellow/subnav/activity.blade.php
+++ resources/views/layouts/fellow/subnav/activity.blade.php
@@ -1,2 +1,8 @@
-<a href="">< Regresar a Semana 1</a>
-<a href="">Sesiones</a>
+<div class="left">
+	<a href="">< Regresar a Semana 1</a>
+	<a href="">Sesiones</a>
+</div>
+<div class="right">
+	<a href="">Anterior</a>
+	<a href="">Siguiente</a>
+</div>
