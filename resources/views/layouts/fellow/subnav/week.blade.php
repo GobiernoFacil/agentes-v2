199c89cded88176ab2b1df2d7625968571805938
El Santo <hugo.om@gmail.com>
2018-02-28 18:03:58 -0600

    add subnav layout

---
+++ resources/views/layouts/fellow/subnav/week.blade.php
@@ -0,0 +1,25 @@
+<nav class="nav_week">
+	<!-- lista de sesiones-->
+	@if($activity->session->module->sessions->count() > 0)
+	<div class="ap_week">
+    	@foreach($activity->session->module->sessions as $session)
+			<h2>{{$session->name}}</h2>
+			@if($session->activities->count() > 0)
+			<ul class="ap_list">
+				@foreach ($session->activities as $activity)
+				<li class="row">
+					<span class="col-sm-9">
+						<b class="{{$activity->type}}"><span class="{{ $activity->type == "video" ? 'arrow-right' : '' }}"></span></b>
+						<a href="{{ url('tablero/aprendizaje/'. $session->module->slug .'/'. $session->slug .'/' . $activity->id) }}">{{$activity->name}} </a> 
+					</span>
+					<span class="col-sm-3">
+						<span class="notes">{{$activity->duration}} min.</span>
+					</span>
+				</li>
+				@endforeach
+			</ul>
+			@endif
+		@endforeach
+	</div>
+	@endif
+</nav>
