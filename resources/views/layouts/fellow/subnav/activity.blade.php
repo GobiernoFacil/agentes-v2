9df25fb578df3faf50e925a9f8e30ec664ec009a
el coruco <boris@gobiernofacil.com>
2018-03-09 17:31:50 -0600

    add toggle to sessions menu

--- resources/views/layouts/fellow/subnav/activity.blade.php
+++ resources/views/layouts/fellow/subnav/activity.blade.php
@@ -1,6 +1,6 @@
 <div class="left">
 	<a href=""><strong>&lt;</strong> Regresar a Semana 1</a>
-	<a href="">Sesiones</a>
+	<a href="#" id="display-week-menu">Sesiones</a>
 </div>
 <div class="right">
 	<a href=""><strong>&lt;</strong> Anterior</a>
--- resources/views/layouts/fellow/subnav/week.blade.php
+++ resources/views/layouts/fellow/subnav/week.blade.php
@@ -1,4 +1,4 @@
-<nav class="nav_week open">
+<nav class="nav_week" id="week-menu-shalala" style="display: none">
 	<!-- lista de sesiones-->
 	@if($activity->session->module->sessions->count() > 0)
 	<div class="ap_week">
@@ -6,14 +6,14 @@
 			<h2>{{$session->name}}</h2>
 			@if($session->activities->count() > 0)
 			<ul class="ap_list">
-				@foreach ($session->activities as $activity)
+				@foreach ($session->activities as $_activity)
 				<li class="row">
 					<span class="col-sm-9">
-						<b class="{{$activity->type}}"><span class="{{ $activity->type == "video" ? 'arrow-right' : '' }}"></span></b>
-						<a href="{{ url('tablero/aprendizaje/'. $session->module->slug .'/'. $session->slug .'/' . $activity->id) }}">{{$activity->name}} </a> 
+						<b class="{{$_activity->type}}"><span class="{{ $_activity->type == "video" ? 'arrow-right' : '' }}"></span></b>
+						<a href="{{ url('tablero/aprendizaje/'. $session->module->slug .'/'. $session->slug .'/' . $_activity->id) }}" class="{{$activity->id == $_activity->id ? 'current' : ''}}">{{$_activity->name}} </a> 
 					</span>
 					<span class="col-sm-3">
-						<span class="notes">{{$activity->duration}} min.</span>
+						<span class="notes">{{$_activity->duration}} min.</span>
 					</span>
 				</li>
 				@endforeach
