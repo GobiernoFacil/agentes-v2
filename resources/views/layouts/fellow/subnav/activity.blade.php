2f6a58a88f881da26d90d7f635799dbb0ea858f6
hugovom <hugo.om@gmail.com>
2018-03-11 14:45:59 -0600

    add class

--- resources/views/layouts/fellow/subnav/activity.blade.php
+++ resources/views/layouts/fellow/subnav/activity.blade.php
@@ -1,6 +1,6 @@
 <div class="left">
 	<a href=""><strong>&lt;</strong> Regresar a Semana 1</a>
-	<a href="#" id="display-week-menu">Sesiones</a>
+	<a href="#" id="display-week-menu" class="btn_sessions hide"><b></b> Sesiones</a>
 </div>
 <div class="right">
 	<a href=""><strong>&lt;</strong> Anterior</a>
