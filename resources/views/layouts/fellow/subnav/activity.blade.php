ad947d9748d59a65c2605ae2de607f354e156d79
hugovom <hugo.om@gmail.com>
2018-03-12 14:01:44 -0600

    update link to module

--- resources/views/layouts/fellow/subnav/activity.blade.php
+++ resources/views/layouts/fellow/subnav/activity.blade.php
@@ -1,5 +1,9 @@
 <div class="left">
-	<a href=""><strong>&lt;</strong> Regresar a Semana 1</a>
+	@if($user->type == "admin")
+	<a href="{{ url('dashboard/programas/' . $activity->session->module->program->id . '/modulos/ver/' . $activity->session->module->id) }}"><strong>&lt;</strong> Regresar {{ $activity->session->module->title }}</a>
+	@else
+	<a href=""><strong>&lt;</strong> Regresar {{ $activity->session->module->title }}</a>
+	@endif
 	<a href="#" id="display-week-menu" class="btn_sessions hide"><b></b> Sesiones</a>
 </div>
 <div class="right">
