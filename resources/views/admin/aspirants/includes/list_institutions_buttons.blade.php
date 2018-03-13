1a8fa1f6739ad302b11cc59e759d419c77a0e118
hugovom <hugo.om@gmail.com>
2018-03-12 23:45:06 -0600

    update includes

--- resources/views/admin/aspirants/includes/list_institutions_buttons.blade.php
+++ resources/views/admin/aspirants/includes/list_institutions_buttons.blade.php
@@ -1,38 +1,22 @@
-
-
-
-@if($type_list === 0)
 <div class="row">
-	<div class="col-sm-6">
+	<div class="col-sm-4">
 		<a class ="btn view gde"  href ='{{url("dashboard/aspirantes/convocatoria/$notice->id/aspirantes-con-aplicacion-por-evaluar")}}'>Tus aspirantes por <span class= "strong" > evaluar</span> ({{$asToE_count}})</a>
 	</div>
-
-	<div class="col-sm-6">
+	<div class="col-sm-4">
 		<a class ="btn view gde"  href ='{{url("dashboard/aspirantes/convocatoria/$notice->id/aspirantes-con-aplicacion-evaluada")}}'>Tus aspirantes <span class= "strong">evaluados ({{$aIaE_count}})</span></a>
 	</div>
+	<div class="col-sm-4">
+		<a class ="btn view gde"  href ='{{url("dashboard/aspirantes/convocatoria/$notice->id/todos-los-aspirantes-con-aplicacion-evaluada")}}'>Todos los aspirantes<span class= "strong" > evaluados</span> ({{$aAe_count}})</a>
+	</div>
 </div>
-<h3><strong>Todos los aspirantes</strong></h3>
+
+
+<div class="divider"></div>
+
+@if($type_list === 0)
+<h2><strong>Todos los aspirantes</strong></h2>
 @elseif($type_list === 1)
-<div class="row">
-	<div class="col-sm-6">
-		<a class ="btn view gde"  href ='{{url("dashboard/aspirantes/convocatoria/$notice->id/todos-los-aspirantes-con-aplicacion-evaluada")}}'>Todos los aspirantes<span class= "strong" > evaluados</span> ({{$aAe_count}})</a>
-	</div>
-
-	<div class="col-sm-6">
-		<a class ="btn view gde"  href ='{{url("dashboard/aspirantes/convocatoria/$notice->id/aspirantes-con-aplicacion-evaluada")}}'>Tus aspirantes <span class= "strong">evaluados ({{$aIaE_count}})</span></a>
-	</div>
-</div>
-<h3><strong>Tus aspirantes por evaluar</strong></h3>
+<h2><strong>Tus aspirantes por evaluar</strong></h2>
 @elseif($type_list === 2)
-<div class="row">
-	<div class="col-sm-6">
-		<a class ="btn view gde"  href ='{{url("dashboard/aspirantes/convocatoria/$notice->id/todos-los-aspirantes-con-aplicacion-evaluada")}}'>Todos los aspirantes<span class= "strong" > evaluados</span> ({{$aAe_count}})</a>
-	</div>
-
-	<div class="col-sm-6">
-		<a class ="btn view gde"  href ='{{url("dashboard/aspirantes/convocatoria/$notice->id/aspirantes-con-aplicacion-por-evaluar")}}'>Tus aspirantes por <span class= "strong">evaluar ({{$asToE_count}})</span></a>
-	</div>
-</div>
-<h3><strong>Tus aspirantes evaluados</strong></h3>
-
+<h2><strong>Tus aspirantes evaluados</strong></h2>
 @endif
