454c1dfec871439936e5a0fee17b9d6466e4672b
hugovom <hugo.om@gmail.com>
2018-03-12 23:49:46 -0600

    update nav

--- resources/views/admin/aspirants/includes/list_institutions_buttons.blade.php
+++ resources/views/admin/aspirants/includes/list_institutions_buttons.blade.php
@@ -1,12 +1,12 @@
 <div class="row">
 	<div class="col-sm-4">
-		<a class ="btn view gde"  href ='{{url("dashboard/aspirantes/convocatoria/$notice->id/aspirantes-con-aplicacion-por-evaluar")}}'>Tus aspirantes por <span class= "strong" > evaluar</span> ({{$asToE_count}})</a>
+		<a class="btn view gde {{ $type_list === 1 ? "active" : ''}}"  href='{{url("dashboard/aspirantes/convocatoria/$notice->id/aspirantes-con-aplicacion-por-evaluar")}}'>Tus aspirantes por evaluar <strong>({{$asToE_count}})</strong></a>
 	</div>
 	<div class="col-sm-4">
-		<a class ="btn view gde"  href ='{{url("dashboard/aspirantes/convocatoria/$notice->id/aspirantes-con-aplicacion-evaluada")}}'>Tus aspirantes <span class= "strong">evaluados ({{$aIaE_count}})</span></a>
+		<a class="btn view gde {{ $type_list === 2 ? "active" : ''}}"  href='{{url("dashboard/aspirantes/convocatoria/$notice->id/aspirantes-con-aplicacion-evaluada")}}'>Tus aspirantes evaluados <strong>({{$aIaE_count}})</strong></a>
 	</div>
 	<div class="col-sm-4">
-		<a class ="btn view gde"  href ='{{url("dashboard/aspirantes/convocatoria/$notice->id/todos-los-aspirantes-con-aplicacion-evaluada")}}'>Todos los aspirantes<span class= "strong" > evaluados</span> ({{$aAe_count}})</a>
+		<a class="btn view gde {{ $type_list === 0 ? "active" : ''}}"  href='{{url("dashboard/aspirantes/convocatoria/$notice->id/todos-los-aspirantes-con-aplicacion-evaluada")}}'>Todos los aspirantes evaluados <strong>({{$aAe_count}})</strong></a>
 	</div>
 </div>
 
