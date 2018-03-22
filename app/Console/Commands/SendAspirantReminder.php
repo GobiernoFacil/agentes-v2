212a029babe47b1bbe53e83a4eb50a46391a87b2
CarlosOsorio <carlos.aosorio89@gmail.com>
2018-03-22 08:47:07 -0600

    avoid aspirant notification

--- app/Console/Commands/SendAspirantReminder.php
+++ app/Console/Commands/SendAspirantReminder.php
@@ -47,7 +47,7 @@ class SendAspirantReminder extends Command
         $aspirants_id = $notice->aspirants()->pluck('aspirant_id');
         $active  = Aspirant::where('is_activated',1)->whereIn('id',$aspirants_id->toArray())->get();
         foreach ($active as $aspirant) {
-          if(!$aspirant->check_aplication_data()){
+          if(!$aspirant->check_aplication_data() && $aspirant->email != 'tlliterartes@gmail.com'){
             $this->info("Aspirante: ".$aspirant->name.' '.$aspirant->surname.' '.$aspirant->lastname.' - Correo: '.$aspirant->email.' notified');
             $aspirant->notify(new SendReminder($aspirant,$notice));
           }
