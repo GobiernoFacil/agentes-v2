3f7377c4c5c69858eae030a0ba218c4ac750e272
hugovom <hugo.om@gmail.com>
2018-03-26 19:12:22 -0600

    update handle method

--- app/Console/Commands/SendAspirantsCount.php
+++ app/Console/Commands/SendAspirantsCount.php
@@ -73,7 +73,7 @@ class SendAspirantsCount extends Command
                  if($aspirant->AspirantsFile){
                    if($aspirant->AspirantsFile->video && $aspirant->AspirantsFile->proof && $aspirant->AspirantsFile->privacy_policies && $aspirant->AspirantsFile->motives){
                      if($aspirant->cv){
-                        if($aspirant->cv->open_experiences()->count()>0 && $aspirant->cv->experiences()->count()>0 && $aspirant->cv->academic_trainings()->count()>0){
+                        if($aspirant->cv->experiences()->count()>0 && $aspirant->cv->academic_trainings()->count()>0){
                           $count_aspirant_with_data++;
                         }else{
                           $count_aspirant_without_data++;
