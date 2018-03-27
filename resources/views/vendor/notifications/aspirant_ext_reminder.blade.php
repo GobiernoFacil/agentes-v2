18a2fafb2fb89365b4e28061d5418af42d091678
hugovom <hugo.om@gmail.com>
2018-03-26 19:22:09 -0600

    update emailñ

--- resources/views/vendor/notifications/aspirant_ext_reminder.blade.php
+++ resources/views/vendor/notifications/aspirant_ext_reminder.blade.php
@@ -2,10 +2,11 @@
 
 Estimado/a {{$user->name." ".$user->surname." ".$user->lastname}},<br>
 
-Por este medio le informamos que el tiempo para completar su postulación al “Programa de Formación de Agentes Locales de Cambio en Gobierno Abierto y Desarrollo Sostenible” se ha extendido en la plataforma, por lo que lo invitamos a terminar su registro lo más pronto posible. De lo contrario su postulación no podrá ser evaluada.<br>
+Por este medio le informamos que el tiempo para completar su postulación al “Programa de Formación de Agentes Locales de Cambio en Gobierno Abierto y Desarrollo Sostenible” se ha extendido en la plataforma, por lo que lo invitamos a terminar su registro antes del 30 de marzo a las 11:59 pm (hora de la Ciudad de México). De lo contrario su postulación no podrá ser evaluada.<br>
 
 Le agradecemos atender este requerimiento cuanto antes dando clic en el enlace que a continuación se presenta.<br>
 
+No olvide cambiar la configuración de privacidad de su video en YouTube: https://support.google.com/youtube/answer/157177?co=GENIE.Platform%3DDesktop&hl=es
 
 @component('mail::button', ['url' => $url])
 Ir a la plataforma
