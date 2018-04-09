3d35759bc561fb26dd339a4bdbe9d114407becd0
hugovom <hugo.om@gmail.com>
2018-04-09 17:27:02 -0500

    update forums list

--- resources/views/fellow/forums/forums-list.blade.php
+++ resources/views/fellow/forums/forums-list.blade.php
@@ -8,44 +8,40 @@
 @section('content')
 <div class="row">
 	<!-- título-->
-	<div class="col-sm-9">
+	<div class="col-sm-8">
 		<h1>{{$forum->topic}}</h1>
 	</div>
 	<!-- agregar pregunta-->
-	<div class="col-sm-3 center">
-		<a href='{{ url("tablero/{$program->slug}/foros/{$forum->slug}/agregar-pregunta") }}' class="btn gde download">Agregar Pregunta o Tema al foro [<strong>+</strong>]</a>
+	<div class="col-sm-4 center">
+		<a href='{{ url("tablero/{$program->slug}/foros/{$forum->slug}/agregar-pregunta") }}' class="btn view block sessions_l">[<strong>+</strong>] Agregar pregunta o tema al foro </a>
 	</div>
 	<!-- descripción-->
 	<div class="col-sm-12 forum_list">
-		<div class="divider top"></div>
 		@if($forum->type === 'activity')
 		<p><span class="type module_session">{{$forum->session->module->title}} / {{$forum->session->name}}</span></p>
 		@elseif($forum->type ==='general')
 			<p><span class="type general">General</span></p>
 		@elseif($forum->type ==='state')
-			<p><span class="type state">Estado</span></p>
+			
 		@else
 			<p><span class="type state">Soporte</span></p>
 		@endif
 		<p class="author">Creado por <strong>{{!empty($forum->user->institution) ? $forum->user->institution : ''}}</strong> <span>{{$forum->created_at->diffForHumans()}}</span></p>
-		<div class="divider top"></div>
+		<div class="divider bg nm"></div>
 	</div>
 	<!-- descripción-->
-	<div class="col-sm-10 col-sm-offset-1">
-
-		<h4>Descripción</h4>
+	<div class="col-sm-12">
 		<p>{{$forum->description}}</p>
 	</div>
 </div>
 
 
 @if($forums->count()>0)
-<div class="box">
+<div class="box session_list last_activity">
 	<div class="row">
 		<div class="col-sm-10 col-sm-offset-1">
 			@foreach ($forums as $conversation)
 				<div class="row forum_list">
-					<div class="divider b"></div>
 					<div class="col-sm-1">
 						@if($conversation->user->image)
 						<img src='{{url("img/users/{$conversation->user->image->name}")}}' width="100%">
@@ -68,7 +64,7 @@
 							<a href='{{url("tablero/$program->slug/foros/perfil/ver/{$conversation->user->name}/{$conversation->user->fellowData->surname}/{$conversation->user->fellowData->lastname}")}}'>
 							Por {{$conversation->user->name." ".$conversation->user->fellowData->surname." ".$conversation->user->fellowData->lastname}}
 						  </a>
-							<span>{{$conversation->created_at->diffForHumans()}}</span>
+							 <span> | {{$conversation->created_at->diffForHumans()}}</span>
 						</p>
 						@elseif($conversation->user->facilitatorData)
 						<!--facilitator data -->
@@ -76,7 +72,7 @@
 							<a href='{{url("tablero/$program->slug/foros/perfil/ver/{$conversation->user->name}/$type")}}'>
 							Por {{$conversation->user->name." ".$conversation->user->facilitatorData->surname." ".$conversation->user->facilitatorData->lastname}}
 						  </a>
-							<span>{{$conversation->created_at->diffForHumans()}}</span>
+							<span> | {{$conversation->created_at->diffForHumans()}}</span>
 						</p>
 						@else
 						<!--super user data -->
@@ -94,15 +90,16 @@
 					<div class="col-sm-2">
 						<h3 class="count_messages">{{$conversation->messages->count()}}</h3>
 					</div>
+					<div class="col-sm-12">
+					<div class="divider b"></div>
+					</div>
 				</div>
 			@endforeach
 			{{ $forums->links() }}
 		</div>
-		<div class="col-sm-10 col-sm-offset-1">
-			<div class="divider b"></div>
-		</div>
+
 		<div class="col-sm-8 col-sm-offset-2 center">
-			<a href='{{ url("tablero/{$program->slug}/foros/{$forum->slug}/agregar-pregunta") }}' class="btn gde download">Agregar Pregunta o Tema al foro [<strong>+</strong>]</a>
+			<a href='{{ url("tablero/{$program->slug}/foros/{$forum->slug}/agregar-pregunta") }}' class="btn view block sessions_l">[<strong>+</strong>] Agregar pregunta o tema al foro </a>
 		</div>
 	</div>
 </div>
@@ -113,7 +110,7 @@
 			<h2>Sin preguntas o temas en el foro</h2>
 		</div>
 		<div class="col-sm-6 col-sm-offset-3">
-				<a href='{{ url("tablero/{$program->slug}/foros/{$forum->slug}/agregar-pregunta") }}' class="btn gde download">Agregar Pregunta o Tema al foro[<strong>+</strong>]</a>
+				<a href='{{ url("tablero/{$program->slug}/foros/{$forum->slug}/agregar-pregunta") }}' class="btn view block sessions_l">[<strong>+</strong>] Agregar pregunta o tema al foro</a>
   		</div>
 	</div>
 </div>
