703e6e1c0299856c5d37caf298e8faa2062b13e2
CarlosOsorio <carlos.aosorio89@gmail.com>
2018-04-23 16:52:42 -0500

    adds email notification to aspirant  - interview phase

---
+++ app/Notifications/SendAspirantInterview.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class SendAspirantInterview extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($aspirant,$notice)
+    {
+        //
+        $this->aspirant = $aspirant;
+        $this->notice   = $notice;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+      $url = url("/login");
+      return (new MailMessage)
+              ->from('info@apertus.org.mx')
+              ->subject('Aviso: Programa de Formación de Agentes Locales de Cambio')
+              ->markdown('vendor.notifications.aspirant_coming_soon_interview', ['url' => $url,'aspirant'=>$this->aspirant,'notice'=>$this->notice]);
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
