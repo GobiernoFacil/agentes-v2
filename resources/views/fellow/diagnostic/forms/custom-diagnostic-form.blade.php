68160b4c9736460aaf58adf7ff7eb62166be65a4
CarlosOsorio <carlos.aosorio89@gmail.com>
2018-05-18 12:13:36 -0500

    adds diagnostic question schemas

--- resources/views/fellow/diagnostic/forms/custom-diagnostic-form.blade.php
+++ resources/views/fellow/diagnostic/forms/custom-diagnostic-form.blade.php
@@ -13,36 +13,9 @@
     @if($question->type ==="open")
       @include('fellow.diagnostic.includes.open_question_template')
     @elseif($question->type ==="radio")
-      @include('fellow.diagnostic.includes.radio_question_templade')
+      @include('fellow.diagnostic.includes.radio_question_template')
     @elseif($question->type === "answers")
-        <div class="col-sm-12">
-          <h3 class="title"><strong>{{$question->question}}{{$question->required ? "" : " (opcional)"}}</strong></h3>
-        </div>
-        <div class="col-sm-12">
-        @if($errors->has('answer_q'.$count))
-        <strong class="danger">{{$errors->first('answer_q'.$count)}}</strong>
-        @endif
-        </div>
-      <div class="col-sm-10 col-sm-offset-1">
-            <?php $countP =0;?>
-
-            @foreach($question->answers as $answer)
-              <div class="divider b"></div>
-                <p class="row"><label><span class="col-sm-1">{{Form::radio('answer_q'.$count.'['.$countP.']',$answer->id, null,['class' => 'form-control answer_q'.$count,'id'=>'answer_'.$count.'_'.$countP])}}</span><span class="col-sm-11">{{$answer->value}}</span></label>
-                <?php $countP++;?>
-                </p>
-            @endforeach
-            @if($question->count_correct($question->id)>1)
-            <div class="divider b"></div>
-              <p class="right"><a hred="#" class="btn xs danger" id='{{"delete".$countP."_".$count}}'>Borrar respuestas seleccionadas en la pregunta {{$count}}</a></p>
-            <script>
-            $('#delete{{$count}}_{{$countP}}').click(function(event) {
-                event.preventDefault();
-                $('.answer_q{{$count}}').not(this).attr('checked', false);
-             });
-            </script>
-        @endif
-        </div>
+      @include('fellow.diagnostic.includes.answers_question_template')
     @endif
   </li>
     <?php $count++;?>
