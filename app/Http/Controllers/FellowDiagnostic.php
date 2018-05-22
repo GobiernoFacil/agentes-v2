204f6ef02f836b9ac10c941f9fe30485821e559d
CarlosOsorio <carlos.aosorio89@gmail.com>
2018-05-21 20:28:52 -0500

    save fellow diagnostic

--- app/Http/Controllers/FellowDiagnostic.php
+++ app/Http/Controllers/FellowDiagnostic.php
@@ -8,6 +8,7 @@ use App\Models\Activity;
 use App\Models\CustomQuestionnaire;
 use App\Models\CustomFellowAnswer;
 use App\Models\Log;
+use App\Models\FellowProgress;
 // FormValidators
 use App\Http\Requests\SaveCustomTest;
 class FellowDiagnostic extends Controller
@@ -100,7 +101,100 @@ class FellowDiagnostic extends Controller
 
 
     public function save(SaveCustomTest $request){
-      var_dump($request->toArray());
+      $user      = Auth::user();
+      $activity  = Activity::where('slug',$request->activity_slug)->firstOrFail();
+      if($user->new_diagnostic($activity->diagnosticInfo->id)->count() > 0){
+        return redirect("tablero/{$activity->session->module->program->slug}/aprendizaje/{$activity->session->module->slug}/{$activity->session->slug}/$request->activity_slug")->with('error','Ya has contestado la evaluación');
+      }
+      $count = 1;
+      foreach ($activity->diagnosticInfo->questions as $question) {
+          if($question->type === 'open'){
+              $fellowAnswer   = CustomFellowAnswer::firstOrCreate(
+                ['user_id'=>$user->id,
+                 'questionnaire_id'=>$activity->diagnosticInfo->id,
+                 'question_id'=>$question->id,
+               ]);
+              $fellowAnswer->answer = $request->{'question_'.$count.'_'.$question->id};
+              $fellowAnswer->save();
+          }elseif($question->type === 'answers'){
+            if($question->count_correct($question->id)>1){
+               foreach($request->{'question_'.$count.'_'.$question->id} as $singleAnswer_id){
+                 $fellowAnswer   = CustomFellowAnswer::firstOrCreate(
+                   ['user_id'=>$user->id,
+                    'questionnaire_id'=>$activity->diagnosticInfo->id,
+                    'question_id'=>$question->id,
+                    'answer_id'  =>$singleAnswer_id
+                  ]);
+               }
+
+            }else{
+              if(isset($request->{'question_'.$count.'_'.$question->id})){
+                $answer_id = current(array_slice($request->{'question_'.$count.'_'.$question->id}, 0, 1));
+                $fellowAnswer   = CustomFellowAnswer::firstOrCreate(
+                  ['user_id'=>$user->id,
+                   'questionnaire_id'=>$activity->diagnosticInfo->id,
+                   'question_id'=>$question->id,
+                   'answer_id'  =>$answer_id
+                 ]);
+              }
+            }
+
+
+          }elseif($question->type === 'radio'){
+            $name   = 'question_'.$count.'_'.$question->id;
+            //multiple rows and columns type radio
+            if($question->options_rows_number > 1){
+               foreach ($question->answers as $answer) {
+                 # code...
+                 $temp_name = $name.'_'.$answer->id;
+                 if(isset($request->{$temp_name})){
+                   $data = current(array_slice($request->{$temp_name}, 0, 1));
+                   $answer = CustomFellowAnswer::firstOrCreate([
+                     'user_id'=>$user->id,
+                     'questionnaire_id'=>$activity->diagnosticInfo->id,
+                     'question_id'=>$question->id,
+                     'answer_id'=>$answer->id
+                     ]);
+                   $answer->answer = $data;
+                   $answer->save();
+                 }
+               }
+
+            }elseif($question->options_rows_number === 1){
+              if(isset($request->{$name})){
+                //one row type radio
+                $data = current(array_slice($request->{$name}, 0, 1));
+                $answer = CustomFellowAnswer::firstOrCreate([
+                  'user_id'=>$user->id,
+                  'questionnaire_id'=>$activity->diagnosticInfo->id,
+                  'question_id'=>$question->id]);
+                $answer->answer = $data;
+                $answer->save();
+              }
+            }
+
+          }
+        $count++;
+      }
+
+
+
+
+      $log              = Log::firstOrCreate(['user_id'=>$user->id,'type'=>'activity']);
+      $log->session_id  = $activity->session->id;
+      $log->module_id   = $activity->session->module->id;
+      $log->activity_id = $activity->id;
+      $log->program_id  = $activity->session->module->program->id;
+      $log->save();
+      $progress         = FellowProgress::firstOrCreate([
+        'fellow_id'   => $user->id,
+        'program_id'  => $activity->session->module->program->id,
+        'activity_id' => $activity->id,
+        'module_id'   => $activity->session->module->id,
+        'session_id'  => $activity->session->id
+      ]);
+      $progress->status =1;
+      return redirect("tablero/{$activity->session->module->program->slug}/aprendizaje/{$activity->session->module->slug}/{$activity->session->slug}/$request->activity_slug")->with('success','Se ha guardado correctamente la evaluación');
 
     }
 
