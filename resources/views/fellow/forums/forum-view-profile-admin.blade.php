ac56268872e740f290f18e3a06052800b96e78fa
hugovom <hugo.om@gmail.com>
2018-06-01 12:37:43 -0500

    update admin profile

--- resources/views/fellow/forums/forum-view-profile-admin.blade.php
+++ resources/views/fellow/forums/forum-view-profile-admin.blade.php
@@ -8,11 +8,34 @@
 </p>
 <!--name-->
 <h2>{{ $userF->name }}</h2>
-<h3 data-data="{{$user->facilitatorData}}">{{ $userF->institution }}</h3>
+<h3 >{{ $userF->institution }}</h3>
 <div class="divider"></div>
 <!--estudios-->
+<!--estudios-->
 <ul class="profile list row">
+	<li class="col-sm-4"><span>Grado de estudios</span>{{$userF->facilitatorData->degree ?  $userF->facilitatorData->degree :"Sin información" }}</li>
 	<li class="col-sm-4"><span>email</span>{{$userF->email}}</li>
+	<li class="col-sm-4"><span>Sitio Web</span>{!! $userF->facilitatorData->web ? '<a href="'. $userF->facilitatorData->web .'">' . $userF->facilitatorData->web.'</a>' : "Sin información" !!}</li>
 </ul>
+
+<!--redes-->
+<p>
+	@if($userF->facilitatorData->twitter)
+	<a href="{{$userF->facilitatorData->twitter}}" class="facilitador_i tw"></a>
+	@endif
+	@if($userF->facilitatorData->facebook)
+	<a href="{{$userF->facilitatorData->facebook}}" class="facilitador_i fb"></a>
+	@endif
+	@if($userF->facilitatorData->linkedin)
+	<a href="{{$userF->facilitatorData->linkedin}}" class="facilitador_i lk"></a>
+	@endif
+	@if($userF->facilitatorData->other)
+	{{$userF->facilitatorData->other}}
+	@endif
+</p>
+<!--semblanza-->
+<h3>Semblanza</h3>
+<p>{{$userF->facilitatorData->semblance ? $userF->facilitatorData->semblance : "Sin información" }}</p>
+
 
 <div class="divider"></div>
