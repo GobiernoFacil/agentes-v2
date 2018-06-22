8810eb3af0e3f361591485a6af8fde591b52aa23
hugovom <hugo.om@gmail.com>
2018-06-22 17:41:15 -0500

    update module sheet

--- resources/views/fellow/progress/progress-module-sheet.blade.php
+++ resources/views/fellow/progress/progress-module-sheet.blade.php
@@ -7,26 +7,40 @@
 
 @section('content')
 <div class="row">
-  <div class="col-sm-9">
-    <h1>Tu progreso</h1>
-    <h2>{{$module->title}}</h2>
-  </div>
- <div class="col-sm-3 right">
-	 <p>Estatus: <span class="score_a block">{{$user->complete_module($module->id) && $user->check_progress($module->slug,0) ? "Completado" : 'Sin completar'}}
-   </span></p>
-  </div>
+	<div class="col-sm-12">
+    	<h1>Tu progreso</h1>
+	</div>
+	<!---módulo--->
+	<div class="col-sm-12">
+		<div class="box session_list last_activity">
+			<div class="row">
+				<!--título-->
+				<div class="col-sm-9">
+					<h3><a href='{{ url("tablero/{$module->program->slug}/aprendizaje/{$module->slug}") }}' class="ap_link_module">{{$module->title}}</a></h3>					
+				</div>
+				<!--enlace-->
+				<div class="col-sm-3">
+					<p class="right">Estatus: 
+						@if($user->complete_module($module->id) && $user->check_progress($module->slug,0))
+						<span class="score_a block">Completado</span>
+						@else
+						 <span class="noscore_a block">Sin completar</span>
+						@endif
+   					</p>
+				</div>
+			</div>
+		</div>
+	</div>
 </div>
-<div class="box score">
-  <div class="row">
 
-    <div class="col-sm-12">
-	    <div class="divider"></div>
+<!--sesiones-->
+<div class="row">
+    <div class="col-sm-11 col-sm-offset-1">
 		<?php $n = 1;?>
 		@foreach($module->sessions as $session)
-				<!-- evaluaciones por módulo-->
-				@include('fellow.progress.includes.session_list')
+			<!-- evaluaciones por módulo-->
+			@include('fellow.progress.includes.session_list')
 		@endforeach
     </div>
-  </div>
 </div>
 @endsection
