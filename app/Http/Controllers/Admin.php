7909543e0444ec8a476ba5268c540d1abafbc326
CarlosOsorio <carlos.aosorio89@gmail.com>
2018-07-13 12:15:16 -0500

    verified admin

--- app/Http/Controllers/Admin.php
+++ app/Http/Controllers/Admin.php
@@ -5,8 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use Auth;
-use Hash;
 use File;
+use Hash;
 use Mail;
 
 // models
@@ -14,15 +14,13 @@ use App\User;
 use App\Models\Aspirant;
 use App\Models\AspirantInstitution;
 use App\Models\AspirantEvaluation;
-use App\Models\Image;
 use App\Models\Conversation;
-use App\Models\Module;
 use App\Models\FacilitatorData;
 use App\Models\FacilitatorModule;
+use App\Models\Image;
+use App\Models\Module;
 use App\Models\NewsEvent;
 use App\Models\Program;
-
-
 // FormValidators
 use App\Http\Requests\SaveAdmin;
 use App\Http\Requests\UpdateAdmin;
@@ -41,66 +39,61 @@ class Admin extends Controller
        */
       public function dashboard()
       {
-        $user 		  = Auth::user();
-        $aspirants 	  = Aspirant::where('is_activated',1)->count();
-        $testUserId   = User::where('email','andre@fcb.com')->get()->pluck('id');
-    	  $fellows	  = User::where('type',"fellow")->where('enabled',1)->whereNotIn('id',$testUserId->toArray())->count();
-		    $programs 	  = Program::all()->count();
-    		$modules_count 		  = Module::all()->count();
-        $listId = FacilitatorModule::all()->pluck('user_id');
-        $adm_count = User::whereIn('id',$listId->toArray())->where('type','!=','facilitator')->where('enabled',1)->count();
-    		/*$facilitators_count   =  User::where("type", "facilitator")
-        ->orWhere(function($query)use($listId){
-          $query->whereIn('id',$listId->toArray())->where("enabled",1);
-        })
-        ->where("enabled",1)->orderBy('name','asc')->count();*/
+        $user 		          = Auth::user();
+        $aspirants 	        = Aspirant::where('is_activated',1)->count();
+        $testUserId         = User::where('email','andre@fcb.com')->get()->pluck('id');
+    	  $fellows	          = User::where('type',"fellow")->where('enabled',1)->whereNotIn('id',$testUserId->toArray())->count();
+		    $programs 	        = Program::all()->count();
+    		$modules_count      = Module::all()->count();
+        $listId             = FacilitatorModule::all()->pluck('user_id');
+        $adm_count          = User::whereIn('id',$listId->toArray())->where('type','!=','facilitator')->where('enabled',1)->count();
         $sessions_count     = FacilitatorModule::where('user_id',$user->id)->count();
         $facilitator_number = User::where("type", "facilitator")->where("enabled",1)->count();
         $facilitators_count = $facilitator_number + $adm_count;
-		    $news = NewsEvent::orderBy('created_at','desc')->take(3)->get();
+		    $news               = NewsEvent::orderBy('created_at','desc')->take(3)->get();
         $conversation_id     = Conversation::where('user_id',$user->id)->pluck('id');
         $conversations_count = Conversation::where('user_id',$user->id)
         ->orWhere(function($query)use($conversation_id,$user){
           $query->where('to_id',$user->id)->whereNotIn('id',$conversation_id->toArray());
         })
         ->count();
-
-        $edomex_number = Aspirant::where('is_activated',1)->where('state','México')->count();
-        $campeche_number = Aspirant::where('is_activated',1)->where('state','Campeche')->count();
-        $durango_number = Aspirant::where('is_activated',1)->where('state','Durango')->count();
+        //conteo de aspirantes por estado
+        $edomex_number     = Aspirant::where('is_activated',1)->where('state','México')->count();
+        $campeche_number   = Aspirant::where('is_activated',1)->where('state','Campeche')->count();
+        $durango_number    = Aspirant::where('is_activated',1)->where('state','Durango')->count();
         $guanajuato_number = Aspirant::where('is_activated',1)->where('state','Guanajuato')->count();
-        $quintana_number = Aspirant::where('is_activated',1)->where('state','Quintana Roo')->count();
-        $sanluis_number = Aspirant::where('is_activated',1)->where('state','San Luis Potosí')->count();
-        $sinaloa_number = Aspirant::where('is_activated',1)->where('state','Sinaloa')->count();
-        $tabasco_number = Aspirant::where('is_activated',1)->where('state','Tabasco')->count();
-        $tlaxcala_number = Aspirant::where('is_activated',1)->where('state','Tlaxcala')->count();
-        $veracruz_number = Aspirant::where('is_activated',1)->where('state','Veracruz de Ignacio de la Llave ')->count();
+        $quintana_number   = Aspirant::where('is_activated',1)->where('state','Quintana Roo')->count();
+        $sanluis_number    = Aspirant::where('is_activated',1)->where('state','San Luis Potosí')->count();
+        $sinaloa_number    = Aspirant::where('is_activated',1)->where('state','Sinaloa')->count();
+        $tabasco_number    = Aspirant::where('is_activated',1)->where('state','Tabasco')->count();
+        $tlaxcala_number   = Aspirant::where('is_activated',1)->where('state','Tlaxcala')->count();
+        $veracruz_number   = Aspirant::where('is_activated',1)->where('state','Veracruz de Ignacio de la Llave ')->count();
 
         return view('admin.dashboard')->with([
           "user"      			=> $user,
           "aspirants"			=> $aspirants,
-		  "modules_count"		=> $modules_count,
-		  'facilitators_count'  => $facilitators_count,
-		  'fellows'			    => $fellows,
-		  'news'			    => $news,
-		  'conversations_count' => $conversations_count,
-		  'sessions_count'		=> $sessions_count,
-		  'programs'			=> $programs,
-		  'edomex_number' 		  => $edomex_number,
-		  'campeche_number' 		  => $campeche_number,
-		  'durango_number' 		  => $durango_number,
-		  'guanajuato_number' 		  => $guanajuato_number,
-		  'quintana_number' 		  => $quintana_number,
-		  'sanluis_number' 		  => $sanluis_number,
-		  'sinaloa_number' 		  => $sinaloa_number,
-		  'tabasco_number' 		  => $tabasco_number,
-		  'tlaxcala_number' 		  => $tlaxcala_number,
-		  'veracruz_number' 		  => $veracruz_number,
+    		  "modules_count"		=> $modules_count,
+    		  'facilitators_count'  => $facilitators_count,
+    		  'fellows'			    => $fellows,
+    		  'news'			    => $news,
+    		  'conversations_count' => $conversations_count,
+    		  'sessions_count'		=> $sessions_count,
+    		  'programs'			=> $programs,
+    		  'edomex_number' 		  => $edomex_number,
+    		  'campeche_number' 		  => $campeche_number,
+    		  'durango_number' 		  => $durango_number,
+    		  'guanajuato_number' 		  => $guanajuato_number,
+    		  'quintana_number' 		  => $quintana_number,
+    		  'sanluis_number' 		  => $sanluis_number,
+    		  'sinaloa_number' 		  => $sinaloa_number,
+    		  'tabasco_number' 		  => $tabasco_number,
+    		  'tlaxcala_number' 		  => $tlaxcala_number,
+    		  'veracruz_number' 		  => $veracruz_number,
          ]);
       }
 
       /**
-       * Muestra lista de administradores
+       * Muestra lista de administradores para usuario super administrador
        *
        * @return \Illuminate\Http\Response
        */
@@ -163,11 +156,8 @@ class Admin extends Controller
                 $message->to($admin->email);
                 $message->subject($from);
         });
-
-
         return redirect("sa/dashboard/administradores/ver/$admin->id")->with('message','Usuario creado correctamente');
       }
-
       /**
        * Muestra un usuario administrador
        *
@@ -178,7 +168,6 @@ class Admin extends Controller
       {
         $user  = Auth::user();
         $admin = User::find($id);
-
         return view("suAdmin.users.admin-profile")->with([
           "user"  => $user,
           "admin" => $admin
@@ -195,14 +184,12 @@ class Admin extends Controller
       {
         $user  = Auth::user();
         $admin = User::find($id);
-
         return view("suAdmin.users.admin-update")->with([
           "user"  => $user,
           "admin" => $admin
         ]);
 
       }
-
       /**
        * Actualiza usuario administrador
        *
@@ -216,7 +203,6 @@ class Admin extends Controller
         $admin->name  = $request->name;
         $admin->email = $request->email;
         $admin->institution    = $request->institution;
-
         if(!empty($request->password)){
           $admin->password = Hash::make($request->password);
         }
@@ -249,7 +235,6 @@ class Admin extends Controller
         $admin        = User::find($id);
         $admin->enabled  = 0;
         $admin->save();
-
         return redirect("sa/dashboard/administradores")->with("message",'Usuario deshabilitado');
       }
 
@@ -320,7 +305,6 @@ class Admin extends Controller
       }
 
       //debug function, para ver hora de servidor y desplegar lista de ev por realizar
-
       public function time($task){
         if($task ==='1'){
           $programs 	  = Program::where('notice_id',2)->firstOrfail();
