fdc6dbc6ffdbcd9729b2e1a3fe39f00231d01f05
hugovom <hugo.om@gmail.com>
2018-10-04 11:17:04 -0500

    update navigation

--- resources/views/layouts/frontend/header.blade.php
+++ resources/views/layouts/frontend/header.blade.php
@@ -16,8 +16,10 @@
 	<ul>
 		<li><a href="{{url('')}}" class="btn_home {{ $__env->yieldContent('body_class') == 'home' ? 'active' : ''}}"><span>Inicio</span></a></li>
 		<li><a href="{{url('programa-gobierno-abierto')}}" class="{{ $__env->yieldContent('body_class') == 'programa' ? 'active' : ''}}"><span>Programa</span></a></li>
+		<li><a href="{{url('programa-gobierno-abierto/programa-2018/ver-generacion')}}" class="btn_cfellow {{ $__env->yieldContent('body_class') == 'programa' ? 'active' : ''}}"><span>Conoce a los Fellows</span></a></li>
 		<li><a href="{{url('gobierno-abierto')}}" class="btn_abierto {{ $__env->yieldContent('body_class') == 'abierto' ? 'active' : ''}}"><span>Gobierno Abierto</span></a></li>
 		<li><a href="{{url('noticias-eventos')}}" class="btn_noticias {{ $__env->yieldContent('body_class') == 'noticias' ? 'active' : ''}}"><span>Noticias</span></a></li>
+		<li><a href="{{url('noticias-eventos/blog-fellow')}}" class="btn_bfellow {{ $__env->yieldContent('body_class') == 'noticias' ? 'active' : ''}}"><span>Blog de Fellows</span></a></li>
 		<li><a href="{{url('contacto')}}" class="btn_contacto {{ $__env->yieldContent('body_class') == 'contacto' ? 'active' : ''}}"><span>Contacto</span></a></li>
 	</ul>
  
