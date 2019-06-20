a8ca07c1d3cb5137685f65fc8d820199d58fbb61
hugovom <hugo.om@gmail.com>
2019-06-20 11:52:00 -0500

    update image

--- resources/views/layouts/frontend/master.blade.php
+++ resources/views/layouts/frontend/master.blade.php
@@ -119,7 +119,7 @@
 			@if($body_class == "home")
 				<div class="bgdimg">
 					<figure>
-						<img src="{{url('img/apertus_2018.jpg')}}" alt="Gobierno Abierto desde lo local para el desarrollo sostenible">
+						<img src="{{url('img/red-de-estado-abierto.desarrollo-sostenible-americas.jpg')}}" alt="Gobierno Abierto desde lo local para el desarrollo sostenible">
 					</figure>
 					<div class="container">
 						<div class="cta">
